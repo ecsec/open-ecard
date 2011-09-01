e366cd6acf21bd0f1d87da707c266bc388ea741d
Tobias Wich <tobias.wich@ecsec.de>
2011-09-01 16:54:42 +0200

    cleanup style in Event and EventHandler

--- event-manager/src/main/java/de/ecsec/ecard/client/event/Event.java
+++ event-manager/src/main/java/de/ecsec/ecard/client/event/Event.java
@@ -6,41 +6,43 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+
 /**
  *
  * @author Johannes Schmoelz <johannes.schmoelz@ecsec.de>
  */
 public class Event {
-    
+
     private final EventType type;
     private List<EventCallback> callbacks;
-    
+
     public Event(EventType type) {
-        this.type = type;
-        this.callbacks = new CopyOnWriteArrayList<EventCallback>();
+	this.type = type;
+	this.callbacks = new CopyOnWriteArrayList<EventCallback>();
     }
-    
+
     public void addListener(EventCallback callback) {
-        callbacks.add(callback);
+	callbacks.add(callback);
     }
-    
+
     public void removeListener(EventCallback callback) {
-        callbacks.remove(callback);
+	callbacks.remove(callback);
     }
-    
+
     public void notify(Object eventData) {
-        EventCallback callback;
-        for (Iterator<EventCallback> iter = callbacks.iterator(); iter.hasNext(); ) {
-            callback = iter.next();
-            callback.signalEvent(type, eventData);
-        }
+	EventCallback callback;
+	for (Iterator<EventCallback> iter = callbacks.iterator(); iter.hasNext(); ) {
+	    callback = iter.next();
+	    callback.signalEvent(type, eventData);
+	}
     }
-    
+
     public EventType getType() {
-        return type;
+	return type;
     }
-    
+
     public String getName() {
-        return type.name();
+	return type.name();
     }
+
 }

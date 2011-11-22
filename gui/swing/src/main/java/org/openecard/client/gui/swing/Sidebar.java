78abe39c6fc1617ea4b94299a8e618c4fbbf3559
Tobias Wich <tobias.wich@ecsec.de>
2011-11-22 12:30:43 +0100

    add logo to Sidebar and disable list component by removing EventListeners (instead of disabling the component)

--- gui/swing/src/main/java/org/openecard/client/gui/swing/Sidebar.java
+++ gui/swing/src/main/java/org/openecard/client/gui/swing/Sidebar.java
@@ -2,41 +2,71 @@ package org.openecard.client.gui.swing;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Image;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 
 /**
+ * A sidebar represents the progress of the user consent. Every step has an
+ * entry and the current step is selected.
  *
  * @author Tobias Wich <tobias.wich@ecsec.de>
  */
 public class Sidebar {
 
-    private final Container panel;
+    private final Container sidebarPanel;
     private final JList steps;
     private final int numSteps;
 
-    public Sidebar(Container panel, String... stepNames) {
-        this.panel = panel;
+    /**
+     * Create a Sidebar instance and draw its content on the supplied panel.
+     *
+     * @param sidebarPanel Panel to draw sidebar content on.
+     * @param stepNames Names of the steps as they are displayed in the sidebar.
+     */
+    public Sidebar(Container sidebarPanel, String... stepNames) {
+        // create step components
+        this.sidebarPanel = sidebarPanel;
         this.numSteps = stepNames.length;
-        this.steps = new JList(stepNames);
+        this.steps = new JList();
+        this.steps.removeAll();
+        this.steps.validate();
+        this.steps.setListData(stepNames);
         this.steps.setSelectedIndex(0);
         BorderLayout layout = new BorderLayout();
-        this.panel.setLayout(layout);
-        this.panel.add(steps, BorderLayout.CENTER);
+        this.sidebarPanel.setLayout(layout);
+        this.sidebarPanel.add(steps, BorderLayout.CENTER);
+
+        // add logo
+        ImageIcon logo = new ImageIcon(Sidebar.class.getResource("/openecard.png"));
+        Image scaled = logo.getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
+        logo.setImage(scaled);
+        JLabel logoLabel = new JLabel(logo);
+        this.sidebarPanel.add(logoLabel, BorderLayout.SOUTH);
 
         // configure steplist
-        this.steps.setEnabled(false);
+        // remove listeners so no selection is possible through the ui
+        for (MouseListener m : this.steps.getMouseListeners()) {
+            this.steps.removeMouseListener(m);
+        }
+        for (MouseMotionListener m : this.steps.getMouseMotionListeners()) {
+            this.steps.removeMouseMotionListener(m);
+        }
+        this.steps.setFocusable(false);
     }
 
 
-    public void next() {
-        selectIdx(steps.getSelectedIndex()+1);
-    }
-
-    public void previous() {
-        selectIdx(steps.getSelectedIndex()-1);
-    }
-
+    /**
+     * Mark the step belonging to the supplied index as selected. The sidebar
+     * emphasises this step accordingly.<br/>
+     * Values outside the defined range (0 .. numsteps-1) are ignored.
+     *
+     * @param idx Index starting from 0.
+     */
     public void selectIdx(int idx) {
         if (idx >= 0 && idx < numSteps) {
             steps.setSelectedIndex(idx);

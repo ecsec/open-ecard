e3dd5281cc86cc2e913c8aef184a1726410aa051
Tobias Wich <tobias.wich@ecsec.de>
2011-12-13 10:54:06 +0100

    fix android marshaller test and format source code according to style guidelines

--- ifd/scio-backend/android-nfc/src/main/java/org/openecard/client/scio/NFCCardTerminal.java
+++ ifd/scio-backend/android-nfc/src/main/java/org/openecard/client/scio/NFCCardTerminal.java
@@ -1,90 +1,87 @@
 package org.openecard.client.scio;
 
-import java.io.IOException;
-
 import android.nfc.tech.IsoDep;
+import java.io.IOException;
 import javax.smartcardio.Card;
 import javax.smartcardio.CardException;
 import javax.smartcardio.CardNotPresentException;
 import javax.smartcardio.CardTerminal;
 
+
 /**
- * NFC implementation of smartcardio's CardTerminal interface. 
+ * NFC implementation of smartcardio's CardTerminal interface.<br/>
  * Implemented as singleton because we only have one nfc-interface. Only activitys can react on a new intent, so they must set the tag via setTag()
- * @author Dirk Petrautzki <petrautzki@hs-coburg.de>
  *
+ * @author Dirk Petrautzki <petrautzki@hs-coburg.de>
  */
-public class NFCCardTerminal extends CardTerminal{
+public class NFCCardTerminal extends CardTerminal {
 
-	private NFCCard nfcCard = null;
-	private static NFCCardTerminal instance = null;
-	
-	private NFCCardTerminal(){
-		
-	}
-	
-	public void setTag(IsoDep tag){
-		this.nfcCard = new NFCCard(tag);
-	}
-	
-	
-	public static synchronized NFCCardTerminal getInstance(){ 
-		if(instance==null)
-			instance = new NFCCardTerminal();
-		return instance;
-		
-	}
-		
-	@Override
-	public Card connect(String arg0) throws CardException {
-		
-		if(this.nfcCard==null||this.nfcCard.isodep==null){
-			System.out.println("no tag present");
-			throw new CardNotPresentException("No tag present");
-		}
-		try {
-			if(!this.nfcCard.isodep.isConnected()){
-			this.nfcCard.isodep.setTimeout(3000);
-			this.nfcCard.isodep.connect();
-			
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-			this.nfcCard=null;
-			throw new CardException("No connection could be established", e);
-		}
-		return this.nfcCard;
-	}
+    private NFCCard nfcCard = null;
+    private static NFCCardTerminal instance = null;
 
-	@Override
-	public String getName() {
-		return "Integrated NFC";
-	}
+    private NFCCardTerminal() { }
 
-	@Override
-	public boolean isCardPresent() throws CardException {
-		//TODO delete the following sleep 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-boolean ret =  (this.nfcCard!=null && this.nfcCard.isodep!=null && this.nfcCard.isodep.isConnected());
-if(!ret && this.nfcCard!=null) {this.nfcCard.isodep = null;}
-		return ret;
-			
-	}
+    public void setTag(IsoDep tag){
+	this.nfcCard = new NFCCard(tag);
+    }
 
-	@Override
-	public boolean waitForCardAbsent(long arg0) throws CardException {
-		// TODO Auto-generated method stub
-		return false;
-	}
 
-	@Override
-	public boolean waitForCardPresent(long arg0) throws CardException {
-		// TODO Auto-generated method stub
-		return false;
+    public static synchronized NFCCardTerminal getInstance(){
+	if (instance == null) {
+	    instance = new NFCCardTerminal();
 	}
+	return instance;
+    }
+
+    @Override
+    public Card connect(String arg0) throws CardException {
+	if (this.nfcCard == null || this.nfcCard.isodep == null) {
+	    System.out.println("no tag present");
+	    throw new CardNotPresentException("No tag present");
+	}
+	try {
+	    if (!this.nfcCard.isodep.isConnected()) {
+		this.nfcCard.isodep.setTimeout(3000);
+		this.nfcCard.isodep.connect();
+	    }
+	} catch (IOException e) {
+	    e.printStackTrace();
+	    this.nfcCard = null;
+	    throw new CardException("No connection could be established", e);
+	}
+	return this.nfcCard;
+    }
+
+    @Override
+    public String getName() {
+	return "Integrated NFC";
+    }
+
+    @Override
+    public boolean isCardPresent() throws CardException {
+	//TODO delete the following sleep
+	try {
+	    Thread.sleep(1000);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+	boolean ret = (this.nfcCard != null && this.nfcCard.isodep != null && this.nfcCard.isodep.isConnected());
+	if (!ret && this.nfcCard != null) {
+	    this.nfcCard.isodep = null;
+	}
+	return ret;
+    }
+
+    @Override
+    public boolean waitForCardAbsent(long arg0) throws CardException {
+	// TODO Auto-generated method stub
+	return false;
+    }
+
+    @Override
+    public boolean waitForCardPresent(long arg0) throws CardException {
+	// TODO Auto-generated method stub
+	return false;
+    }
 
 }
--- wsdef/android-marshaller/src/test/java/org/openecard/client/ws/android/AndroidMarshallerTest.java
+++ wsdef/android-marshaller/src/test/java/org/openecard/client/ws/android/AndroidMarshallerTest.java
@@ -1,123 +1,126 @@
 package org.openecard.client.ws.android;
 
-
-import iso.std.iso_iec._24727.tech.schema.ChannelHandleType;
-import iso.std.iso_iec._24727.tech.schema.EstablishContext;
-import iso.std.iso_iec._24727.tech.schema.GetRecognitionTreeResponse;
-import iso.std.iso_iec._24727.tech.schema.GetStatus;
-import iso.std.iso_iec._24727.tech.schema.PathSecurityType;
-import iso.std.iso_iec._24727.tech.schema.RecognitionTree;
-import iso.std.iso_iec._24727.tech.schema.Wait;
-
+import iso.std.iso_iec._24727.tech.schema.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.math.BigInteger;
-
+import java.nio.charset.Charset;
 import javax.xml.bind.JAXB;
-
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.openecard.client.ws.WSMarshaller;
 import org.w3c.dom.Document;
 
+
 /**
- * 
- * @author Dirk Petrautzki <petrautzki@hs-coburg.de>
  *
+ * @author Dirk Petrautzki <petrautzki@hs-coburg.de>
  */
 public class AndroidMarshallerTest {
-	private static final String getRecognitionTreeResponseXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?> <ns2:GetRecognitionTreeResponse xmlns=\"urn:oasis:names:tc:dss:1.0:core:schema\" 	xmlns:ns2=\"urn:iso:std:iso-iec:24727:tech:schema\" xmlns:ns3=\"http://www.w3.org/2000/09/xmldsig#\" 	xmlns:ns4=\"http://www.bsi.bund.de/ecard/api/1.1\" xmlns:ns5=\"http://ws.openecard.org/gui/v1.0\" 	xmlns:ns6=\"http://uri.etsi.org/02231/v3.1.2#\" xmlns:ns7=\"http://uri.etsi.org/02231/v2.1.1#\" 	xmlns:ns8=\"http://uri.etsi.org/02231/v2.x#\" xmlns:ns9=\"http://www.setcce.org/schemas/ers\" 	Profile=\"http://www.bsi.bund.de/ecard/api/1.1\"> 	<Result> 		<ResultMajor>http://www.bsi.bund.de/ecard/api/1.1/resultmajor#ok 		</ResultMajor> 	</Result> 	<ns2:RecognitionTree> 		<ns2:CardCall> 			<ns2:CommandAPDU>00A4000C023F00</ns2:CommandAPDU> 			<ns2:ResponseAPDU> 				<ns2:Trailer>9000</ns2:Trailer> 				<ns2:Conclusion> 					<ns2:CardCall> 						<ns2:CommandAPDU>00A4020C022F00</ns2:CommandAPDU> 						<ns2:ResponseAPDU> 							<ns2:Trailer>9000</ns2:Trailer> 							<ns2:Conclusion> 								<ns2:CardCall> 									<ns2:CommandAPDU>00B20404FF</ns2:CommandAPDU> 									<ns2:ResponseAPDU> 										<ns2:Body> 											<ns2:MatchingData> 												<ns2:Offset>00</ns2:Offset> 												<ns2:Length>3B</ns2:Length> 												<ns2:MatchingValue>61394F08D0400000170013015015476577C3B6686E6C69636865205369676E6174757251023F057312300804063F003F055031A00604043F005032 												</ns2:MatchingValue> 											</ns2:MatchingData> 										</ns2:Body> 										<ns2:Trailer>6282</ns2:Trailer> 										<ns2:Conclusion> 											<ns2:RecognizedCardType>http://cif.chipkarte.at/e-card/g3 											</ns2:RecognizedCardType> 										</ns2:Conclusion> 									</ns2:ResponseAPDU> 									<ns2:ResponseAPDU> 										<ns2:Body> 											<ns2:Tag>61</ns2:Tag> 											<ns2:MatchingData> 												<ns2:Offset>00</ns2:Offset> 												<ns2:Length>11</ns2:Length> 												<ns2:MatchingValue>4F0FE828BD080FA000000167455349474E 												</ns2:MatchingValue> 											</ns2:MatchingData> 										</ns2:Body> 										<ns2:Trailer>6282</ns2:Trailer> 										<ns2:Conclusion> 											<ns2:RecognizedCardType>http://ws.gematik.de/egk/1.0.0 											</ns2:RecognizedCardType> 										</ns2:Conclusion> 									</ns2:ResponseAPDU> 								</ns2:CardCall> 							</ns2:Conclusion> 						</ns2:ResponseAPDU> 					</ns2:CardCall> 				</ns2:Conclusion> 			</ns2:ResponseAPDU> 		</ns2:CardCall> 		<ns2:CardCall> 			<ns2:CommandAPDU>00A4020C022F00</ns2:CommandAPDU> 			<ns2:ResponseAPDU> 				<ns2:Trailer>9000</ns2:Trailer> 				<ns2:Conclusion> 					<ns2:CardCall> 						<ns2:CommandAPDU>00B000005A</ns2:CommandAPDU> 						<ns2:ResponseAPDU> 							<ns2:Body> 								<ns2:MatchingData> 									<ns2:Offset>00</ns2:Offset> 									<ns2:Length>5A</ns2:Length> 									<ns2:MatchingValue>61324F0FE828BD080FA000000167455349474E500F434941207A752044462E655369676E5100730C4F0AA000000167455349474E61094F07A0000002471001610B4F09E80704007F00070302610C4F0AA000000167455349474E 									</ns2:MatchingValue> 								</ns2:MatchingData> 							</ns2:Body> 							<ns2:Trailer>9000</ns2:Trailer> 							<ns2:Conclusion> 								<ns2:RecognizedCardType>http://bsi.bund.de/cif/npa.xml 								</ns2:RecognizedCardType> 							</ns2:Conclusion> 						</ns2:ResponseAPDU> 					</ns2:CardCall> </ns2:Conclusion> 			</ns2:ResponseAPDU> 		</ns2:CardCall> 	</ns2:RecognitionTree> </ns2:GetRecognitionTreeResponse>";
 
-	private static final String establishContextXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\r\n"
-			+ "<iso:EstablishContext xmlns:iso=\"urn:iso:std:iso-iec:24727:tech:schema\"/>";
+    private static final String getRecognitionTreeResponseXML;
+    private static final String establishContextXML;
+    private static final String getStatusResponse;
 
-	private static final String getStatusResponse  = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><ns3:GetStatusResponse Profile=\"http://www.bsi.bund.de/ecard/api/1.1\" xmlns:ns2=\"http://www.w3.org/2000/09/xmldsig#\" xmlns=\"urn:oasis:names:tc:dss:1.0:core:schema\" xmlns:ns4=\"http://www.bsi.bund.de/ecard/api/1.1\" xmlns:ns3=\"urn:iso:std:iso-iec:24727:tech:schema\" xmlns:ns9=\"http://uri.etsi.org/02231/v2.x#\" xmlns:ns5=\"http://ws.openecard.org/gui/v1.0\" xmlns:ns6=\"http://www.setcce.org/schemas/ers\" xmlns:ns7=\"http://uri.etsi.org/02231/v3.1.2#\" xmlns:ns8=\"http://uri.etsi.org/02231/v2.1.1#\"><Result><ResultMajor>http://www.bsi.bund.de/ecard/api/1.1/resultmajor#ok</ResultMajor></Result><ns3:IFDStatus><ns3:IFDName>REINER SCT cyberJack RFID basis 0</ns3:IFDName><ns3:Connected>true</ns3:Connected><ns3:SlotStatus><ns3:Index>0</ns3:Index><ns3:CardAvailable>false</ns3:CardAvailable></ns3:SlotStatus></ns3:IFDStatus><ns3:IFDStatus><ns3:IFDName>RIM BlackBerry Smart Card Reader 0</ns3:IFDName><ns3:Connected>true</ns3:Connected><ns3:SlotStatus><ns3:Index>0</ns3:Index><ns3:CardAvailable>false</ns3:CardAvailable></ns3:SlotStatus></ns3:IFDStatus></ns3:GetStatusResponse>";
-
-	  
-	  
-	@Test
-	public void testConversionOfEstablishContext() throws Exception {
-		WSMarshaller m = new AndroidMarshaller();
-		EstablishContext establishContext = new EstablishContext();
-		Document d = m.marshal(establishContext);
-		String s = m.doc2str(d);
-
-		Assert.assertEquals(s.trim(), establishContextXML.trim());
-
-		Object o = m.unmarshal(d);
-		if (!(o instanceof EstablishContext))
-			throw new Exception(
-					"Object should be an instace of EstablishContext");
-
-		
+    static {
+	try {
+	    getRecognitionTreeResponseXML = loadXML("GetRecognitionTreeResponse.xml");
+	    establishContextXML = loadXML("EstablishContext.xml");
+	    getStatusResponse = loadXML("GetStatusResponse.xml");
+	} catch (IOException ex) {
+	    throw new RuntimeException(ex);
 	}
-	
-	@Test
-	public void testConversionOfGetRecognitionTreeResponse() throws Exception {
-		WSMarshaller m = new AndroidMarshaller();
-		
-		Object o = m.unmarshal(m.str2doc(getRecognitionTreeResponseXML));
+    }
+
+    private static String loadXML(String resourcePath) throws IOException {
+	InputStream in = AndroidMarshallerTest.class.getClassLoader().getResourceAsStream(resourcePath);
+	StringWriter w = new StringWriter();
+	BufferedReader r = new BufferedReader(new InputStreamReader(in, Charset.forName("utf-8")));
+	String nextLine;
+	while ((nextLine = r.readLine()) != null) {
+	    w.write(nextLine);
+	    w.write(String.format("%n")); // platform dependant newline character
+	}
+	return w.toString();
+    }
+
+
+    @Test
+    public void testConversionOfEstablishContext() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	EstablishContext establishContext = new EstablishContext();
+	Document d = m.marshal(establishContext);
+	String s = m.doc2str(d);
+
+	Assert.assertEquals(s.trim(), establishContextXML.trim());
+
+	Object o = m.unmarshal(d);
+	if (!(o instanceof EstablishContext)) {
+	    throw new Exception("Object should be an instace of EstablishContext");
+	}
+    }
+
+
+    @Test
+    public void testConversionOfGetRecognitionTreeResponse() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+
+	Object o = m.unmarshal(m.str2doc(getRecognitionTreeResponseXML));
     	if(o instanceof GetRecognitionTreeResponse) {
-    		RecognitionTree tree =  ((GetRecognitionTreeResponse) o).getRecognitionTree();
-    		StringWriter sw = new StringWriter();
-    		
-    		JAXB.marshal(tree, sw);
-    		
-    		
-    		
-    	} else 
-    		throw new Exception(
-					"Object should be an instace of GetRecognitionTreeResponse");
+	    RecognitionTree tree =  ((GetRecognitionTreeResponse) o).getRecognitionTree();
+	    StringWriter sw = new StringWriter();
 
+	    JAXB.marshal(tree, sw);
+    	} else {
+	    throw new Exception("Object should be an instace of GetRecognitionTreeResponse");
 	}
-	
-	@Test
-	public void testConversionOfGetStatus() throws Exception {
-		WSMarshaller m = new AndroidMarshaller();
-		GetStatus getStatus = new GetStatus();
-		getStatus.setIFDName("ifdName");
-		getStatus.setContextHandle(new byte[] {0x0, 0x1, 0x2});
-		
-		Document d = m.marshal(getStatus);
-		
-		String s = m.doc2str(d);
-System.out.println(s);	
+    }
 
-	}
-	
-	
-	@Test
-	public void testConversionOfGetStatusResponse() throws Exception {
-		WSMarshaller m = new AndroidMarshaller();
-		m.unmarshal(m.str2doc(getStatusResponse));
-		
-		
-	}
-	
-	
-	@Test
-	public void testConversionOfWait() throws Exception {
-		WSMarshaller m = new AndroidMarshaller();
-		Wait w = new Wait();
-		w.setContextHandle(new byte[] {0x0, 0x1, 0x2} );
-		w.setTimeOut(new BigInteger("123"));
-		ChannelHandleType channelHandleType = new ChannelHandleType();
-		channelHandleType.setBinding("binding");
-		channelHandleType.setProtocolTerminationPoint("protocolterminatiopoint");
-		channelHandleType.setSessionIdentifier("sessionidentifier");
-		PathSecurityType pathSecurityType = new PathSecurityType();
-		pathSecurityType.setParameters("omg");
-		pathSecurityType.setProtocol("protocol");
-		channelHandleType.setPathSecurity(pathSecurityType);
-		w.setCallback(channelHandleType);
-		
-		Document d = m.marshal(w);
-		
-		String s = m.doc2str(d);
+
+    @Test
+    public void testConversionOfGetStatus() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	GetStatus getStatus = new GetStatus();
+	getStatus.setIFDName("ifdName");
+	getStatus.setContextHandle(new byte[] {0x0, 0x1, 0x2});
+
+	Document d = m.marshal(getStatus);
+
+	String s = m.doc2str(d);
 	System.out.println(s);
+    }
 
-	}
-	
-	
+
+    @Test
+    public void testConversionOfGetStatusResponse() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	m.unmarshal(m.str2doc(getStatusResponse));
+    }
+
+
+    @Test
+    public void testConversionOfWait() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	Wait w = new Wait();
+	w.setContextHandle(new byte[] {0x0, 0x1, 0x2} );
+	w.setTimeOut(new BigInteger("123"));
+	ChannelHandleType channelHandleType = new ChannelHandleType();
+	channelHandleType.setBinding("binding");
+	channelHandleType.setProtocolTerminationPoint("protocolterminatiopoint");
+	channelHandleType.setSessionIdentifier("sessionidentifier");
+	PathSecurityType pathSecurityType = new PathSecurityType();
+	pathSecurityType.setParameters("omg");
+	pathSecurityType.setProtocol("protocol");
+	channelHandleType.setPathSecurity(pathSecurityType);
+	w.setCallback(channelHandleType);
+
+	Document d = m.marshal(w);
+
+	String s = m.doc2str(d);
+	System.out.println(s);
+    }
 
 }

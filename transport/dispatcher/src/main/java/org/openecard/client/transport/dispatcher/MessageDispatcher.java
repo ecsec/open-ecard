7d403d48777ba6f512845371ee9914e5b3f08a6d
Tobias Wich <tobias.wich@ecsec.de>
2012-01-26 10:23:18 +0100

    rewrite dispatcher to depend on Dispatchable annotations in the environment

--- transport/dispatcher/src/main/java/org/openecard/client/transport/dispatcher/MessageDispatcher.java
+++ transport/dispatcher/src/main/java/org/openecard/client/transport/dispatcher/MessageDispatcher.java
@@ -1,15 +1,27 @@
+/*
+ * Copyright 2012 Tobias Wich ecsec GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecard.client.transport.dispatcher;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.openecard.client.common.interfaces.Dispatchable;
 import org.openecard.client.common.interfaces.Dispatcher;
 import org.openecard.client.common.interfaces.Environment;
 import org.openecard.client.common.logging.LogManager;
@@ -31,8 +43,9 @@ public class MessageDispatcher implements Dispatcher {
 
     public MessageDispatcher(Environment environment) {
 	this.environment = environment;
-	serviceMap = loadServiceDefinition();
-	serviceInstMap = loadEnvDefinition();
+	serviceMap = new TreeMap<String, Service>();
+	serviceInstMap = new TreeMap<String, Method>();
+	initDefinitions();
     }
 
 
@@ -62,97 +75,41 @@ public class MessageDispatcher implements Dispatcher {
     }
 
 
-    private static TreeMap<String,Service> loadServiceDefinition() {
-	// get properties
-	String packagePrefix = DispatcherProperties.getProperty("org.openecard.client.transport.dispatcher.pkg-prefix");
-	String interfaceStr = DispatcherProperties.getProperty("org.openecard.client.transport.dispatcher.interfaces");
-	// split input into class names or package names with classes
-	Pattern p = Pattern.compile("([A-Za-z_]{1}[A-Za-z0-9_]*(\\.[A-Za-z_]{1}[A-Za-z0-9_]*)*)");
-	Matcher matcher = p.matcher(interfaceStr);
+    private void initDefinitions() {
+	// load all annotated service methods from environment
+	Class envClass = this.environment.getClass();
+	Method[] envMethods = envClass.getMethods();
 
-	// check if the classes can be loaded
-	ArrayList<String> classNames = new ArrayList<String>();
-	while (matcher.find()) {
-	    String nextMatch = matcher.group();
-	    String nextWithPrefix = packagePrefix + "." + nextMatch;
-	    // determine the type of the name
-	    if (isClass(nextMatch) && isWebService(nextMatch)) {
-		classNames.add(nextMatch);
-	    } else if (isClass(nextWithPrefix) && isWebService(nextWithPrefix)) {
-		classNames.add(nextWithPrefix);
-	    } else {
-		_logger.log(Level.WARNING, "Omitting interface with name {0}, because it could not be loaded or is not a Webservice interface.", new Object[]{nextMatch});
-	    }
-	}
+	// loop over methods and build index structure
+	for (Method nextAccessor : envMethods) {
+	    // is the method annotated?
+	    if (nextAccessor.isAnnotationPresent(Dispatchable.class)) {
+		// try to read class from annotation, if not take return value
+		Dispatchable methodAnnotation = nextAccessor.getAnnotation(Dispatchable.class);
+		Class returnType = methodAnnotation.interfaceClass();
+		if (returnType == null) {
+		    returnType = nextAccessor.getReturnType();
+		}
 
-	// loadServiceDefinition methods and create pairing
-	TreeMap<String,Service> serviceMap = new TreeMap<String, Service>();
-	for (String next : classNames) {
-	    Class c;
-	    try {
-		c = MessageDispatcher.class.getClassLoader().loadClass(next);
-		Service s = new Service(c);
-		List<Class> reqClasses = s.getRequestClasses();
-		for (Class cl : reqClasses) {
-		    if (serviceMap.containsKey(cl.getName())) {
-			_logger.log(Level.WARNING, "Omitting method with parameter type {0} in service interface {1} because its type already associated with another service.", new Object[]{cl.getName(), c.getName()});
+		// check if the service is already defined
+		if (this.serviceInstMap.containsKey(returnType.getName())) {
+		    _logger.log(Level.WARNING, "Omitting service with type {0} because its type already associated with another service.", returnType.getName());
+		    continue;
+		}
+		// add env method mapping
+		this.serviceInstMap.put(returnType.getName(), nextAccessor);
+
+		// create service and map its request parameters
+		Service service = new Service(returnType);
+		for (Class reqClass : service.getRequestClasses()) {
+		    if (serviceMap.containsKey(reqClass.getName())) {
+			_logger.log(Level.WARNING, "Omitting method with parameter type {0} in service interface {1} because its type already associated with another service.", new Object[]{reqClass.getName(), returnType.getName()});
 		    } else {
-			serviceMap.put(cl.getName(), s);
+			serviceMap.put(reqClass.getName(), service);
 		    }
 		}
-	    } catch (ClassNotFoundException ex) {
 	    }
 	}
-
-	return serviceMap;
-    }
-
-    private static boolean isClass(String name) {
-	try {
-	    // try to loadServiceDefinition class, if that fails it may lack a package name
-	    MessageDispatcher.class.getClassLoader().loadClass(name);
-	} catch (ClassNotFoundException ex) {
-	    return false;
-	}
-	return true;
-    }
-
-    private static boolean isWebService(String className) {
-	try {
-	    // loadServiceDefinition class
-	    Class clazz = MessageDispatcher.class.getClassLoader().loadClass(className);
-	    // check if it is an interface
-	    if (! clazz.isInterface()) {
-		return false;
-	    }
-	    // check annotation for Webservice
-	    if (! clazz.isAnnotationPresent(javax.jws.WebService.class)) {
-		return false;
-	    }
-	} catch (ClassNotFoundException ex) {
-	    return false;
-	}
-	return true;
-    }
-
-
-    private static TreeMap<String,Method> loadEnvDefinition() {
-	Method[] allMethods = Environment.class.getMethods();
-	TreeMap<String,Method> methods = new TreeMap<String,Method>();
-	// filter methods
-	for (Method m : allMethods) {
-	    int modifiers = m.getModifiers();
-	    // conditions that make the code ignore the parameter
-	    if (  Modifier.isStatic(modifiers))      continue;
-	    if (! Modifier.isPublic(modifiers))      continue;
-	    if (  m.getParameterTypes().length != 0) continue;
-	    if (! m.getReturnType().isInterface())   continue;
-	    if (! m.getReturnType().isAnnotationPresent(javax.jws.WebService.class)) continue;
-	    // seems we have a good one
-	    methods.put(m.getReturnType().getName(), m);
-	}
-
-	return methods;
     }
 
 }
--- transport/dispatcher/src/main/java/org/openecard/client/transport/dispatcher/Service.java
+++ transport/dispatcher/src/main/java/org/openecard/client/transport/dispatcher/Service.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 Tobias Wich ecsec GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.openecard.client.transport.dispatcher;
 
 import java.lang.reflect.InvocationTargetException;
@@ -33,7 +49,7 @@ public class Service {
 	    if (isReqParam(m)) {
 		Class reqClass = getReqParamClass(m);
 		if (requestMethods.containsKey(reqClass.getName())) {
-		    _logger.log(Level.WARNING, "Omitting method {0} in srvice interface {1} because its parameter type is already associated with another method.", new Object[]{m.getName(), iface.getName()});
+		    _logger.log(Level.WARNING, "Omitting method {0} in service interface {1} because its parameter type is already associated with another method.", new Object[]{m.getName(), iface.getName()});
 		} else {
 		    requestClasses.add(reqClass);
 		    requestMethods.put(reqClass.getName(), m);

d2efd033db3ebb922902880da75e15c1bfa5aebd
Tobias Wich <tobias.wich@ecsec.de>
2012-03-16 16:15:18 +0100

    change LogManager to support different logging configurations

--- common/src/main/java/org/openecard/client/common/logging/LogManager.java
+++ common/src/main/java/org/openecard/client/common/logging/LogManager.java
@@ -6,33 +6,62 @@ import java.io.InputStream;
 
 
 /**
+ * Shallow LogManager which can help set up the logging configuration<br/>
+ * This class is mostly irrelevant when using java.util.logging, however it makes it easy to switch implementations
+ * like log4j etc. (provided someone wants to do such a foolish thing ;-).
  *
  * @author Johannes Schmoelz <johannes.schmoelz@ecsec.de>
+ * @author Tobias Wich <tobias.wich@ecsec.de>
  */
 public class LogManager {
     
-    static {
-        setup();
+    public static final String openecardPath = System.getProperty("user.home") + File.separator + ".openecard";
+    public static final String openecardConfFileName = "logging.properties";
+
+
+    public static void createHomeLogPath(String dirName) {
+	File f = new File(System.getProperty("user.home") + File.separator + dirName);
+	createLogPath(f);
     }
-    
-    private static void setup() {
-        try {
-            InputStream is = LogManager.class.getResourceAsStream("/logging.properties");
-            if (is != null) {
-                java.util.logging.LogManager.getLogManager().readConfiguration(is);
-                File f = new File(System.getProperty("user.home") + File.separator + ".ecsec");
-                if (! f.exists()) {
-                    f.mkdir();
-                }
-            }
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-        } catch (SecurityException ex) {
-            System.err.println(ex.getMessage());
-        }
+    public static void createLogPath(File path) {
+	if (! path.exists()) {
+	    path.mkdirs();
+	}
+    }
+
+    public static boolean loadOpeneCardDefaultConfig() {
+	createLogPath(new File(openecardPath));
+	return loadConfigFromResource("openecard_config/" + openecardConfFileName);
+    }
+
+    public static boolean loadConfigFromResource(String resourceName) {
+	InputStream s = LogManager.class.getResourceAsStream(resourceName);
+	// try loading with leading /
+	if (s == null && !resourceName.startsWith("/")) {
+	    s = LogManager.class.getResourceAsStream("/" + resourceName);
+	}
+	// check again
+	if (s == null) {
+	    System.err.println("ERROR: The logging config file '" + resourceName + "' could not be found in classpath.");
+	    return false;
+	} else {
+	    return loadConfig(s);
+	}
+    }
+
+    public static boolean loadConfig(InputStream config) {
+	try {
+	    java.util.logging.LogManager.getLogManager().readConfiguration(config);
+	    return true;
+	} catch (IOException ex) {
+	    System.err.println("ERROR: Unable to read new log configuration.");
+	    ex.printStackTrace(System.err);
+	    return false;
+	}
     }
     
     public static java.util.logging.Logger getLogger(String name) {
         return java.util.logging.Logger.getLogger(name);
     }
+
 }

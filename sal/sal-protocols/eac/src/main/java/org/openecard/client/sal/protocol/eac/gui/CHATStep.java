03fc7b817e9e7995ecebea56cfb2cbbb63afdd15
Moritz Horsch <horsch@cdc.informatik.tu-darmstadt.de>
2012-05-15 08:14:37 +0200

    Update EAC

---
+++ sal/sal-protocols/eac/src/main/java/org/openecard/client/sal/protocol/eac/gui/CHATStep.java
@@ -0,0 +1,65 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.openecard.client.sal.protocol.eac.gui;
+
+import java.util.Map;
+import java.util.TreeMap;
+import org.openecard.client.common.I18n;
+import org.openecard.client.crypto.common.asn1.cvc.CHAT;
+import org.openecard.client.crypto.common.asn1.cvc.CardVerifiableCertificate;
+import org.openecard.client.crypto.common.asn1.cvc.CertificateDescription;
+import org.openecard.client.gui.definition.BoxItem;
+import org.openecard.client.gui.definition.Checkbox;
+import org.openecard.client.gui.definition.Step;
+
+/**
+ *
+ * @author John
+ */
+public class CHATStep {
+
+    // GUI translation constants
+    private static final String TITLE = "service_providers_statements_title";
+    private static final String DATA_GROUP_PREFIX = "";
+    private static final String SUBJECT_URL = "service_providers_internetaddress";
+    private static final String TERMS_OF_USAGE = "service_providers_termsofusage";
+    private static final String VALIDITY = "service_providers_validity";
+    private static final String VALIDITY_FORMAT = "service_providers_validity";
+    private static final String VALIDITY_FROM = "service_providers_validity";
+    private static final String VALIDITY_TO = "service_providers_validity";
+    private static final String ISSUER_NAME = "service_providers_validity";
+    private static final String ISSUER_URL = "service_providers_validity";
+    //
+    private I18n lang = I18n.getTranslation("sal");
+    private Step step = new Step(lang.translationForKey(TITLE));
+    private CHAT requiredCHAT, optionalCHAT;
+    private ContentMap content;
+
+    public CHATStep(ContentMap content) {
+	this.requiredCHAT = (CHAT) content.get(ContentMap.ELEMENT.REQUIRED_CHAT);
+	this.optionalCHAT = (CHAT) content.get(ContentMap.ELEMENT.OPTIONAL_CHAT);;
+    }
+
+    public Step create() {
+
+	Checkbox readAccessCheckBox = new Checkbox();
+	TreeMap<CHAT.DataGroup, Boolean> readAccess = requiredCHAT.getReadAccess();
+	for (Map.Entry<CHAT.DataGroup, Boolean> entry : readAccess.entrySet()) {
+	    CHAT.DataGroup dataGroup = entry.getKey();
+	    Boolean isRequired = entry.getValue();
+
+	    if (isRequired) {
+		BoxItem item = new BoxItem();
+		item.setName(dataGroup.name());
+		item.setChecked(true);
+		item.setText(lang.translationForKey(DATA_GROUP_PREFIX + dataGroup.name()));
+
+		readAccessCheckBox.getBoxItems().add(item);
+	    }
+	}
+
+	return step;
+    }
+}

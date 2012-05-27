d2c1c397ba8e7a1d26c2edf2cc8d535f22997f21
Moritz Horsch <horsch@cdc.informatik.tu-darmstadt.de>
2012-05-27 13:46:47 +0200

    Ignore RichClientTest, else the unit test fails (this is an integration/debug test).

--- clients/richclient/src/test/java/org/openecard/client/richclient/RichClientTest.java
+++ clients/richclient/src/test/java/org/openecard/client/richclient/RichClientTest.java
@@ -1,3 +1,24 @@
+/****************************************************************************
+ * Copyright (C) 2012 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and ecsec.
+ *
+ ***************************************************************************/
+
 package org.openecard.client.richclient;
 
 import java.util.List;
@@ -5,6 +26,7 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import static org.junit.Assert.*;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openecard.client.common.logging.LogManager;
 import org.openecard.client.ifd.scio.wrapper.SCChannel;
@@ -22,17 +44,15 @@ import org.slf4j.LoggerFactory;
 
 
 /**
+ *
  * @author Moritz Horsch <horsch@cdc.informatik.tu-darmstadt.de>
  */
-//@Ignore
+@Ignore
 public class RichClientTest {
 
     private static final Logger logger = LoggerFactory.getLogger(RichClient.class.getName());
     private static List<TCToken> tokens;
     private static String tokenURI = "https://willow.mtg.de/eid-server-demo-app/result/request.html";
-
-    public RichClientTest() {
-    }
 
     @Before
     public void setUp() {
@@ -99,4 +119,5 @@ public class RichClientTest {
 	    fail(e.getMessage());
 	}
     }
+
 }

d860509a9d8e02aa91d3406a22da90712470361a
Tobias Wich <tobias.wich@ecsec.de>
2012-06-06 14:37:21 +0200

    Make RecognitionException based on ECardException.

--- recognition/src/main/java/org/openecard/client/recognition/RecognitionException.java
+++ recognition/src/main/java/org/openecard/client/recognition/RecognitionException.java
@@ -1,52 +1,51 @@
-/*
- * Copyright 2012 Tobias Wich ecsec GmbH
+/****************************************************************************
+ * Copyright (C) 2012 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of the Open eCard App.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
 
 package org.openecard.client.recognition;
 
 import oasis.names.tc.dss._1_0.core.schema.Result;
-import org.openecard.client.common.ECardConstants;
+import org.openecard.client.common.ECardException;
 
 
 /**
  *
  * @author Tobias Wich <tobias.wich@ecsec.de>
  */
-public class RecognitionException extends Exception {
+public class RecognitionException extends ECardException {
 
-    private final String major;
-    private final String minor;
-    private final String message;
-    private final String lang;
+    public RecognitionException(String msg) {
+	makeException(this, msg);
+    }
+
+    public RecognitionException(String minor, String msg) {
+	makeException(this, minor, msg);
+    }
 
     public RecognitionException(Result r) {
-	major = r.getResultMajor();
-	if (r.getResultMinor() != null) {
-	    minor = r.getResultMinor();
-	    if (r.getResultMessage() != null) {
-		message = r.getResultMessage().getValue();
-		lang = r.getResultMessage().getLang();
-	    } else {
-		message = "Unknown error";
-		lang = "EN";
-	    }
-	} else {
-	    minor = ECardConstants.Minor.App.UNKNOWN_ERROR;
-	    message = "Unknown error";
-	    lang = "EN";
-	}
+	makeException(this, r);
+    }
+
+    public RecognitionException(Throwable cause) {
+	makeException(this, cause);
     }
 
 }

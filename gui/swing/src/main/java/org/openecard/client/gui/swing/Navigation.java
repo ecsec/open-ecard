93a56be91129807a16edd4cb657f586ada0bfd4d
Tobias Wich <tobias.wich@ecsec.de>
2012-06-20 17:13:23 +0200

    Set new title for javadoc.

--- gui/swing/src/main/java/org/openecard/client/gui/swing/Navigation.java
+++ gui/swing/src/main/java/org/openecard/client/gui/swing/Navigation.java
@@ -1,3 +1,25 @@
+/****************************************************************************
+ * Copyright (C) 2012 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
 package org.openecard.client.gui.swing;
 
 import java.awt.event.ActionEvent;
@@ -16,6 +38,7 @@ import org.slf4j.LoggerFactory;
 
 
 /**
+ *
  * @author Moritz Horsch <horsch@cdc.informatik.tu-darmstadt.de>
  */
 public class Navigation extends JPanel implements ActionListener {
@@ -85,8 +108,7 @@ public class Navigation extends JPanel implements ActionListener {
 
 	for (Iterator<ActionListener> it = listeners.iterator(); it.hasNext();) {
 	    ActionListener actionListener = it.next();
-	    actionListener.actionPerformed(
-		    new ActionEvent(steps.get(stepPointer), ActionEvent.ACTION_PERFORMED, command));
+	    actionListener.actionPerformed(new ActionEvent(steps.get(stepPointer), ActionEvent.ACTION_PERFORMED, command));
 	}
 
 	if (command.equals(GUIConstants.BUTTON_NEXT)) {
@@ -109,4 +131,5 @@ public class Navigation extends JPanel implements ActionListener {
 	    nextButton.setText(lang.translationForKey(GUIConstants.BUTTON_NEXT));
 	}
     }
+
 }

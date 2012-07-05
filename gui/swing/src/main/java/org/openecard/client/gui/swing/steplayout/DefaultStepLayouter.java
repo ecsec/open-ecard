bcc01e406c5b306bce6a292351ce5746cdbf65c6
Johannes Schmoelz <johannes.schmoelz@ecsec.de>
2012-07-05 11:03:14 +0200

    Add missing license headers and remove unused imports.

--- gui/swing/src/main/java/org/openecard/client/gui/swing/steplayout/DefaultStepLayouter.java
+++ gui/swing/src/main/java/org/openecard/client/gui/swing/steplayout/DefaultStepLayouter.java
@@ -1,10 +1,28 @@
+/****************************************************************************
+ * Copyright (C) 2012 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
 package org.openecard.client.gui.swing.steplayout;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.BorderFactory;
@@ -15,13 +33,8 @@ import org.openecard.client.gui.definition.InputInfoUnit;
 import org.openecard.client.gui.definition.PasswordField;
 import org.openecard.client.gui.definition.Radiobox;
 import org.openecard.client.gui.definition.TextField;
-import org.openecard.client.gui.swing.components.AbstractInput;
 import org.openecard.client.gui.swing.components.Checkbox;
-import org.openecard.client.gui.swing.components.Hyperlink;
-import org.openecard.client.gui.swing.components.Radiobutton;
-import org.openecard.client.gui.swing.components.StepComponent;
-import org.openecard.client.gui.swing.components.Text;
-import org.openecard.client.gui.swing.components.ToggleText;
+import org.openecard.client.gui.swing.components.*;
 
 
 /**

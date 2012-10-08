0e0d515aa49c8500ff0a8a9a565535cdb0fcf9f2
Dirk Petrautzki <petrautzki@hs-coburg.de>
2012-10-08 10:48:40 +0200

    Use the card images from the card recognition instead of own ones in CardInfoActivity
    Conflicts:
    	clients/android/src/main/java/org/openecard/client/android/activities/CardInfoActivity.java

--- clients/android/src/main/java/org/openecard/client/android/activities/CardInfoActivity.java
+++ clients/android/src/main/java/org/openecard/client/android/activities/CardInfoActivity.java
@@ -29,12 +29,21 @@ import org.openecard.client.android.RootHelper;
 import org.openecard.client.common.enums.EventType;
 import org.openecard.client.common.interfaces.EventCallback;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import iso.std.iso_iec._24727.tech.schema.ConnectionHandleType;
+import java.io.InputStream;
+import org.openecard.client.android.ApplicationContext;
+import org.openecard.client.android.R;
+import org.openecard.client.android.RootHelper;
+import org.openecard.client.common.enums.EventType;
+import org.openecard.client.common.interfaces.EventCallback;
+
 
 /**
  * This Activity shows information about the currently used ecard.
@@ -44,6 +53,8 @@ import android.widget.TextView;
 public class CardInfoActivity extends Activity implements EventCallback {
 
     private ApplicationContext applicationContext;
+    private ImageView imageView;
+    private TextView textCardType;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,12 +65,13 @@ public class CardInfoActivity extends Activity implements EventCallback {
 	setContentView(R.layout.cardinfo);
 
 	RootHelper.startPCSCD(getFilesDir());
-	
-	// register for events
+
+	// initialize and register for events
 	applicationContext = ((ApplicationContext) getApplicationContext());
 	applicationContext.initialize();
 	applicationContext.getEnv().getEventManager().registerAllEvents(this);
-
+	imageView = (ImageView) findViewById(R.id.imageView_card);
+	textCardType  = (TextView) findViewById(R.id.text_cardType);
 	Button b = (Button) findViewById(R.id.button_back);
 	b.setOnClickListener(new OnClickListener() {
 	    public void onClick(View v) {
@@ -81,48 +93,30 @@ public class CardInfoActivity extends Activity implements EventCallback {
 	if (eventType.equals(EventType.CARD_RECOGNIZED)) {
 	    if (eventData instanceof ConnectionHandleType) {
 		ConnectionHandleType ch = (ConnectionHandleType) eventData;
-		if (ch.getRecognitionInfo().getCardType().equals("http://bsi.bund.de/cif/npa.xml")) {
-		    runOnUiThread(new Runnable() {
-			public void run() {
-			    ImageView imageView = (ImageView) findViewById(R.id.imageView_card);
-			    imageView.setImageResource(R.drawable.npa);
-			    // imageView.invalidate();
-			    TextView textCardType = (TextView) findViewById(R.id.text_cardType);
-			    textCardType.setText("Personalausweis");
-			}
-		    });
-		} else if (ch.getRecognitionInfo().getCardType().equals("http://ws.gematik.de/egk/1.0.0")){
-		    runOnUiThread(new Runnable() {
-			public void run() {
-			    ImageView imageView = (ImageView) findViewById(R.id.imageView_card);
-			    imageView.setImageResource(R.drawable.egk);
-			    // imageView.invalidate();
-			    TextView textCardType = (TextView) findViewById(R.id.text_cardType);
-			    textCardType.setText("elektronische Gesundheitskarte");
-			}
-		    });   
-		} else {
-		    runOnUiThread(new Runnable() {
-			public void run() {
-			    ImageView imageView = (ImageView) findViewById(R.id.imageView_card);
-			    imageView.setImageResource(R.drawable.unknown_card);
-			    // imageView.invalidate();
-			    TextView textCardType = (TextView) findViewById(R.id.text_cardType);
-			    textCardType.setText("unbekannte Karte");
-			}
-		    });
-		}
+		InputStream is = applicationContext.getRecognition()
+			.getCardImage(ch.getRecognitionInfo().getCardType());
+		Drawable drawable = Drawable.createFromStream(is, null);
+		updateUI(drawable, ch.getRecognitionInfo().getCardType());
 	    }
 	} else if (eventType.equals(EventType.CARD_REMOVED)) {
-	    runOnUiThread(new Runnable() {
-		public void run() {
-		    ImageView imageView = (ImageView) findViewById(R.id.imageView_card);
-		    imageView.setImageResource(R.drawable.no_card);
-		    // imageView.invalidate();
-		    TextView textCardType = (TextView) findViewById(R.id.text_cardType);
-		    textCardType.setText(R.string.text_cardTye);
-		}
-	    });
+	    InputStream is = applicationContext.getRecognition().getNoCardImage();
+	    Drawable drawable = Drawable.createFromStream(is, null);
+	    updateUI(drawable, this.getString(R.string.text_cardTye));
 	}
     }
+
+    /**
+     * Updates the UI with the new cardImage and CardType.
+     * @param cardImage image to use
+     * @param cardType card type as string
+     */
+    private void updateUI(final Drawable cardImage, final String cardType) {
+	runOnUiThread(new Runnable() {
+	    public void run() {
+		imageView.setImageDrawable(cardImage);
+		textCardType.setText(cardType);
+	    }
+	});
+    }
+
 }

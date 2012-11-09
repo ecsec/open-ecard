ff5e4a9665f038360c2a230de0b10336f49b182a
Tobias Wich <tobias.wich@ecsec.de>
2012-11-09 15:34:47 +0100

    Checkstyle fix of SVG GraphicsUtil

--- gui/graphics/src/test/java/org/openecard/client/gui/graphics/GraphicsUtilTest.java
+++ gui/graphics/src/test/java/org/openecard/client/gui/graphics/GraphicsUtilTest.java
@@ -26,9 +26,10 @@ import java.awt.Image;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
+
 /**
  * Test of GraphicsUtil class.
- * 
+ *
  * @author Johannes Schmölz <johannes.schmoelz@ecsec.de>
  */
 public class GraphicsUtilTest {
@@ -38,65 +39,65 @@ public class GraphicsUtilTest {
      */
     @Test
     public void testCreateImage_3args() {
-        System.out.println("createImage");
-        
-        //
-        // valid values for imageWidth and imageHeight
-        //
-        
-        Image result = GraphicsUtil.createImage(OecLogoBgWhite.class, 200, 200);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgWhite.class, 150, 220);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgWhite.class, 370, 280);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 400, 400);
-        assertNotNull(result);
+	System.out.println("createImage");
 
-        result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 1, 1);
-        assertNotNull(result);
-        
-        //
-        // invalid values for imageWidth and/or imageHeight
-        //
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgWhite.class, 0, 0);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgWhite.class, -300, 0);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgWhite.class, 0, -150);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgTransparent.class, -100, 30);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgTransparent.class, 50, -70);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
+	//
+	// valid values for imageWidth and imageHeight
+	//
+
+	Image result = GraphicsUtil.createImage(OecLogoBgWhite.class, 200, 200);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgWhite.class, 150, 220);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgWhite.class, 370, 280);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 400, 400);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 1, 1);
+	assertNotNull(result);
+
+	//
+	// invalid values for imageWidth and/or imageHeight
+	//
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgWhite.class, 0, 0);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgWhite.class, -300, 0);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgWhite.class, 0, -150);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgTransparent.class, -100, 30);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgTransparent.class, 50, -70);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
     }
 
     /**
@@ -104,54 +105,55 @@ public class GraphicsUtilTest {
      */
     @Test
     public void testCreateImage_7args() {
-        System.out.println("createImage");
-        
-        //
-        // the image resides within the canvas; all values are valid
-        //
-        
-        Image result = GraphicsUtil.createImage(OecLogoBgWhite.class, 100, 100, 200, 200, 25, 25);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 150, 120, 260, 240, 0, 10);
-        assertNotNull(result);
-        
-        //
-        // the image doesn't reside completely within the canvas or it is beyond the canvas; all values are valid
-        //
-        
-        result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 200, 200, 100, 100, 0, 0);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgWhite.class, 200, 200, 400, 400, -300, -250);
-        assertNotNull(result);
-        
-        result = GraphicsUtil.createImage(OecLogoBgTransparent.class, -10, -5, 100, 100, 20, 20);
-        assertNotNull(result);
-        
-        //
-        // invalid values for canvasWidth and/or canvasHeight
-        //
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgTransparent.class, 50, 50, 0, 0, 0, 0);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgTransparent.class, -35, 17, -80, 0, 20, 20);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
-        
-        try {
-            GraphicsUtil.createImage(OecLogoBgWhite.class, 100, 120, 0, -1, 0, 0);
-            fail("IllegalArgumentException expected.");
-        } catch (IllegalArgumentException ex) {
-            // do nothing; exception expected
-        }
+	System.out.println("createImage");
+
+	//
+	// the image resides within the canvas; all values are valid
+	//
+
+	Image result = GraphicsUtil.createImage(OecLogoBgWhite.class, 100, 100, 200, 200, 25, 25);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 150, 120, 260, 240, 0, 10);
+	assertNotNull(result);
+
+	//
+	// the image doesn't reside completely within the canvas or it is beyond the canvas; all values are valid
+	//
+
+	result = GraphicsUtil.createImage(OecLogoBgTransparent.class, 200, 200, 100, 100, 0, 0);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgWhite.class, 200, 200, 400, 400, -300, -250);
+	assertNotNull(result);
+
+	result = GraphicsUtil.createImage(OecLogoBgTransparent.class, -10, -5, 100, 100, 20, 20);
+	assertNotNull(result);
+
+	//
+	// invalid values for canvasWidth and/or canvasHeight
+	//
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgTransparent.class, 50, 50, 0, 0, 0, 0);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgTransparent.class, -35, 17, -80, 0, 20, 20);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
+
+	try {
+	    GraphicsUtil.createImage(OecLogoBgWhite.class, 100, 120, 0, -1, 0, 0);
+	    fail("IllegalArgumentException expected.");
+	} catch (IllegalArgumentException ex) {
+	    // do nothing; exception expected
+	}
     }
+
 }

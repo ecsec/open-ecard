e9fee7446a765d6b98c07cbd3e90b741e67ff047
Tobias Wich <tobias.wich@ecsec.de>
2012-11-27 10:08:03 +0100

    Extent OverridingProperties with two files constructor

--- wsdef/wsdef-common/src/main/java/org/openecard/common/OverridingProperties.java
+++ wsdef/wsdef-common/src/main/java/org/openecard/common/OverridingProperties.java
@@ -29,7 +29,9 @@ import java.util.Properties;
 
 
 /**
- * Basic Properties class which overrides default values from System.properties on its creation.
+ * Basic Properties class which overrides default values from System properties on its creation.
+ * When looking up a key, it is first searched in the system properties, then in the properties structure instantiated
+ * in the constructor.
  *
  * @author Tobias Wich <tobias.wich@ecsec.de>
  */
@@ -37,6 +39,13 @@ public class OverridingProperties {
 
     private final Properties properties;
 
+    /**
+     * Loads properties from named resource.
+     * The resource must be located in the classpath.
+     *
+     * @param fName Name of the properties file.
+     * @throws IOException If loading of the resource failed.
+     */
     public OverridingProperties(String fName) throws IOException {
 	InputStream in = this.getClass().getResourceAsStream("/" + fName);
 	if (in == null) {
@@ -46,13 +55,49 @@ public class OverridingProperties {
 	properties.load(in);
 	init();
     }
+    /**
+     * Load properties from InputStream.
+     *
+     * @param stream Stream with Java properties format.
+     * @throws IOException If loading of the resource failed.
+     */
     public OverridingProperties(InputStream stream) throws IOException {
-	properties = new Properties();
-	properties.load(stream);
+	this(stream, null);
 	init();
     }
+    /**
+     * Load properties from property instance.
+     *
+     * @param props Properties instance.
+     */
     public OverridingProperties(Properties props) {
 	properties = new Properties(props);
+	init();
+    }
+    /**
+     * Load properties from InputStreams.
+     * The second properties stream takes precedence over the first one. This intended as a persistent default
+     * mechanism. The first stream may point to a bundled properties file, while the second can point to a file in the
+     * applications config directory, so that the user can alter the config values.
+     *
+     * @param bundledConf Stream to base properties.
+     * @param homeConf Stream to properties overriding the base properties. May be null.
+     * @throws IOException If loading of the base resource failed.
+     */
+    public OverridingProperties(InputStream bundledConf, InputStream homeConf) throws IOException {
+	Properties baseProps = new Properties();
+	baseProps.load(bundledConf);
+
+	try {
+	    if (homeConf != null) {
+		Properties homeProps = new Properties(baseProps);
+		homeProps.load(homeConf);
+		baseProps = homeProps;
+	    }
+	} catch (IOException ex) {
+	}
+
+	properties = baseProps;
 	init();
     }
 
@@ -72,22 +117,49 @@ public class OverridingProperties {
     }
 
 
+    /**
+     * Gets URL to resource reachable by the classloader of this class.
+     * This convenience method tries the path with / prepended and without.
+     *
+     * @param fname Path to the file to load.
+     * @return URL instance pointing to the resource. A stream can be opened afterwards.
+     */
     public URL getDependentResource(String fname) {
-	URL url = this.getClass().getResource("/" + fname);
+	URL url = getClass().getResource("/" + fname);
 	if (url == null) {
-	    url = this.getClass().getResource(fname);
+	    url = getClass().getResource(fname);
 	}
 	return url;
     }
 
+    /**
+     * Gets the value for the given property key.
+     *
+     * @see Properties#getProperty(java.lang.String)
+     * @param key
+     * @return The value, or null if none is found.
+     */
     public final String getProperty(String key) {
 	return properties.getProperty(key);
     }
 
+    /**
+     * Sets the value for a property key.
+     *
+     * @see Properties#setProperty(java.lang.String, java.lang.String)
+     * @param key The key to be placed into the property list.
+     * @param value The value corresponding to key.
+     * @return The previous value in the properties structure, or null if none was set.
+     */
     public final Object setProperty(String key, String value) {
 	return properties.setProperty(key, value);
     }
 
+    /**
+     * Gets a copy of all defined properties.
+     *
+     * @return Copy of the properties.
+     */
     public final Properties properties() {
 	return (Properties) properties.clone();
     }

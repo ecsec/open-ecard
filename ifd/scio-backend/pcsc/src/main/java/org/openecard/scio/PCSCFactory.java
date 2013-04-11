6ba039cf415bafd03b679025ae12785ba970f06a
Benedikt Biallowons <benedikt@biallowons.de>
2013-04-12 00:30:48 +0200

    Add OS X specific PC/SC JNI libs & SmartCardIO (fixes #214)
    This patch addresses multiple issues with the PC/SC JNI libraries on
    Mac OS X (10.6 - 10.8) and Java 6 & 7. Due to unsufficient support
    of 64bit JRE's and missed PCSC-Lite API changes, we chose to supply
    cleaned PC/SC JNI libraries and an adapted SmartCardIO.

--- ifd/scio-backend/pcsc/src/main/java/org/openecard/scio/PCSCFactory.java
+++ ifd/scio-backend/pcsc/src/main/java/org/openecard/scio/PCSCFactory.java
@@ -22,39 +22,54 @@
 
 package org.openecard.scio;
 
-import org.openecard.common.util.LinuxLibraryFinder;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.security.NoSuchAlgorithmException;
 import javax.smartcardio.CardTerminals;
 import javax.smartcardio.TerminalFactory;
+import org.openecard.common.util.LinuxLibraryFinder;
+import org.openecard.scio.osx.SunOSXPCSC;
 
 
 /**
  * Proxy and abstracted Factory for SCIO PC/SC driver.
  *
  * @author Tobias Wich <tobias.wich@ecsec.de>
+ * @author Benedikt Biallowons <benedikt.biallowons@ecsec.de>
  */
 public class PCSCFactory implements org.openecard.common.ifd.TerminalFactory {
 
+    private static final String ALGORITHM = "PC/SC";
+
+    private final TerminalFactory terminalFactory;
+
     /**
      * Default constructor with fixes for the faulty SmartcardIO library.
+     *
+     * @throws FileNotFoundException if pcsclite for Linux can't be found.
+     * @throws NoSuchAlgorithmException if no PC/SC provider can be found.
      */
-    public PCSCFactory() throws FileNotFoundException {
+    public PCSCFactory() throws FileNotFoundException, NoSuchAlgorithmException {
 	String osName = System.getProperty("os.name");
 	if (osName.startsWith("Linux")) {
 	    File libFile = LinuxLibraryFinder.getLibraryPath("pcsclite", "1");
 	    System.setProperty("sun.security.smartcardio.library", libFile.getAbsolutePath());
+	// see https://developer.apple.com/library/mac/technotes/tn2002/tn2110.html#FINDINGMAC
+	} else if (osName.contains("OS X")) {
+	    terminalFactory = TerminalFactory.getInstance(ALGORITHM, null, new SunOSXPCSC());
+	    return;
 	}
+	terminalFactory = TerminalFactory.getInstance(ALGORITHM, null);
     }
 
     @Override
     public String getType() {
-	return TerminalFactory.getDefault().getType();
+	return terminalFactory.getType();
     }
 
     @Override
     public CardTerminals terminals() {
-	return TerminalFactory.getDefault().terminals();
+	return terminalFactory.terminals();
     }
 
 }

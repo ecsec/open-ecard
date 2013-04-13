587e02878fe003701d1689a4a44182db116a88e8
Tobias Wich <tobias.wich@ecsec.de>
2013-04-13 16:17:57 +0200

    Create ExceptionUtils class with path checker function

---
+++ common/src/test/java/org/openecard/common/util/ExceptionUtilsTest.java
@@ -0,0 +1,50 @@
+/****************************************************************************
+ * Copyright (C) 2012 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
+package org.openecard.common.util;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.util.InvalidPropertiesFormatException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Test for the ExceptionUtils class.
+ *
+ * @author Tobias Wich <tobias.wich@ecsec.de>
+ */
+public class ExceptionUtilsTest {
+
+    @Test
+    public void testMatchPath() {
+	// create random exceptions, nest them and check if the path matches
+	Throwable root = new NullPointerException();
+	Throwable ex = new InvalidAlgorithmParameterException(root);
+	ex = new InvalidPropertiesFormatException(ex);
+	NullPointerException result = ExceptionUtils.matchPath(ex, NullPointerException.class,
+		InvalidAlgorithmParameterException.class);
+	Assert.assertNotNull(result);
+	Assert.assertEquals(result, root);
+    }
+
+}

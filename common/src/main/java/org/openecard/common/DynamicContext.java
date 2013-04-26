179aeabb679ef61ba5e6740670be4e4e3ae9b0ca
Tobias Wich <tobias.wich@ecsec.de>
2013-04-26 18:45:18 +0200

    Create Promise class and use it as container in DynamicContext

--- common/src/main/java/org/openecard/common/DynamicContext.java
+++ common/src/main/java/org/openecard/common/DynamicContext.java
@@ -24,11 +24,13 @@ package org.openecard.common;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import org.openecard.common.util.Promise;
 
 
-/**
+/**i
  * Thread local dynamic context implemented as a singleton.
  * Dynamic context information is needed at various places in the app. Perhaps the most important use case is the
  * eService certificate validation as defined in TR-03112-7.<br/>
@@ -38,21 +40,16 @@ import javax.annotation.Nullable;
  */
 public class DynamicContext {
 
-    private static final String prefix = "org.openecard.common.DynamicContext.";
-
-    public static final String ESERVICE_CERTIFICATE = prefix + "eservice_certificate";
-
-    private static InheritableThreadLocal<DynamicContext> local;
+    private static InheritableThreadLocal<Map<String, DynamicContext>> localMap;
     static {
-	// thread local which initializes a new instance per thread, copying the values from the parent thread
-	local = new InheritableThreadLocal<DynamicContext>() {
+	localMap = new InheritableThreadLocal<Map<String, DynamicContext>>() {
 	    @Override
-	    protected DynamicContext initialValue() {
-		return new DynamicContext();
+	    protected Map<String, DynamicContext> initialValue() {
+		return new ConcurrentSkipListMap<String, DynamicContext>();
 	    }
 	    @Override
-	    protected DynamicContext childValue(DynamicContext parentValue) {
-		return new DynamicContext(parentValue);
+	    protected Map<String, DynamicContext> childValue(Map<String, DynamicContext> parentValue) {
+		return parentValue;
 	    }
 	};
     }
@@ -64,33 +61,75 @@ public class DynamicContext {
      * @return The DynamicContext instance of this thread.
      */
     @Nonnull
-    public static DynamicContext getInstance() {
-	return local.get();
+    public static DynamicContext getInstance(@Nonnull String key) {
+	final Map<String, DynamicContext> local = localMap.get();
+	synchronized (local) {
+	    DynamicContext inst;
+	    if (local.containsKey(key)) {
+		inst = local.get(key);
+	    } else {
+		inst = new DynamicContext();
+		local.put(key, inst);
+	    }
+	    return inst;
+	}
     }
 
-    private final Map<String, Object> context;
+    /**
+     * Removes the value from this thread.
+     * This does not clear the values saved in the context, but makes the context inaccessible for further invocations
+     * of the {@link #getInstance()} method.
+     *
+     * @see ThreadLocal#remove()
+     */
+    public static void remove() {
+	localMap.remove();
+    }
+
+    private final Map<String, Promise<Object>> context;
 
     private DynamicContext() {
-	this.context = new HashMap<String, Object>();
-    }
-    private DynamicContext(DynamicContext parent) {
-	this.context = new HashMap<String, Object>(parent.context);
+	this.context = new HashMap<String, Promise<Object>>();
     }
 
 
     /**
+     * Gets the promise which represents the given key.
+     * If no promise exists yet, a new one will be created. Retrieving the promise value blocks until a value is
+     * delivered to it.
+     *
+     * @param key Key for which the promise should be retrieved.
+     * @return Promise for the given key.
+     */
+    public synchronized @Nonnull Promise<Object> getPromise(@Nonnull String key) {
+	Promise<Object> p = context.get(key);
+	if (p != null) {
+	    return p;
+	} else {
+	    p = new Promise<Object>();
+	    context.put(key, p);
+	    return p;
+	}
+    }
+
+    /**
      * Gets the value saved for the given key.
+     * The method does not block and returns null if no value is in the promise represented by thos key
      *
      * @see Map#get(java.lang.Object)
      * @param key Key for which the value should be retrieved.
      * @return The value for the given key, or {@code null} if no value is defined yet or {@code null} is mapped.
      */
     public @Nullable Object get(@Nonnull String key) {
-	return context.get(key);
+	Promise<Object> p = getPromise(key);
+	return p.derefNonblocking();
     }
 
     /**
      * Saves the given value for the given key.
+     * This method writes the value into the promise of the given key, or in case the promise already has a value,
+     * overwrites the promise and sets the value in the new instance. By doing that, the function mimics the exact
+     * behaviour of the Map class.
      *
      * @see Map#put(java.lang.Object, java.lang.Object)
      * @param key Key for which the value should be saved.
@@ -98,8 +137,14 @@ public class DynamicContext {
      * @return The previous value for key or {@code null} if there was no previous mapping or {@code null} was mapped.
      */
     @Nullable
-    public Object put(@Nonnull String key, @Nullable Object value) {
-	return context.put(key, value);
+    public synchronized Object put(@Nonnull String key, @Nullable Object value) {
+	Promise<Object> p = getPromise(key);
+	if (p.isDelivered()) {
+	    p = new Promise<Object>();
+	    context.put(key, p);
+	}
+	p.deliver(value);
+	return value;
     }
 
     /**
@@ -111,7 +156,11 @@ public class DynamicContext {
      */
     @Nullable
     public Object remove(@Nonnull String key) {
-	return context.remove(key);
+	Promise<Object> p = context.remove(key);
+	if (p != null) {
+	    return p.derefNonblocking();
+	}
+	return null;
     }
 
     /**
---
+++ common/src/main/java/org/openecard/common/util/Promise.java
@@ -0,0 +1,139 @@
+/****************************************************************************
+ * Copyright (C) 2013 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
+package org.openecard.common.util;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+
+/**
+ * Implementation of a promise inspired by clojure's promise.
+ * The implementation blocks until the value is set or a timeout occured.
+ *
+ * @see <tt><a href="http://clojuredocs.org/clojure_core/clojure.core/promise#source">clojure.core/promise</a></tt>
+ * @author Tobias Wich <tobias.wich@ecsec.de>
+ */
+public class Promise <T> {
+
+    private final CountDownLatch gate;
+
+    private boolean isSet;
+    private T value;
+
+    /**
+     * Creates an undelivered promise.
+     */
+    public Promise() {
+	gate = new CountDownLatch(1);
+	isSet = false;
+    }
+
+    /**
+     * Checks if the promise is already delivered.
+     *
+     * @return {@code true} if the promise is delivered, {@code false} otherwise.
+     */
+    public synchronized boolean isDelivered() {
+	return isSet;
+    }
+
+    /**
+     * Delivers the given value to the promise.
+     * The result of calling this function is that the promise is delivered and either contains the given value or a
+     * value which has been set previously. In the latter case an IllegalStateException is thrown. If that happens it is
+     * an indicator that the implementation using the promise is flawed.
+     *
+     * @param value Value to be delivered.
+     * @return The value that has been delivered.
+     * @throws IllegalStateException Thrown in case the promise is already delivered when this function gets called.
+     */
+    public synchronized @Nullable T deliver(@Nullable final T value) throws IllegalStateException {
+	if (! isSet) {
+	    this.value = value;
+	    this.isSet = true;
+	    this.gate.countDown();
+	    return this.value;
+	} else {
+	    throw new IllegalStateException("Failed to deliver promise, as it is already delivered.");
+	}
+    }
+
+    /**
+     * Dereferences the promise, aka tries to get its result.
+     * The function waits as long as the promise is not delivered yet.
+     *
+     * @return The value that has been delivered to the promise.
+     * @throws InterruptedException Thrown in case the current thread has been interrupted while waiting.
+     */
+    public @Nullable T deref() throws InterruptedException {
+	try {
+	    // wait an infinite time, most certainly longer than the machine running this program exists :p
+	    return deref(Long.MAX_VALUE, TimeUnit.DAYS);
+	} catch (TimeoutException ex) {
+	    // ignore the exception, as trhe timout is quasi forever
+	    return null;
+	}
+    }
+    /**
+     * Dereferences the promise, aka tries to get its result.
+     * The function waits as long as defined by the wait parameters. A timeout value of 0 indicates not to wait at all.
+     *
+     * @param timeout Value from 0 to {@link Long#MAX_VALUE} indicating the number of units to wait.
+     * @param unit The unit qualifying the timeout value.
+     * @return The value that has been delivered to the promise.
+     * @throws InterruptedException Thrown in case the current thread has been interrupted while waiting.
+     * @throws TimeoutException Thrown in case a timeout occured.
+     */
+    public @Nullable T deref(@Nonnegative long timeout, @Nonnull TimeUnit unit) throws InterruptedException,
+	    TimeoutException {
+	boolean delivered = gate.await(timeout, unit);
+	synchronized (this) {
+	    if (delivered) {
+		return this.value;
+	    } else {
+		throw new TimeoutException("Wait for promised value timed out.");
+	    }
+	}
+    }
+
+    /**
+     * Gets the value of the promise but does not wait for its delivery.
+     * This method is different than calling {@link #deref(long, java.util.concurrent.TimeUnit)} with a timeout value of
+     * 0. It does not throw a TimeoutException, but returns null when no value is delivered yet. This may be
+     * unambiguous with a delivered null value.
+     *
+     * @return The delivered value or {@code null} when no value has been deliverd yet.
+     */
+    public @Nullable T derefNonblocking() {
+	if (isDelivered()) {
+	    return value;
+	} else {
+	    return null;
+	}
+    }
+
+}

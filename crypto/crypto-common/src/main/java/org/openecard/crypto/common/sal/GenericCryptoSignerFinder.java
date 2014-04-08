005d2e0c1edddb31c54bf517713bf4a8725ad08e
Hans-Martin Haase <hans-martin.haase@ecsec.de>
2014-04-08 07:23:51 +0200

    Select a DID for TLS just if a readable certificate with Client authentication extension is referenced in the DID

--- crypto/crypto-common/src/main/java/org/openecard/crypto/common/sal/GenericCryptoSignerFinder.java
+++ crypto/crypto-common/src/main/java/org/openecard/crypto/common/sal/GenericCryptoSignerFinder.java
@@ -26,9 +26,13 @@ import iso.std.iso_iec._24727.tech.schema.ACLList;
 import iso.std.iso_iec._24727.tech.schema.ACLListResponse;
 import iso.std.iso_iec._24727.tech.schema.AccessRuleType;
 import iso.std.iso_iec._24727.tech.schema.ActionNameType;
+import iso.std.iso_iec._24727.tech.schema.CardApplicationConnect;
+import iso.std.iso_iec._24727.tech.schema.CardApplicationConnectResponse;
+import iso.std.iso_iec._24727.tech.schema.CardApplicationDisconnect;
 import iso.std.iso_iec._24727.tech.schema.CardApplicationList;
 import iso.std.iso_iec._24727.tech.schema.CardApplicationListResponse;
 import iso.std.iso_iec._24727.tech.schema.CardApplicationListResponse.CardApplicationNameList;
+import iso.std.iso_iec._24727.tech.schema.CardApplicationPathType;
 import iso.std.iso_iec._24727.tech.schema.CertificateRefType;
 import iso.std.iso_iec._24727.tech.schema.ConnectionHandleType;
 import iso.std.iso_iec._24727.tech.schema.DIDGet;
@@ -37,15 +41,26 @@ import iso.std.iso_iec._24727.tech.schema.DIDList;
 import iso.std.iso_iec._24727.tech.schema.DIDListResponse;
 import iso.std.iso_iec._24727.tech.schema.DIDQualifierType;
 import iso.std.iso_iec._24727.tech.schema.DIDScopeType;
+import iso.std.iso_iec._24727.tech.schema.DIDStructureType;
+import iso.std.iso_iec._24727.tech.schema.DSIRead;
+import iso.std.iso_iec._24727.tech.schema.DSIReadResponse;
+import iso.std.iso_iec._24727.tech.schema.DataSetSelect;
+import iso.std.iso_iec._24727.tech.schema.DataSetSelectResponse;
 import iso.std.iso_iec._24727.tech.schema.NamedDataServiceActionName;
 import iso.std.iso_iec._24727.tech.schema.TargetNameType;
+import java.io.ByteArrayInputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateParsingException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.openecard.bouncycastle.asn1.x509.Certificate;
 import org.openecard.bouncycastle.crypto.tls.CertificateRequest;
+import org.openecard.common.SecurityConditionUnsatisfiable;
 import org.openecard.common.WSHelper;
 import org.openecard.common.WSHelper.WSException;
 import org.openecard.common.interfaces.Dispatcher;
@@ -182,44 +197,39 @@ public class GenericCryptoSignerFinder {
 	    String algorithm = cryptoMarker.getAlgorithmInfo().getAlgorithmIdentifier().getAlgorithm();
 	    if (algorithm.equals(OID_PKCS_1_PURE_RSA_SIGNATURE_LEGACY)) {
 		logger.debug("{} is usable for TLSv1.1 and TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if(algorithm.equals(OID_PKCS_1_PURE_RSA_SIGNATURE)) {
 		logger.debug("{} is usable for TLSv1.1 and TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_PKCS_1_RSA_SHA1)) {
 		logger.debug("{} is usable for TLSv1.1 and TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_PKCS_1_RSA_SHA256)) {
 		logger.debug("{} is usable for TLSv1.1 and TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_PKCS_1_RSA_SHA224)) {
 		logger.debug("{} is usable for TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_PKCS_1_RSA_SHA384)) {
 		logger.debug("{} is usable for TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_PKCS_1_RSA_SHA512)) {
 		logger.debug("{} is usable for TLS1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_ECDSA_SHA1)) {
 		logger.debug("{} is usable for TLSv1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_ECDSA_SHA224)) {
 		logger.debug("{} is usable for TLSv1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_ECDSA_SHA256)) {
 		logger.debug("{} is usable for TLSv1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_ECDSA_SHA384)) {
 		logger.debug("{} is usable for TLSv1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else if (algorithm.equals(OID_ECDSA_SHA512)) {
 		logger.debug("{} is usable for TLSv1.2 signatures.", didName);
-		remainingDIDs.add(didName);
 	    } else {
 		logger.debug("{} is not usable for TLS signatures.", didName);
+		continue;
+	    }
+
+	    System.out.println("DIDName: " + didName);
+	    if (containsAuthenticationCertificate(dispatcher, handle, cryptoMarker)) {
+		remainingDIDs.add(didName);
 	    }
 	}
+
 	return remainingDIDs;
     }
 
@@ -291,6 +301,82 @@ public class GenericCryptoSignerFinder {
 	WSHelper.checkResult(didListResponse);
 	List<String> didNames = didListResponse.getDIDNameList().getDIDName();
 	return didNames;
+    }
+
+    /**
+     * The method reads the certificate, referenced in the DID, and checks whether the client authentication extension
+     * is set.
+     *
+     * @param dispatcher Dispatcher for delivering the command to the card.
+     * @param handle ConnectionHandle for identification of the terminal to use.
+     * @param cryptoMarker CryptoMarker object of the DID which contains the certificate to check.
+     * @return The value of the input parameter didName if the certificate referenced in the DID contains the client
+     * authentication extension. If the extension is not part of the certificate null is returned.
+     * @throws DispatcherException
+     * @throws InvocationTargetException
+     */
+    private boolean containsAuthenticationCertificate(Dispatcher dispatcher, ConnectionHandleType handle,
+	    CryptoMarkerType cryptoMarker) throws DispatcherException, InvocationTargetException {
+	boolean hasAuthCert = false;
+	// connent the application
+	CardApplicationConnect applicationConnect = new CardApplicationConnect();
+	CardApplicationPathType cardAppPath = new CardApplicationPathType();
+	cardAppPath.setCardApplication(handle.getCardApplication());
+	applicationConnect.setCardApplicationPath(cardAppPath);
+	CardApplicationConnectResponse connectResp = (CardApplicationConnectResponse) dispatcher.deliver(applicationConnect);
+
+	handle = connectResp.getConnectionHandle();
+
+	// resolve acls of the certificate data set
+	TargetNameType targetName = new TargetNameType();
+	targetName.setDataSetName(cryptoMarker.getCertificateRef().getDataSetName());
+	ACLResolver aclResolver = new ACLResolver(dispatcher, handle);
+	try {
+	    List<DIDStructureType> didList = aclResolver.getUnsatisfiedDIDs(targetName);
+	    // no dids necessary to work with the certificate
+	    if (didList.isEmpty()) {
+		// select the certificate data set
+		DataSetSelect dSelect = new DataSetSelect();
+		dSelect.setConnectionHandle(handle);
+		dSelect.setDataSetName(cryptoMarker.getCertificateRef().getDataSetName());
+		DataSetSelectResponse selResp = (DataSetSelectResponse) dispatcher.deliver(dSelect);
+		// read the certificate
+		String dsiName = cryptoMarker.getCertificateRef().getDataSetName();
+		DSIRead dsiRead = new DSIRead();
+		dsiRead.setDSIName(dsiName);
+		dsiRead.setConnectionHandle(handle);
+		DSIReadResponse readResponse = (DSIReadResponse) dispatcher.deliver(dsiRead);
+		// transform the byte array into an certificate object
+		Certificate cert = Certificate.getInstance(readResponse.getDSIContent());
+		cert.getTBSCertificate();
+		CertificateFactory cf = CertificateFactory.getInstance("X509");
+		ByteArrayInputStream bIn = new ByteArrayInputStream(readResponse.getDSIContent());
+		X509Certificate x509cert = (X509Certificate) cf.generateCertificate(bIn);
+		// get the extensions which should contain the client authentication oid 1.3.6.1.5.5.7.3.2
+		List<String> extendedKeyUsage = x509cert.getExtendedKeyUsage();
+		for (String oid : extendedKeyUsage) {
+		    System.out.println("Extension: " + oid);
+		    if (oid.equals("1.3.6.1.5.5.7.3.2")) {
+			hasAuthCert = true;
+			break;
+		    }
+		}
+
+		CardApplicationDisconnect appDisconnect = new CardApplicationDisconnect();
+		appDisconnect.setConnectionHandle(handle);
+		dispatcher.deliver(appDisconnect);
+	    }
+	} catch (WSException ex) {
+	    logger.error("Result check of the ACLResolver failed.", ex);
+	} catch (SecurityConditionUnsatisfiable ex) {
+	    logger.error("The ACLList operation is not allowed for the certificate data set.", ex);
+	} catch (CertificateParsingException ex) {
+	    logger.error("Failed to parse the certificate.");
+	} catch (CertificateException ex) {
+	    logger.error("Failed to generate the Certificate object.", ex);
+	}
+
+	return false;
     }
 
 }

01b354b6a5b4f7012982b9e3cd5da551d826c02d
Tobias Wich <tobias.wich@ecsec.de>
2014-05-27 16:46:55 +0200

    Add annotations to set "iso" as prefix for the iso namespace

---
+++ wsdef/wsdef-client/src/main/java/iso/std/iso_iec/_24727/tech/schema/package-info.java
@@ -0,0 +1,34 @@
+/****************************************************************************
+ * Copyright (C) 2014 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
+@XmlSchema(
+	elementFormDefault=XmlNsForm.QUALIFIED,
+	namespace="urn:iso:std:iso-iec:24727:tech:schema",
+	xmlns={
+	    @XmlNs(prefix="iso", namespaceURI="urn:iso:std:iso-iec:24727:tech:schema")
+	}
+)
+package iso.std.iso_iec._24727.tech.schema;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;

6bc3e17737d509a32a81108602a3ce4dbb7db3ac
Moritz Horsch <horsch@cdc.informatik.tu-darmstadt.de>
2014-07-16 08:32:01 +0200

    Remove Java Smart Card API for Android

--- ifd/scio-backend/android-pcsc/jni/Application.mk
+++
@@ -1,2 +0,0 @@
-APP_STL := stlport_static
-APP_PLATFORM := android-14
--- ifd/scio-backend/android-pcsc/jni/cyberjack/cjeca32/cjeca32.cpp
+++
@@ -1,9 +0,0 @@
-// cjeca32.cpp : Definiert den Einstiegspunkt für die DLL-Anwendung.
-//
-
-#include "Platform.h"
-#include "cjeca32.h"
-
-
-
-// Dies ist das Beispiel einer exportierten Variable.

5862961bf721c0bceacd13798ef7e3ba75d79cf6
Hans-Martin Haase <hans-martin.haase@ecsec.de>
2014-08-05 14:23:28 +0200

    Put the settings group onto a ScrollPanel.
    This is necessary in case there are many configuration entries. Previously this was not 
    checked so it would lead to a messed up layout.

--- clients/richclient/src/main/java/org/openecard/richclient/gui/manage/SettingsPanel.java
+++ clients/richclient/src/main/java/org/openecard/richclient/gui/manage/SettingsPanel.java
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (C) 2013 ecsec GmbH.
+ * Copyright (C) 2013-2014 ecsec GmbH.
  * All rights reserved.
  * Contact: ecsec GmbH (info@ecsec.de)
  *
@@ -22,10 +22,15 @@
 
 package org.openecard.richclient.gui.manage;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
-import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import org.openecard.addon.AddonPropertiesException;
 
 
 /**
@@ -33,19 +38,36 @@ import javax.swing.BoxLayout;
  * The entries are of type {@link SettingsGroup}.
  *
  * @author Tobias Wich <tobias.wich@ecsec.de>
+ * @author Hans-Martin Haase <hans-martin.haase@ecsec.de>
  */
 public abstract class SettingsPanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
-    private ArrayList<SettingsGroup> groups;
+    private final ArrayList<SettingsGroup> groups;
+    private final JPanel contentPane = new JPanel();
 
     /**
      * Creates a panel instance.
      */
     public SettingsPanel() {
-	this.groups = new ArrayList<SettingsGroup>();
-	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	this.groups = new ArrayList<>();
+	contentPane.setLayout(new GridBagLayout());
+	setLayout(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
+	JScrollPane scrollPane = new JScrollPane(contentPane);
+	scrollPane.setBorder(BorderFactory.createEmptyBorder());
+	scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	scrollPane.getVerticalScrollBar().setValue(0);
+	scrollPane.getVerticalScrollBar().setBlockIncrement(16);
+	scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+	c.fill = GridBagConstraints.BOTH;
+	c.gridwidth = GridBagConstraints.REMAINDER;
+	c.weightx = 1.0;
+	c.weighty = 1.0;
+	c.anchor = GridBagConstraints.NORTHWEST;
+	add(scrollPane, c);
     }
 
     /**
@@ -54,7 +76,14 @@ public abstract class SettingsPanel extends JPanel {
      * @param item The group to add to the panel.
      */
     protected void addSettingsGroup(SettingsGroup item) {
-	add(item);
+
+	GridBagConstraints c = new GridBagConstraints();
+	c.fill = GridBagConstraints.HORIZONTAL;
+	c.gridwidth = GridBagConstraints.REMAINDER;
+	c.weightx = 1.0;
+	c.weighty = 1.0;
+	c.anchor = GridBagConstraints.NORTHWEST;
+	contentPane.add(item, c);
 	groups.add(item);
     }
 
@@ -63,8 +92,10 @@ public abstract class SettingsPanel extends JPanel {
      *
      * @throws IOException Thrown in case the properties could not be written to the output device.
      * @throws SecurityException Thrown in case the permission to save the properties is missing.
+     * @throws org.openecard.addon.AddonPropertiesException Thrown in case the AddonProperties object throws this
+     * exception in the store method.
      */
-    public void saveProperties() throws IOException, SecurityException {
+    public void saveProperties() throws IOException, SecurityException, AddonPropertiesException {
 	for (SettingsGroup next : groups) {
 	    next.saveProperties();
 	}

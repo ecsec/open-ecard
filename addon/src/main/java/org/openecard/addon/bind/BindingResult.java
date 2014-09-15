d63d25f9e3e56408b9ffd9614c3aaa9d2a6d258a
Tobias Wich <tobias.wich@ecsec.de>
2014-09-15 11:52:28 +0200

    Make BindingResult printable

--- addon/src/main/java/org/openecard/addon/bind/BindingResult.java
+++ addon/src/main/java/org/openecard/addon/bind/BindingResult.java
@@ -22,76 +22,147 @@
 
 package org.openecard.addon.bind;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 
 /**
  *
- * @author Tobias Wich <tobias.wich@ecsec.de>
- * @author Dirk Petrautzki <petrautzki@hs-coburg.de>
+ * @author Tobias Wich
+ * @author Dirk Petrautzki
  */
 public class BindingResult {
 
     private BindingResultCode resultCode;
     private Body body;
     private String resultMessage;
-    private final Map<String, String> parameters = new HashMap<String, String>();
-    private final Map<String, String> auxData = new HashMap<String, String>();
-    private final List<Attachment> attachments = new ArrayList<Attachment>(10);
+    private final Map<String, String> parameters = new HashMap<>();
+    private final Map<String, String> auxData = new HashMap<>();
+    private final List<Attachment> attachments = new ArrayList<>(10);
 
+    /**
+     * Creates a response with result code OK.
+     */
     public BindingResult() {
 	resultCode = BindingResultCode.OK;
     }
 
-    public BindingResult(BindingResultCode resultCode) {
+    public BindingResult(@Nonnull BindingResultCode resultCode) {
+	if (resultCode == null) {
+	    throw new IllegalArgumentException("No result code given in constructor invocation.");
+	}
 	this.resultCode = resultCode;
     }
 
-    public void setResultCode(BindingResultCode resultCode) {
+    public void setResultCode(@Nonnull BindingResultCode resultCode) {
+	if (resultCode == null) {
+	    throw new IllegalArgumentException("No result code given in constructor invocation.");
+	}
 	this.resultCode = resultCode;
     }
 
+    @Nonnull
     public BindingResultCode getResultCode() {
 	return resultCode;
     }
 
-    public void setBody(Body body) {
+    public void setBody(@Nullable Body body) {
 	this.body = body;
     }
 
+    @Nullable
     public Body getBody() {
 	return this.body;
     }
 
+    @Nonnull
     public Map<String, String> getParameters() {
 	return this.parameters;
     }
 
-    public String addParameter(String key, String value) {
+    public String addParameter(@Nonnull String key, @Nullable String value) {
 	return parameters.put(key, value);
     }
 
-    public void addParameters(Map<String, String> parameters) {
+    public void addParameters(@Nonnull Map<String, String> parameters) {
 	this.parameters.putAll(parameters);
     }
 
+    @Nullable
+    public String removeParameter(@Nonnull String key) {
+	return this.parameters.remove(key);
+    }
+
+    @Nonnull
     public List<Attachment> getAttachments() {
 	return attachments;
     }
 
-    public String getResultMessage() {
-	return resultMessage;
-    }
-
+    @Nonnull
     public Map<String, String> getAuxResultData() {
 	return this.auxData;
     }
 
-    public void setResultMessage(String resultMessage) {
+    public void setResultMessage(@Nullable String resultMessage) {
 	this.resultMessage = resultMessage;
+    }
+
+    @Nullable
+    public String getResultMessage() {
+	return resultMessage;
+    }
+
+    @Override
+    public String toString() {
+	StringWriter w = new StringWriter();
+	// Header
+	w.write("BindingResult <");
+	w.write(getResultCode().name());
+	w.write(", ");
+	w.write(getResultMessage() == null ? "" : "'");
+	w.write(getResultMessage());
+	w.write(getResultMessage() == null ? "" : "'");
+	w.write(">\n");
+	// Parameter
+	Map<String, String> params = getParameters();
+	printMap(w, "  ", "Parameters", params);
+	// AuxData
+	Map<String, String> aux = getAuxResultData();
+	printMap(w, "  ", "AuxResultData", aux);
+	// Body
+	Body b = getBody();
+	if (b != null) {
+	    w.append("  Body type: ").append(body.getMimeType()).append("\n");
+	}
+	// Attachments
+	List<Attachment> atts = getAttachments();
+	for (Attachment a : atts) {
+	    w.append("  Attachment with type: ").append(a.getMIMEType()).append("\n");
+	}
+	// done
+	return w.toString();
+    }
+
+    private <V> void printMap(StringWriter w, String prefix, String identifier, Map<String, V> map) {
+	if (! map.isEmpty()) {
+	    w.append(prefix).append(identifier).append(" {\n");
+	    for (Map.Entry<String, V> next : map.entrySet()) {
+		w.append(prefix).append(prefix).append("'").append(next.getKey()).append("': ");
+		V v = next.getValue();
+		if (v instanceof String) {
+		    w.append("'").append(v.toString()).append("',\n");
+		} else {
+		    String s = v == null ? "null" : v.toString();
+		    w.append(s).append(",\n");
+		}
+	    }
+	    w.append(prefix).append("}\n");
+	}
     }
 
 }

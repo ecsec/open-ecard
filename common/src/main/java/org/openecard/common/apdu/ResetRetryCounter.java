02ba513487bc04ce992362ba3a38983010f1cbb7
Hans-Martin Haase <hans-martin.haase@ecsec.de>
2014-09-30 14:21:49 +0200

    Adds constructor for the APDU variant which requires a resetting code and new ref data.
    Furthermore the commit adds new getters which indicate the different requirements of this APDU.

--- common/src/main/java/org/openecard/common/apdu/ResetRetryCounter.java
+++ common/src/main/java/org/openecard/common/apdu/ResetRetryCounter.java
@@ -23,6 +23,7 @@
 package org.openecard.common.apdu;
 
 import org.openecard.common.apdu.common.CardCommandAPDU;
+import org.openecard.common.util.ByteUtils;
 
 
 /**
@@ -40,6 +41,7 @@ public class ResetRetryCounter extends CardCommandAPDU {
 
     /**
      * Creates a RESET RETRY COUNTER APDU.
+     * The APDU requires the password type and a new password. The old password is replaced by the given new one.
      *
      * @param password Password
      * @param type Password type
@@ -51,11 +53,64 @@ public class ResetRetryCounter extends CardCommandAPDU {
 
     /**
      * Creates a RESET RETRY COUNTER APDU.
+     * The APDU requires just the password type to reset the retry counter.
      *
      * @param type Password type
      */
     public ResetRetryCounter(byte type) {
 	super(x00, RESET_RETRY_COUNTER_INS, (byte) 0x03, type);
+    }
+
+    /**
+     * Creates a RESET RETRY COUNTER APDU.
+     * The APDU requires a resetting code, new reference data and the password type. This means the old reference data
+     * is replaced with the new reference data after sending the APDU to the card.
+     *
+     * @param resettingCode The specific resetting code necessary to change the old reference data.
+     * @param newRefData The new reference data which shall replace the old reference data.
+     * @param type Password type
+     */
+    public ResetRetryCounter(byte[] resettingCode, byte[] newRefData, byte type) {
+	super(x00, RESET_RETRY_COUNTER_INS, (byte) 0x00, type);
+	setData(ByteUtils.concatenate(resettingCode, newRefData));
+    }
+
+    /**
+     * Checks whether just new reference data is necessary for the APDU.
+     *
+     * @return {@code TRUE} if P1 indicates that the data field contains just new reference data, otherwise {@code FALSE}.
+     */
+    public boolean isWithNewPin() {
+	return getP1() == (byte) 0x02;
+    }
+
+    /**
+     * Checks whether no data is necessary for the APDU.
+     *
+     * @return {@code TRUE} if P1 indicates that the data field have to be empty, otherwise {@code FALSE}.
+     */
+    public boolean isDataAbsent() {
+	return getP1() == (byte) 0x03;
+    }
+
+    /**
+     * Checks whether a resetting code (aka PUK) and new reference data (aka PIN) is necessary for the APDU.
+     *
+     * @return {@code TRUE} if P1 indicates that the data field have to contain a resetting code and without delimitation
+     * new reference data (a.k.a PIN) otherwise {@code FALSE}.
+     */
+    public boolean isWithPukAndNewPin() {
+	return getP1() == (byte) 0x00;
+    }
+
+    /**
+     * Checks whether just a resetting code is required for the APDU.
+     *
+     * @return {@code TRUE} if P1 indicates that the data field have to contain just a resetting code, otherwise
+     * {@code FALSE}.
+     */
+    public boolean isWithPuk() {
+	return getP1() == (byte) 0x01;
     }
 
 }

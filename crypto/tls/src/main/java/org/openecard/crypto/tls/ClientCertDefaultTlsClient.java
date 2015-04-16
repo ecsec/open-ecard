0304b0cebe30079dfb2b24015de62b2e3fb89c85
Tobias Wich <tobias.wich@ecsec.de>
2015-04-16 09:52:44 +0200

    Add SHA1 as hash alogrithms for TLS
    Without these algorithms, TLS client authentication with PKCS#1 v1.5
    fails.

--- crypto/tls/src/main/java/org/openecard/crypto/tls/ClientCertDefaultTlsClient.java
+++ crypto/tls/src/main/java/org/openecard/crypto/tls/ClientCertDefaultTlsClient.java
@@ -183,7 +183,7 @@ public class ClientCertDefaultTlsClient extends DefaultTlsClient implements Clie
 	// overwrite hash and signature algorithms
         if (TlsUtils.isSignatureAlgorithmsExtensionAllowed(clientVersion)) {
             short[] hashAlgorithms = new short[]{ HashAlgorithm.sha512, HashAlgorithm.sha384, HashAlgorithm.sha256,
-                HashAlgorithm.sha224 };
+                HashAlgorithm.sha224, HashAlgorithm.sha1 };
 
             short[] signatureAlgorithms = new short[]{ SignatureAlgorithm.rsa, SignatureAlgorithm.ecdsa };
 

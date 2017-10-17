b77ad4ebb5940e908e8c0b2668df0fcdd62a7299
Tobias Wich <tobias.wich@ecsec.de>
2017-10-17 11:05:39 +0200

    Implement Android EAC GUI test

--- gui/android/src/test/java/org/openecard/gui/android/eac/EacGuiImplTest.java
+++ gui/android/src/test/java/org/openecard/gui/android/eac/EacGuiImplTest.java
@@ -23,12 +23,27 @@
 package org.openecard.gui.android.eac;
 
 import android.content.Context;
+import android.os.RemoteException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import mockit.Expectations;
 import mockit.Mocked;
+import org.openecard.common.util.Promise;
+import org.openecard.gui.StepResult;
+import org.openecard.gui.android.eac.types.ServerData;
+import org.openecard.gui.definition.BoxItem;
+import org.openecard.gui.definition.Checkbox;
+import org.openecard.gui.definition.PasswordField;
 import org.openecard.gui.definition.Step;
+import org.openecard.gui.definition.ToggleText;
 import org.openecard.gui.definition.UserConsentDescription;
+import org.openecard.gui.executor.ExecutionEngine;
+import org.openecard.gui.executor.ExecutionResults;
+import org.openecard.gui.executor.StepAction;
+import org.openecard.gui.executor.StepActionResult;
+import org.openecard.gui.executor.StepActionResultStatus;
+import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
 
@@ -44,22 +59,88 @@ public class EacGuiImplTest {
     UserConsentDescription ucd;
     @Mocked
     EacGuiService anyEacGuiService;
+    @Mocked
+    EacGui.Stub stub;
 
     @Test
-    public void testPinOkFirstTime() {
-	new Expectations(EacGuiImpl.class) {{
-	    anyEacGuiService.getServiceImpl(); result = new EacGuiImpl();
+    public void testPinOkFirstTime() throws InterruptedException, RemoteException {
+	final EacGuiImpl anyGuiImpl = new EacGuiImpl();
+	new Expectations() {{
+	    anyEacGuiService.prepare(androidCtx);
+	    Promise<EacGuiImpl> p = new Promise<>();
+	    p.deliver(anyGuiImpl);
+	    anyEacGuiService.getServiceImpl(); result = p;
 	    ucd.getDialogType(); result = "EAC";
 	    ucd.getSteps(); result = createInitialSteps();
 	}};
 
-	EacNavigator nav = new EacNavigator(androidCtx, ucd);
+	Thread t = new Thread(new Runnable() {
+	    @Override
+	    public void run() {
+		EacNavigator nav = new EacNavigator(androidCtx, ucd);
+		ExecutionEngine exe = new ExecutionEngine(nav);
+		exe.process();
+	    }
+	}, "GUI-Executor");
+	t.start();
+
+	// use the Binders API to access the values
+	ServerData sd = anyGuiImpl.getServerData();
+	assertEquals(sd.getSubject(), "Test Subject");
+	anyGuiImpl.selectAttributes(sd.getReadAccessAttributes(), sd.getWriteAccessAttributes());
+	assertEquals(anyGuiImpl.getPinStatus(), "PIN");
+	assertTrue(anyGuiImpl.enterPin(null, "123456"));
+
+	// wait for executor to finish
+	t.join();
     }
 
     private List<Step> createInitialSteps() {
-	Step step1 = new Step("PROTOCOL_EAC_GUI_STEP_CVC");
+	Step step1 = new Step("PROTOCOL_EAC_GUI_STEP_CVC", "CVC");
+	ToggleText sub = new ToggleText();
+	sub.setID("SubjectName");
+	sub.setText("Test Subject");
 
-	return Arrays.asList(step1);
+	step1.getInputInfoUnits().add(sub);
+
+	final Step step2 = new Step("PROTOCOL_EAC_GUI_STEP_CHAT", "CHAT");
+	Checkbox readBox = new Checkbox("ReadAccess");
+	readBox.getBoxItems().add(makeBoxItem("DG04", false, false));
+	readBox.getBoxItems().add(makeBoxItem("RESTRICTED_IDENTIFICATION", true, true));
+
+	step2.getInputInfoUnits().add(readBox);
+
+	step1.setAction(new StepAction(step1) {
+	    @Override
+	    public StepActionResult perform(Map<String, ExecutionResults> oldResults, StepResult result) {
+		return new StepActionResult(StepActionResultStatus.NEXT, step2);
+	    }
+	});
+
+	final Step step3 = new Step("PROTOCOL_EAC_GUI_STEP_PIN", "PIN");
+	PasswordField pin = new PasswordField("PACE_PIN_FIELD");
+	step3.getInputInfoUnits().add(pin);
+
+	step2.setAction(new StepAction(step2) {
+	    @Override
+	    public StepActionResult perform(Map<String, ExecutionResults> oldResults, StepResult result) {
+		return new StepActionResult(StepActionResultStatus.NEXT, step3);
+	    }
+	});
+
+	final Step step4 = new Step("PROTOCOL_GUI_STEP_PROCESSING", "Finished");
+
+	return Arrays.asList(step1, step2, step3, step4);
+    }
+
+    private BoxItem makeBoxItem(String value, boolean checked, boolean disabled) {
+	BoxItem item = new BoxItem();
+
+	item.setName(value);
+	item.setChecked(checked);
+	item.setDisabled(disabled);
+
+	return item;
     }
 
 }

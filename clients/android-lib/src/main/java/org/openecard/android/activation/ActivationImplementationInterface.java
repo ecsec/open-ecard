eeac0fd2b4a79b5295eaa34b1b0daad6fd0db718
Tobias Wich <tobias.wich@ecsec.de>
2018-09-11 10:39:06 +0200

    Add composite style to implement Activation handlers

--- clients/android-lib/src/main/java/org/openecard/android/activation/ActivationImplementationInterface.java
+++ clients/android-lib/src/main/java/org/openecard/android/activation/ActivationImplementationInterface.java
@@ -23,19 +23,18 @@
 package org.openecard.android.activation;
 
 import android.app.Dialog;
-import java.util.Set;
 import javax.annotation.Nullable;
 import org.openecard.gui.android.AndroidGui;
 
 
 /**
- * Interface containing all methods which can/must be overridden in order to create a functional Activation Activity.
+ * Interface containing all methods which can/must be overridden in order to create a functional Activation Handler.
  *
  * @author Mike Prechtl
  * @author Tobias Wich
  * @param <GUI> Type of the UI that should be used.
  */
-public interface ActivationImplementationInterface <GUI extends AndroidGui> {
+public interface ActivationImplementationInterface <GUI extends AndroidGui> extends ActivationCallbackInterface<GUI> {
 
     /**
      * Key for the return class which is used in extra arguments of the Intent used to transport the activation URL.
@@ -43,70 +42,6 @@ public interface ActivationImplementationInterface <GUI extends AndroidGui> {
      * end of the authentication procedure.</p>
      */
     public static final String RETURN_CLASS = "org.openecard.android.activation.return-class";
-
-
-    /**
-     * Gets a set of card types supported by this activity.
-     * This list is used to determine whether {@link #onCardInserted()} is called or not when a card is recognized by
-     * the system. In case all cards should be matched, {@code null} can be returned by this method.
-     *
-     * @return The set of supported cards, or {@code null} in case all cards are supported.
-     */
-    Set<String> getSupportedCards();
-
-    /**
-     * Callback for card insert events.
-     * By implementing this method, it is possible to notify the user (via the UI) that the card has been inserted and
-     * the EAC process is running. Without this callback the Activity can not distinguish between 'insert card' and
-     * 'please wait for the server'.
-     *
-     * @param cardType Type of the card which has been inserted into the terminal.
-     */
-    void onCardInserted(String cardType);
-
-    /**
-     * This method is called when the specific android GUI is available to the activity.
-     * <p>If this method is called the server data can be accessed and the PIN can be entered.</p>
-     * <p>This method marks the starting point when interaction between the EAC process and the implementing activity
-     * can start.</p>
-     *
-     * @param androidGui Instance of the Android GUI interface connected to the current process.
-     */
-    void onGuiIfaceSet(GUI androidGui);
-
-    /**
-     * Callback when the authentication process is concluded with a redirect address or OK status.
-     * <p>Receiving a redirect address does not necessarily mean that the authentication has been successful. This is
-     * for the server to decide. The URL contains more information about the actual outcome of the authentication.
-     * Functionality handling these kind of errors may be added to this method before calling the default
-     * implementation.</p>
-     * <p>The default implementation in {@link AbstractActivationActivity} of this method performs a TR-03124 conforming
-     * URL intent (redirect). When the Intent starting the Activity contained the return class in the extra arguments
-     * (see {@link #RETURN_CLASS}, then an explicit Intent is performed and the user does not have the choice to select
-     * a suitable Application to receive the URL.</p>
-     *
-     * @param result Result of the finished eID Activation process.
-     */
-    void onAuthenticationSuccess(final ActivationResult result);
-
-    /**
-     * Callback when the authentication process failed without providing a redirect address.
-     * <p>These are unrecoverable errors with regard to a server driven application. That means the error must be
-     * handled independent of the server.</p>
-     * <p>An implementation could for example open move to an activity providing a suitable error view.</p>
-     *
-     * @param result Result containing details about the error happened during the authentication. The refresh URL will
-     *   not be present.
-     */
-    void onAuthenticationFailure(ActivationResult result);
-
-    /**
-     * Callback when the authentication process has been interrupted.
-     * <p>An interruption can be caused by either a user cancel call or by the shutdown of a subsystem.</p>
-     *
-     * @param result Result possibly containg a message describing what interrupted the process.
-     */
-    void onAuthenticationInterrupted(ActivationResult result);
 
 
     /**

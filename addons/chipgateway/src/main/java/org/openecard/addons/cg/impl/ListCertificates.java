cbba53a3e536ec067e8b9b00eacc52b9a7e5cf38
Tobias Wich <tobias.wich@ecsec.de>
2019-07-15 13:08:53 +0200

    Revert "Updated according to the Turkish eID Card"
    This reverts commit 1f73e65068b56e1eeb5c7811665e4776d341c93c.

--- addons/chipgateway/src/main/java/org/openecard/addons/cg/impl/ListCertificates.java
+++ addons/chipgateway/src/main/java/org/openecard/addons/cg/impl/ListCertificates.java
@@ -31,7 +31,6 @@ import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -73,273 +72,264 @@ import org.slf4j.LoggerFactory;
  */
 public class ListCertificates {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ListCertificates.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ListCertificates.class);
 
-	private final TokenCache tokenCache;
-	private final ConnectionHandleType handle;
-	private final List<CertificateFilterType> certFilter;
-	private final char[] pin;
+    private final TokenCache tokenCache;
+    private final ConnectionHandleType handle;
+    private final List<CertificateFilterType> certFilter;
+    private final char[] pin;
 
 
-	public ListCertificates(TokenCache tokenCache, byte[] slotHandle, List<CertificateFilterType> certFilter,
-							@Nullable char[] pin) throws ParameterInvalid {
-		if (slotHandle == null || slotHandle.length == 0) {
-			throw new ParameterInvalid("Slot handle is empty.");
-		}
-
-		this.tokenCache = tokenCache;
-		this.handle = new ConnectionHandleType();
-		this.handle.setSlotHandle(ByteUtils.clone(slotHandle));
-		//this.certFilter = certFilter;
-		CertificateFilterType f1 = new CertificateFilterType();
-		f1.setKeyUsage(KeyUsageType.AUTHENTICATION);
-		this.certFilter = Arrays.asList(f1);
-		this.pin = pin;
+    public ListCertificates(TokenCache tokenCache, byte[] slotHandle, List<CertificateFilterType> certFilter,
+	    @Nullable char[] pin) throws ParameterInvalid {
+	if (slotHandle == null || slotHandle.length == 0) {
+	    throw new ParameterInvalid("Slot handle is empty.");
 	}
 
-	public List<CertificateInfoType> getCertificates() throws WSHelper.WSException, NoSuchDid, CertificateException,
-			CertificateEncodingException, SecurityConditionUnsatisfiable, ParameterInvalid, SlotHandleInvalid {
-		try {
-			ArrayList<CertificateInfoType> result = new ArrayList<>();
-			// get crypto dids
-			DidInfos didInfos = tokenCache.getInfo(pin, handle);
-			List<DidInfo> cryptoDids = didInfos.getCryptoDidInfos();
+	this.tokenCache = tokenCache;
+	this.handle = new ConnectionHandleType();
+	this.handle.setSlotHandle(ByteUtils.clone(slotHandle));
+	this.certFilter = certFilter;
+	this.pin = pin;
+    }
 
-			// get certificates for each crypto did
-			for (DidInfo nextDid : cryptoDids) {
-				LOG.debug("Reading certificates from DID={}.", nextDid.getDidName());
-				List<X509Certificate> certChain = getCertChain(nextDid);
-				if (! certChain.isEmpty() && matchesFilter(certChain)) {
-					AlgorithmInfoType algInfo = nextDid.getGenericCryptoMarker().getAlgorithmInfo();
-					try {
-						String jcaAlg = convertAlgInfo(algInfo);
-						X509Certificate cert = certChain.get(0);
+    public List<CertificateInfoType> getCertificates() throws WSHelper.WSException, NoSuchDid, CertificateException,
+	    CertificateEncodingException, SecurityConditionUnsatisfiable, ParameterInvalid, SlotHandleInvalid {
+	try {
+	    ArrayList<CertificateInfoType> result = new ArrayList<>();
+	    // get crypto dids
+	    DidInfos didInfos = tokenCache.getInfo(pin, handle);
+	    List<DidInfo> cryptoDids = didInfos.getCryptoDidInfos();
 
-						CertificateInfoType certInfo = new CertificateInfoType();
-						for (X509Certificate nextCert : certChain) {
-							certInfo.getCertificate().add(nextCert.getEncoded());
-						}
-						certInfo.setUniqueSSN(getUniqueIdentifier(cert));
-						certInfo.setAlgorithm(jcaAlg);
-						certInfo.setDIDName(nextDid.getDidName());
-
-						result.add(certInfo);
-					} catch (UnsupportedAlgorithmException ex) {
-						// ignore this DID
-						String algId = algInfo.getAlgorithmIdentifier().getAlgorithm();
-						LOG.warn("Ignoring DID with unsupported algorithm ({}).", algId);
-					}
-				}
-			}
-
-			for (CertificateInfoType certInfo : result) {
-				if (certInfo.getDIDName().equals("Authenticate-rsaSSA-PSS")) {
-					return new ArrayList<>(Collections.singleton(certInfo));
-				}
-			}
-
-			return result;
-		} catch (WSHelper.WSException ex) {
-			String minor = StringUtils.nullToEmpty(ex.getResultMinor());
-			switch (minor) {
-				case ECardConstants.Minor.App.INCORRECT_PARM:
-					throw new ParameterInvalid(ex.getMessage(), ex);
-				case ECardConstants.Minor.IFD.INVALID_SLOT_HANDLE:
-					throw new SlotHandleInvalid(ex.getMessage(), ex);
-				case ECardConstants.Minor.SAL.SECURITY_CONDITION_NOT_SATISFIED:
-					throw new SecurityConditionUnsatisfiable(ex.getMessage(), ex);
-				case ECardConstants.Minor.IFD.CANCELLATION_BY_USER:
-				case ECardConstants.Minor.SAL.CANCELLATION_BY_USER:
-					throw new ThreadTerminateException("Certificate retrieval interrupted.", ex);
-				default:
-					throw ex;
-			}
-		} catch (InvocationTargetExceptionUnchecked ex) {
-			if (ex.getCause() instanceof InterruptedException || ex.getCause() instanceof ThreadTerminateException) {
-				String msg = "Certificate retrieval interrupted.";
-				LOG.debug(msg, ex);
-				throw new ThreadTerminateException(msg);
-			} else {
-				String msg = ex.getCause().getMessage();
-				throw WSHelper.createException(WSHelper.makeResultError(ECardConstants.Minor.App.INT_ERROR, msg));
-			}
-		} finally {
-			tokenCache.clearPins();
-		}
-	}
-
-	private boolean matchesFilter(List<X509Certificate> certChain) throws CertificateException, ParameterInvalid {
-		// check if any of the filters matches
-		if (! certFilter.isEmpty()) {
-			for (CertificateFilterType filter : certFilter) {
-				if (filter.getPolicy() != null) {
-					if (! matchesPolicy(filter.getPolicy(), certChain)) {
-						continue;
-					}
-				}
-				if (filter.getIssuer() != null) {
-					if (! matchesIssuer(filter.getIssuer(), certChain)) {
-						continue;
-					}
-				}
-				if (filter.getKeyUsage() != null) {
-					if (! matchesKeyUsage(filter.getKeyUsage(), certChain)) {
-						continue;
-					}
-				}
-
-				// all filters passed, we have a match
-				return true;
-			}
-
-			// no filter matched
-			return false;
-		} else {
-			// no filter to apply, so every chain matches
-			return true;
-		}
-	}
-
-	private boolean matchesPolicy(String policy, List<X509Certificate> certChain) throws CertificateException, ParameterInvalid {
-		try {
-			ASN1ObjectIdentifier policyId = new ASN1ObjectIdentifier(policy);
+	    // get certificates for each crypto did
+	    for (DidInfo nextDid : cryptoDids) {
+		LOG.debug("Reading certificates from DID={}.", nextDid.getDidName());
+		List<X509Certificate> certChain = getCertChain(nextDid);
+		if (! certChain.isEmpty() && matchesFilter(certChain)) {
+		    AlgorithmInfoType algInfo = nextDid.getGenericCryptoMarker().getAlgorithmInfo();
+		    try {
+			String jcaAlg = convertAlgInfo(algInfo);
 			X509Certificate cert = certChain.get(0);
-			byte[] encodedPolicy = cert.getExtensionValue(Extension.certificatePolicies.getId());
-			if (encodedPolicy != null) {
-				encodedPolicy = ASN1OctetString.getInstance(encodedPolicy).getOctets();
-				try {
-					// extract policy object
-					CertificatePolicies certPolicies = CertificatePolicies.getInstance(encodedPolicy);
-					// see if any of the policies matches
-					PolicyInformation targetPolicy = certPolicies.getPolicyInformation(policyId);
-					return targetPolicy != null;
-				} catch (IllegalArgumentException ex) {
-					throw new CertificateException("Certificate contains invalid policy.");
-				}
-			} else {
-				// no policy defined in certificate, so no match
-				return false;
+
+			CertificateInfoType certInfo = new CertificateInfoType();
+			for (X509Certificate nextCert : certChain) {
+			    certInfo.getCertificate().add(nextCert.getEncoded());
 			}
-		} catch (IllegalArgumentException ex) {
-			throw new ParameterInvalid("Requested policy filter is not an OID.");
+			certInfo.setUniqueSSN(getUniqueIdentifier(cert));
+			certInfo.setAlgorithm(jcaAlg);
+			certInfo.setDIDName(nextDid.getDidName());
+
+			result.add(certInfo);
+		    } catch (UnsupportedAlgorithmException ex) {
+			// ignore this DID
+			String algId = algInfo.getAlgorithmIdentifier().getAlgorithm();
+			LOG.warn("Ignoring DID with unsupported algorithm ({}).", algId);
+		    }
 		}
+	    }
+
+	    return result;
+	} catch (WSHelper.WSException ex) {
+	    String minor = StringUtils.nullToEmpty(ex.getResultMinor());
+	    switch (minor) {
+	    	case ECardConstants.Minor.App.INCORRECT_PARM:
+		    throw new ParameterInvalid(ex.getMessage(), ex);
+	    	case ECardConstants.Minor.IFD.INVALID_SLOT_HANDLE:
+		    throw new SlotHandleInvalid(ex.getMessage(), ex);
+	    	case ECardConstants.Minor.SAL.SECURITY_CONDITION_NOT_SATISFIED:
+		    throw new SecurityConditionUnsatisfiable(ex.getMessage(), ex);
+		case ECardConstants.Minor.IFD.CANCELLATION_BY_USER:
+		case ECardConstants.Minor.SAL.CANCELLATION_BY_USER:
+		    throw new ThreadTerminateException("Certificate retrieval interrupted.", ex);
+	    	default:
+		    throw ex;
+	    }
+	} catch (InvocationTargetExceptionUnchecked ex) {
+	    if (ex.getCause() instanceof InterruptedException || ex.getCause() instanceof ThreadTerminateException) {
+		String msg = "Certificate retrieval interrupted.";
+		LOG.debug(msg, ex);
+		throw new ThreadTerminateException(msg);
+	    } else {
+		String msg = ex.getCause().getMessage();
+		throw WSHelper.createException(WSHelper.makeResultError(ECardConstants.Minor.App.INT_ERROR, msg));
+	    }
+	} finally {
+	    tokenCache.clearPins();
+	}
+    }
+
+    private boolean matchesFilter(List<X509Certificate> certChain) throws CertificateException, ParameterInvalid {
+	// check if any of the filters matches
+	if (! certFilter.isEmpty()) {
+	    for (CertificateFilterType filter : certFilter) {
+		if (filter.getPolicy() != null) {
+		    if (! matchesPolicy(filter.getPolicy(), certChain)) {
+			continue;
+		    }
+		}
+		if (filter.getIssuer() != null) {
+		    if (! matchesIssuer(filter.getIssuer(), certChain)) {
+			continue;
+		    }
+		}
+		if (filter.getKeyUsage() != null) {
+		    if (! matchesKeyUsage(filter.getKeyUsage(), certChain)) {
+			continue;
+		    }
+		}
+
+		// all filters passed, we have a match
+		return true;
+	    }
+
+	    // no filter matched
+	    return false;
+	} else {
+	    // no filter to apply, so every chain matches
+	    return true;
+	}
+    }
+
+    private boolean matchesPolicy(String policy, List<X509Certificate> certChain) throws CertificateException, ParameterInvalid {
+	try {
+	    ASN1ObjectIdentifier policyId = new ASN1ObjectIdentifier(policy);
+	    X509Certificate cert = certChain.get(0);
+	    byte[] encodedPolicy = cert.getExtensionValue(Extension.certificatePolicies.getId());
+	    if (encodedPolicy != null) {
+		encodedPolicy = ASN1OctetString.getInstance(encodedPolicy).getOctets();
+		try {
+		    // extract policy object
+		    CertificatePolicies certPolicies = CertificatePolicies.getInstance(encodedPolicy);
+		    // see if any of the policies matches
+		    PolicyInformation targetPolicy = certPolicies.getPolicyInformation(policyId);
+		    return targetPolicy != null;
+		} catch (IllegalArgumentException ex) {
+		    throw new CertificateException("Certificate contains invalid policy.");
+		}
+	    } else {
+		// no policy defined in certificate, so no match
+		return false;
+	    }
+	} catch (IllegalArgumentException ex) {
+	    throw new ParameterInvalid("Requested policy filter is not an OID.");
+	}
+    }
+
+    private boolean matchesIssuer(String issuer, List<X509Certificate> certChain) {
+	X509Certificate cert = certChain.get(0);
+
+	// determine where to add wildcards
+	boolean prefixWildcard = false;
+	boolean infixWildcard = false;
+	if (issuer.startsWith("*")) {
+	    issuer = issuer.substring(1);
+	    prefixWildcard = true;
+	}
+	if (issuer.endsWith("*")) {
+	    issuer = issuer.substring(0, issuer.length() - 1);
+	    infixWildcard = true;
+	}
+	// quote the remaining text to prevent regex injection
+	issuer = Pattern.quote(issuer);
+	// add the wildcards
+	if (prefixWildcard) {
+	    issuer = ".*" + issuer;
+	}
+	if (infixWildcard) {
+	    issuer = issuer + ".*";
+	}
+	Pattern searchPattern = Pattern.compile(issuer);
+
+	// extract RDNs if they exist
+	String certIssuer = cert.getIssuerX500Principal().getName();
+	X500Name issuerDn = new X500Name(certIssuer);
+
+	// compare CN
+	if (matchesRdn(searchPattern, issuerDn, BCStyle.CN)) {
+	    return true;
+	}
+	// compare GN
+	if (matchesRdn(searchPattern, issuerDn, BCStyle.GIVENNAME)) {
+	    return true;
 	}
 
-	private boolean matchesIssuer(String issuer, List<X509Certificate> certChain) {
-		X509Certificate cert = certChain.get(0);
+	// no match
+	return false;
+    }
 
-		// determine where to add wildcards
-		boolean prefixWildcard = false;
-		boolean infixWildcard = false;
-		if (issuer.startsWith("*")) {
-			issuer = issuer.substring(1);
-			prefixWildcard = true;
-		}
-		if (issuer.endsWith("*")) {
-			issuer = issuer.substring(0, issuer.length() - 1);
-			infixWildcard = true;
-		}
-		// quote the remaining text to prevent regex injection
-		issuer = Pattern.quote(issuer);
-		// add the wildcards
-		if (prefixWildcard) {
-			issuer = ".*" + issuer;
-		}
-		if (infixWildcard) {
-			issuer = issuer + ".*";
-		}
-		Pattern searchPattern = Pattern.compile(issuer);
+    private boolean matchesRdn(Pattern searchPattern, X500Name name, ASN1ObjectIdentifier rdnIdentifier) {
+	RDN[] rdns = name.getRDNs(rdnIdentifier);
+	if (rdns.length >= 1) {
+	    // only compare first as everything else would be non standard in X509 certs
+	    AttributeTypeAndValue rdnAttr = rdns[0].getFirst();
+	    ASN1String attrStr = (ASN1String) rdnAttr.getValue().toASN1Primitive();
+	    String rdnStr = attrStr.getString();
+	    return searchPattern.matcher(rdnStr).matches();
+	} else {
+	    return false;
+	}
+    }
 
-		// extract RDNs if they exist
-		String certIssuer = cert.getIssuerX500Principal().getName();
-		X500Name issuerDn = new X500Name(certIssuer);
-
-		// compare CN
-		if (matchesRdn(searchPattern, issuerDn, BCStyle.CN)) {
-			return true;
-		}
-		// compare GN
-		if (matchesRdn(searchPattern, issuerDn, BCStyle.GIVENNAME)) {
-			return true;
-		}
-
-		// no match
+    private boolean matchesKeyUsage(KeyUsageType keyUsage, List<X509Certificate> certChain) {
+	X509Certificate cert = certChain.get(0);
+	boolean[] certUsage = cert.getKeyUsage();
+	switch (keyUsage) {
+	    case AUTHENTICATION:
+		// digitalSignature
+		return certUsage[0];
+	    case SIGNATURE:
+		// nonRepudiation
+		return certUsage[1];
+	    case ENCRYPTION:
+		// keyEncipherment, dataEncipherment, keyAgreement
+		return certUsage[2] || certUsage[3] || certUsage[4];
+	    default:
 		return false;
 	}
+    }
 
-	private boolean matchesRdn(Pattern searchPattern, X500Name name, ASN1ObjectIdentifier rdnIdentifier) {
-		RDN[] rdns = name.getRDNs(rdnIdentifier);
-		if (rdns.length >= 1) {
-			// only compare first as everything else would be non standard in X509 certs
-			AttributeTypeAndValue rdnAttr = rdns[0].getFirst();
-			ASN1String attrStr = (ASN1String) rdnAttr.getValue().toASN1Primitive();
-			String rdnStr = attrStr.getString();
-			return searchPattern.matcher(rdnStr).matches();
-		} else {
-			return false;
-		}
+    private String getUniqueIdentifier(X509Certificate cert) {
+	// try to get SERIALNUMBER from subject
+	X500Name sub = X500Name.getInstance(cert.getSubjectX500Principal().getEncoded());
+	RDN[] serials = sub.getRDNs(BCStyle.SERIALNUMBER);
+	if (serials.length >= 1) {
+	    AttributeTypeAndValue serialValueType = serials[0].getFirst();
+	    ASN1Encodable serialValue = serialValueType.getValue();
+	    if (ASN1String.class.isInstance(serialValue)) {
+		return ASN1String.class.cast(serialValue).getString();
+	    }
 	}
 
-	private boolean matchesKeyUsage(KeyUsageType keyUsage, List<X509Certificate> certChain) {
-		X509Certificate cert = certChain.get(0);
-		boolean[] certUsage = cert.getKeyUsage();
-		switch (keyUsage) {
-			case AUTHENTICATION:
-				// digitalSignature
-				return certUsage[0];
-			case SIGNATURE:
-				// nonRepudiation
-				return certUsage[1];
-			case ENCRYPTION:
-				// keyEncipherment, dataEncipherment, keyAgreement
-				return certUsage[2] || certUsage[3] || certUsage[4];
-			default:
-				return false;
-		}
+	// no SERIALNUMBER, hash subject and cross fingers that this is unique across replacement cards
+	try {
+	    SHA256Digest digest = new SHA256Digest();
+	    byte[] subData = sub.getEncoded();
+	    digest.update(subData, 0, subData.length);
+	    byte[] hashResult = new byte[digest.getDigestSize()];
+	    digest.doFinal(hashResult, 0);
+	    String hashedSub = ByteUtils.toWebSafeBase64String(hashResult);
+	    return hashedSub;
+	} catch (IOException ex) {
+	    throw new RuntimeException("Failed to encode subject.", ex);
 	}
+    }
 
-	private String getUniqueIdentifier(X509Certificate cert) {
-		// try to get SERIALNUMBER from subject
-		X500Name sub = X500Name.getInstance(cert.getSubjectX500Principal().getEncoded());
-		RDN[] serials = sub.getRDNs(BCStyle.SERIALNUMBER);
-		if (serials.length >= 1) {
-			AttributeTypeAndValue serialValueType = serials[0].getFirst();
-			ASN1Encodable serialValue = serialValueType.getValue();
-			if (ASN1String.class.isInstance(serialValue)) {
-				return ASN1String.class.cast(serialValue).getString();
-			}
-		}
+    private String convertAlgInfo(iso.std.iso_iec._24727.tech.schema.AlgorithmInfoType isoAlgInfo)
+	    throws UnsupportedAlgorithmException {
+	String oid = isoAlgInfo.getAlgorithmIdentifier().getAlgorithm();
+	String jcaAlg = AllowedSignatureAlgorithms.algIdtoJcaName(oid);
+	return jcaAlg;
+    }
 
-		// no SERIALNUMBER, hash subject and cross fingers that this is unique across replacement cards
-		try {
-			SHA256Digest digest = new SHA256Digest();
-			byte[] subData = sub.getEncoded();
-			digest.update(subData, 0, subData.length);
-			byte[] hashResult = new byte[digest.getDigestSize()];
-			digest.doFinal(hashResult, 0);
-			String hashedSub = ByteUtils.toWebSafeBase64String(hashResult);
-			return hashedSub;
-		} catch (IOException ex) {
-			throw new RuntimeException("Failed to encode subject.", ex);
-		}
+    private List<X509Certificate> getCertChain(DidInfo nextDid) throws WSHelper.WSException,
+	    SecurityConditionUnsatisfiable, NoSuchDid {
+	try {
+	    return nextDid.getRelatedCertificateChain();
+	} catch (CertificateException ex) {
+	    LOG.warn("DID {} did not contain any certificates.", nextDid.getDidName());
+	    LOG.debug("Cause:", ex);
+	    return Collections.emptyList();
 	}
-
-	private String convertAlgInfo(iso.std.iso_iec._24727.tech.schema.AlgorithmInfoType isoAlgInfo)
-			throws UnsupportedAlgorithmException {
-		String oid = isoAlgInfo.getAlgorithmIdentifier().getAlgorithm();
-		String jcaAlg = AllowedSignatureAlgorithms.algIdtoJcaName(oid);
-		return jcaAlg;
-	}
-
-	private List<X509Certificate> getCertChain(DidInfo nextDid) throws WSHelper.WSException,
-			SecurityConditionUnsatisfiable, NoSuchDid {
-		try {
-			return nextDid.getRelatedCertificateChain();
-		} catch (CertificateException ex) {
-			LOG.warn("DID {} did not contain any certificates.", nextDid.getDidName());
-			LOG.debug("Cause:", ex);
-			return Collections.emptyList();
-		}
-	}
+    }
 
 }

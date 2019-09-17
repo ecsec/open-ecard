11f3eaa4979fd3e23407aea95607bc80c282ee76
Neil Crossley <neil.crossley@ecsec.de>
2019-09-17 11:34:32 +0200

    Add initial iOS specific lib.

---
+++ clients/ios-lib/src/main/java/org/openecard/ios/activation/IOSActivationUtils.java
@@ -0,0 +1,65 @@
+/****************************************************************************
+ * Copyright (C) 2019 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
+package org.openecard.ios.activation;
+
+import org.openecard.mobile.activation.ContextManager;
+import org.openecard.mobile.activation.EacControllerFactory;
+import org.openecard.mobile.activation.PinManagementControllerFactory;
+import org.openecard.mobile.activation.common.CommonActivationUtils;
+import org.openecard.mobile.system.OpeneCardContextConfig;
+import org.openecard.scio.IOSNFCFactory;
+import org.openecard.ws.jaxb.JAXBMarshaller;
+
+/**
+ *
+ * @author Neil Crossley
+ */
+public class IOSActivationUtils {
+
+    private final IOSNFCCapabilities capabilities;
+    private final CommonActivationUtils utils;
+
+    public IOSActivationUtils(IOSNFCCapabilities capabilities, CommonActivationUtils utils) {
+	this.capabilities = capabilities;
+	this.utils = utils;
+    }
+
+    public ContextManager context() {
+	return this.utils.context(capabilities);
+    }
+
+    public EacControllerFactory eacFactory() {
+	return this.utils.eacFactory();
+    }
+
+    public PinManagementControllerFactory pinManagementFactory() {
+	return this.utils.pinManagementFactory();
+    }
+
+    public static IOSActivationUtils create() {
+	IOSNFCCapabilities capabilities = new IOSNFCCapabilities();
+	OpeneCardContextConfig config = new OpeneCardContextConfig(IOSNFCFactory.class.getCanonicalName(), JAXBMarshaller.class.getCanonicalName());
+	CommonActivationUtils activationUtils = new CommonActivationUtils(config);
+	return new IOSActivationUtils(capabilities, activationUtils);
+    }
+}

f15b7d536631b9d582fc55a645afdc41577b7e20
Neil Crossley <neil.crossley@ecsec.de>
2019-11-06 13:58:33 +0100

    Split tests into seperate unit tests

--- wsdef/android-marshaller/src/test/java/org/openecard/ws/android/AndroidMarshallerTest.java
+++ wsdef/android-marshaller/src/test/java/org/openecard/ws/android/AndroidMarshallerTest.java
@@ -183,7 +183,7 @@ public class AndroidMarshallerTest {
     }
 
     @Test
-    public void testConversionOfCardInfo() throws Exception {
+    public void testConversionOfCardInfoNPA_CIF() throws Exception {
 	WSMarshaller m = new AndroidMarshaller();
 	Object o = m.unmarshal(m.str2doc(NPA_CIF));
 	if (!(o instanceof CardInfo)) {
@@ -217,12 +217,17 @@ public class AndroidMarshallerTest {
 	    }
 	}
 
+    }
+
+    @Test
+    public void testConversionOfCardInfoEGK_CIF() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
 	// Test eGK
-	o = m.unmarshal(m.str2doc(EGK_CIF));
+	Object o = m.unmarshal(m.str2doc(EGK_CIF));
 	if (!(o instanceof CardInfo)) {
 	    throw new Exception("Object should be an instace of CardInfo");
 	}
-	cardInfo = (CardInfo) o;
+	CardInfo cardInfo = (CardInfo) o;
 
 	assertEquals("http://ws.gematik.de/egk/1.0.0", cardInfo.getCardType().getObjectIdentifier());
 	CardApplicationType cardApplicationESIGN = cardInfo.getApplicationCapabilities().getCardApplication().get(2);
@@ -243,13 +248,16 @@ public class AndroidMarshallerTest {
 	FileOutputStream fos2 = new FileOutputStream(f2);
 	marshalLog(cardInfoJM, fos);
 	marshalLog(cardInfo, fos2);*/
-
+    }
+    @Test
+    public void testConversionOfCardInfoECARD_AT_CIF() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
 	// Test ecard AT 0.9.0
-	o = m.unmarshal(m.str2doc(ECARD_AT_CIF));
+	Object o = m.unmarshal(m.str2doc(ECARD_AT_CIF));
 	if (! (o instanceof CardInfo)) {
 	    throw new Exception("Object should be an instance of CardInfo");
 	}
-	cardInfo = (CardInfo) o;
+	CardInfo cardInfo = (CardInfo) o;
     }
 
     @Test
@@ -606,7 +614,7 @@ public class AndroidMarshallerTest {
     }
 
     @Test
-    public void testConversionOfDIDAutheticate() throws Exception {
+    public void testConversionOfDIDAutheticateDID_AUTHENTICATE_PACE() throws Exception {
 	WSMarshaller m = new AndroidMarshaller();
 	Document d = m.str2doc(DID_AUTHENTICATE_PACE);
 
@@ -647,15 +655,18 @@ public class AndroidMarshallerTest {
 		Assert.fail("Unknown element contained in message.");
 	    }
 	}
+    }
+    @Test
+    public void testConversionOfDIDAutheticateDID_AUTHENTICATE_TA() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	Document d = m.str2doc(DID_AUTHENTICATE_TA);
 
-	d = m.str2doc(DID_AUTHENTICATE_TA);
-
-	o = m.unmarshal(d);
+	Object o = m.unmarshal(d);
 	if (!(o instanceof DIDAuthenticate)) {
 	    throw new Exception("Object should be an instance of DIDAuthenticate");
 	}
 
-	didAuthenticate = (DIDAuthenticate) o;
+	DIDAuthenticate didAuthenticate = (DIDAuthenticate) o;
 	assertEquals(didAuthenticate.getDIDName(), "PIN");
 	assertEquals(didAuthenticate.getConnectionHandle().getSlotHandle(), StringUtils.toByteArray("05D4F40AEBD9919383C22216055EA3DB15056C51"));
 	assertEquals(EAC2InputType.class, didAuthenticate.getAuthenticationProtocolData().getClass());
@@ -668,15 +679,18 @@ public class AndroidMarshallerTest {
 		"7F218201427F4E81FB5F290100420E5A5A4456434141544130303030357F494F060A04007F0007020202020386410470C07FAA329E927D961F490F5430B395EECF3D2A538194D8B637DE0F8ACF60A9031816AC51B594097EB211FB8F55FAA8507D5800EF7B94E024F9630314116C755F200B5A5A444B423230303033557F4C12060904007F0007030102025305000301DF045F25060100000601085F2406010000070001655E732D060904007F00070301030280207C1901932DB75D08539F2D4A27C938F79E69E083C442C068B299D185BC8AFA78732D060904007F0007030103018020BFD2A6A2E4237948D7DCCF7975D71D40F15307AA59F580A48777CBEED093F54B5F3740618F584E4293F75DDE8977311694B69A3ED73BBE43FDAFEC11B7ECF054F84ACB1231615338CE8D6EC332480883E14E0664950F85134290DD716B7C153232BC96");
 
 	marshalLog(didAuthenticate);
+    }
+    @Test
+    public void testConversionOfDIDAutheticateDID_AUTHENTICATE_CA() throws Exception {
+	WSMarshaller m = new AndroidMarshaller();
+	Document d = m.str2doc(DID_AUTHENTICATE_CA);
 
-	d = m.str2doc(DID_AUTHENTICATE_CA);
-
-	o = m.unmarshal(d);
+	Object o = m.unmarshal(d);
 	if (!(o instanceof DIDAuthenticate)) {
 	    throw new Exception("Object should be an instance of DIDAuthenticate");
 	}
 
-	didAuthenticate = (DIDAuthenticate) o;
+	DIDAuthenticate didAuthenticate = (DIDAuthenticate) o;
 	assertEquals(didAuthenticate.getDIDName(), "PIN");
 	assertEquals(didAuthenticate.getConnectionHandle().getSlotHandle(), StringUtils.toByteArray("05D4F40AEBD9919383C22216055EA3DB15056C51"));
 	assertEquals(EACAdditionalInputType.class, didAuthenticate.getAuthenticationProtocolData().getClass());

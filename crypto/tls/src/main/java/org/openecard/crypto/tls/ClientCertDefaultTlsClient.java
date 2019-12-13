0fcfc65ebeb693ebfc21490e30c43c98da60477c
Florian Otto <florian.otto@ecsec.de>
2019-12-13 11:18:59 +0100

    Revert "Allow sha1 to be used in TLS signature algorithms"
    This reverts commit 33132d98cd45098282b7e27cfb6a8183f391d62a.

--- crypto/tls/src/main/java/org/openecard/crypto/tls/ClientCertDefaultTlsClient.java
+++ crypto/tls/src/main/java/org/openecard/crypto/tls/ClientCertDefaultTlsClient.java
@@ -253,7 +253,7 @@ public class ClientCertDefaultTlsClient extends DefaultTlsClient implements Clie
     protected Vector getSupportedSignatureAlgorithms() {
 	TlsCrypto crypto = context.getCrypto();
         short[] hashAlgorithms = new short[]{HashAlgorithm.sha512, HashAlgorithm.sha384, HashAlgorithm.sha256,
-	    HashAlgorithm.sha224, HashAlgorithm.sha1};
+	    HashAlgorithm.sha224};
         short[] signatureAlgorithms = new short[]{ SignatureAlgorithm.rsa, SignatureAlgorithm.ecdsa };
 
         Vector result = new Vector();

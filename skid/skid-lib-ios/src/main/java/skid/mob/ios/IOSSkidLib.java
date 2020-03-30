ba79c9c53e9fc46129a41c2fcd193c33ba9ea838
Florian Otto <florian.otto@ecsec.de>
2020-03-30 08:58:36 +0200

    Add ios-core project

--- skid/skid-lib-ios/src/main/java/skid/mob/ios/IOSSkidLib.java
+++ skid/skid-lib-ios/src/main/java/skid/mob/ios/IOSSkidLib.java
@@ -10,172 +10,177 @@
 
 package skid.mob.ios;
 
-import android.content.Intent;
-import android.nfc.Tag;
-import java.io.IOException;
-import org.openecard.android.activation.AndroidContextManager;
-import org.openecard.android.activation.DelegatingAndroidContextManager;
-import org.openecard.common.util.Promise;
+import java.security.Provider;
+import java.security.Security;
+import org.openecard.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.openecard.common.util.SysUtils;
 import org.openecard.mobile.activation.ActivationSource;
 import org.openecard.mobile.activation.ActivationUtils;
-import org.openecard.mobile.activation.NFCCapabilities;
-import org.openecard.mobile.activation.NfcCapabilityResult;
+import org.openecard.mobile.activation.ContextManager;
 import org.openecard.mobile.activation.PinManagementControllerFactory;
-import org.openecard.mobile.activation.ServiceErrorResponse;
-import org.openecard.mobile.activation.StartServiceHandler;
-import org.openecard.mobile.activation.StopServiceHandler;
 import org.openecard.mobile.activation.common.CommonActivationUtils;
-import org.openecard.mobile.activation.common.NFCDialogMsgSetter;
-import org.openecard.mobile.ex.ApduExtLengthNotSupported;
-import org.openecard.mobile.ex.NFCTagNotSupported;
-import org.openecard.mobile.ex.NfcDisabled;
-import org.openecard.mobile.ex.NfcUnavailable;
-import org.openecard.mobile.ex.UnableToInitialize;
-import org.openecard.mobile.system.OpeneCardContextConfig;
-import org.openecard.scio.AndroidNFCFactory;
 import org.openecard.scio.CachingTerminalFactoryBuilder;
-import org.openecard.ws.android.AndroidMarshaller;
+import org.openecard.scio.IOSNFCFactory;
 import skid.mob.impl.SamlClientImpl;
-import skid.mob.impl.SkidResultImpl;
 import skid.mob.lib.SamlClient;
-import skid.mob.lib.SkidErrorCodes;
 import skid.mob.lib.SkidLib;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.openecard.ios.activation.DeveloperOptions;
+import org.openecard.ios.activation.NFCConfig;
+import org.openecard.robovm.annotations.FrameworkObject;
+import org.slf4j.LoggerFactory;
 import skid.mob.lib.SkidResult;
 
 
 /**
  *
- * @author Tobias Wich
+ * @author Florian Otto
  */
+@FrameworkObject(factoryMethod = "createSkidLib")
 public class IOSSkidLib implements SkidLib {
 
     static {
 	SysUtils.setIsIOS();
+
+	Provider provider = new BouncyCastleProvider();
+	try {
+	    Security.removeProvider(provider.getName());
+	    Security.removeProvider("BC");
+	} catch (Exception e) {
+	}
+	Security.addProvider(provider);
+
+	LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+	Logger rootLogger = context.getLogger(Logger.ROOT_LOGGER_NAME);
+	rootLogger.setLevel(Level.ERROR);
     }
 
-    private final ActivationUtils utils;
-    private final CachingTerminalFactoryBuilder<AndroidNFCFactory> builder;
+    private /* final */ ActivationUtils utils;
+    private /* final */ CachingTerminalFactoryBuilder<IOSNFCFactory> builder;
 
-    private AndroidContextManager oecCtx;
+    private ContextManager oecCtx;
     private ActivationSource oecActivationSource;
 
-    IOSSkidLib(CommonActivationUtils utils, CachingTerminalFactoryBuilder<AndroidNFCFactory> builder) {
+    private /*final*/ DeveloperOptions developerOptions;
+    private NFCConfig nfcConfig;
+
+    public IOSSkidLib() {
+
+    }
+
+    IOSSkidLib(CommonActivationUtils utils, CachingTerminalFactoryBuilder<IOSNFCFactory> builder) {
 	this.utils = utils;
 	this.builder = builder;
     }
+//in constructor cause create method gets done via roboface
 
-    public static IOSSkidLib createSkidLib() {
-	CachingTerminalFactoryBuilder<AndroidNFCFactory> factory = new CachingTerminalFactoryBuilder<>(() -> new AndroidNFCFactory());
-
-	OpeneCardContextConfig config = new OpeneCardContextConfig(factory, AndroidMarshaller.class.getCanonicalName());
-	CommonActivationUtils activationUtils = new CommonActivationUtils(config, new NFCDialogMsgSetter() {
-	    @Override
-	    public void setText(String msg) {
-	    }
-
-	    @Override
-	    public boolean isSupported() {
-		return false;
-	    }
-	});
-
-	return new IOSSkidLib(activationUtils, factory);
-    }
+//    public static IOSSkidLib createSkidLib() {
+//	CachingTerminalFactoryBuilder<IOSNFCFactory> factory = new CachingTerminalFactoryBuilder<>(() -> new IOSNFCFactory());
+//
+//	OpeneCardContextConfig config = new OpeneCardContextConfig(factory, AndroidMarshaller.class.getCanonicalName());
+//	CommonActivationUtils activationUtils = new CommonActivationUtils(config, new NFCDialogMsgSetter() {
+//	    @Override
+//	    public void setText(String msg) {
+//	    }
+//
+//	    @Override
+//	    public boolean isSupported() {
+//		return false;
+//	    }
+//	});
+//
+//	return new IOSSkidLib(activationUtils, factory);
+//    }
 
 
     @Override
     public SkidResult initialize() {
-	// pretend NFC is fully functional, the actual check must be done by the application beforehand and eid actions must be prevented if necessary
-	oecCtx = new DelegatingAndroidContextManager(utils.context(new NFCCapabilities() {
-	    @Override
-	    public boolean isAvailable() {
-		return true;
-	    }
-
-	    @Override
-	    public boolean isEnabled() {
-		return true;
-	    }
-
-	    @Override
-	    public NfcCapabilityResult checkExtendedLength() {
-		return NfcCapabilityResult.SUPPORTED;
-	    }
-	}), builder);
-
-	Promise<ActivationSource> finished = new Promise<>();
-	Promise<ServiceErrorResponse> error = new Promise<>();
-
-	try {
-	    oecCtx.initializeContext(new StartServiceHandler() {
-		@Override
-		public void onSuccess(ActivationSource source) {
-		    error.cancel();
-		    finished.deliver(source);
-		}
-
-		@Override
-		public void onFailure(ServiceErrorResponse response) {
-		    error.deliver(response);
-		    finished.cancel();
-		}
-	    });
-
-	    oecActivationSource = finished.deref();
-	    return new SkidResultImpl();
-	} catch (InterruptedException ex) {
-	    if (error.isDelivered()) {
-		ServiceErrorResponse res = error.derefNonblocking();
-		SkidResult skidRes = SkidResultImpl.fromOecServiceError(res);
-		return skidRes;
-	    } else {
-		return new SkidResultImpl(SkidErrorCodes.INTERRUPTED, "Initialization has been interrupted.");
-	    }
-	} catch (ApduExtLengthNotSupported | NfcDisabled | NfcUnavailable | UnableToInitialize ex) {
-	    return new SkidResultImpl(SkidErrorCodes.INTERNAL_ERROR, "Failed to initialize Open eCard Stack.");
-	}
+//	// pretend NFC is fully functional, the actual check must be done by the application beforehand and eid actions must be prevented if necessary
+//	oecCtx = new DelegatingAndroidContextManager(utils.context(new NFCCapabilities() {
+//	    @Override
+//	    public boolean isAvailable() {
+//		return true;
+//	    }
+//
+//	    @Override
+//	    public boolean isEnabled() {
+//		return true;
+//	    }
+//
+//	    @Override
+//	    public NfcCapabilityResult checkExtendedLength() {
+//		return NfcCapabilityResult.SUPPORTED;
+//	    }
+//	}), builder);
+//
+//	Promise<ActivationSource> finished = new Promise<>();
+//	Promise<ServiceErrorResponse> error = new Promise<>();
+//
+//	try {
+//	    oecCtx.initializeContext(new StartServiceHandler() {
+//		@Override
+//		public void onSuccess(ActivationSource source) {
+//		    error.cancel();
+//		    finished.deliver(source);
+//		}
+//
+//		@Override
+//		public void onFailure(ServiceErrorResponse response) {
+//		    error.deliver(response);
+//		    finished.cancel();
+//		}
+//	    });
+//
+//	    oecActivationSource = finished.deref();
+//	    return new SkidResultImpl();
+//	} catch (InterruptedException ex) {
+//	    if (error.isDelivered()) {
+//		ServiceErrorResponse res = error.derefNonblocking();
+//		SkidResult skidRes = SkidResultImpl.fromOecServiceError(res);
+//		return skidRes;
+//	    } else {
+//		return new SkidResultImpl(SkidErrorCodes.INTERRUPTED, "Initialization has been interrupted.");
+//	    }
+//	} catch (ApduExtLengthNotSupported | NfcDisabled | NfcUnavailable | UnableToInitialize ex) {
+//	    return new SkidResultImpl(SkidErrorCodes.INTERNAL_ERROR, "Failed to initialize Open eCard Stack.");
+//	}
+	return null;
     }
 
     @Override
     public SkidResult terminate() {
-	Promise<Object> finished = new Promise<>();
-	Promise<ServiceErrorResponse> error = new Promise<>();
-
-	try {
-	    oecCtx.terminateContext(new StopServiceHandler() {
-		@Override
-		public void onSuccess() {
-		    error.cancel();
-		    finished.deliver(true);
-		}
-
-		@Override
-		public void onFailure(ServiceErrorResponse response) {
-		    error.deliver(response);
-		    finished.cancel();
-		}
-	    });
-	    
-	    finished.deref();
-	    return new SkidResultImpl();
-	} catch (InterruptedException ex) {
-	    if (error.isDelivered()) {
-		ServiceErrorResponse res = error.derefNonblocking();
-		SkidResult skidRes = SkidResultImpl.fromOecServiceError(res);
-		return skidRes;
-	    } else {
-		return new SkidResultImpl(SkidErrorCodes.INTERRUPTED, "Termination has been interrupted.");
-	    }
-	}
-    }
-
-    public void onNewIntent(Intent intent) throws ApduExtLengthNotSupported, NFCTagNotSupported, IOException {
-	oecCtx.onNewIntent(intent);
-    }
-
-    public void onNewIntent(Tag tag) throws ApduExtLengthNotSupported, NFCTagNotSupported, IOException {
-	oecCtx.onNewIntent(tag);
+//	Promise<Object> finished = new Promise<>();
+//	Promise<ServiceErrorResponse> error = new Promise<>();
+//
+//	try {
+//	    oecCtx.terminateContext(new StopServiceHandler() {
+//		@Override
+//		public void onSuccess() {
+//		    error.cancel();
+//		    finished.deliver(true);
+//		}
+//
+//		@Override
+//		public void onFailure(ServiceErrorResponse response) {
+//		    error.deliver(response);
+//		    finished.cancel();
+//		}
+//	    });
+//
+//	    finished.deref();
+//	    return new SkidResultImpl();
+//	} catch (InterruptedException ex) {
+//	    if (error.isDelivered()) {
+//		ServiceErrorResponse res = error.derefNonblocking();
+//		SkidResult skidRes = SkidResultImpl.fromOecServiceError(res);
+//		return skidRes;
+//	    } else {
+//		return new SkidResultImpl(SkidErrorCodes.INTERRUPTED, "Termination has been interrupted.");
+//	    }
+//	}
+	return null;
     }
 
     @Override

bd6137692e60e366267015546b90e69e12b45eac
Tobias Wich <tobias.wich@ecsec.de>
2020-10-14 14:33:56 +0200

    Update script, so it runs with podman if installed

--- doc/gendoc.sh
+++ doc/gendoc.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 
-BUILD_CMD="docker-compose build"
-RUN_CMD="docker-compose run --rm sphinx"
+addMount() {
+    local src=$(echo "$1" | awk '{split($0,a,":"); print a[1]}')
+    local dst=$(echo "$1" | awk '{split($0,a,":"); print a[2]}')
+    src=$(realpath $src)
+    MOUNT_OPTS="$MOUNT_OPTS --mount=type=bind,source=$src,destination=$dst"
+}
+
+
+IMG_VERSION="1.1"
+IMAGE="public.docker.ecsec.de/ecsec/tools/sphinx:$IMG_VERSION"
+MOUNTS=".:/docs ..:/src"
+
+
+# create mount options
+for m in $MOUNTS; do
+    addMount $m
+done
+
+# replace docker with podman if possible
+DOCKER="docker"
+if [ -f $(which podman) ]; then
+    DOCKER="podman"
+fi
+
+RUN_CMD="$DOCKER run -it --rm $MOUNT_OPTS $IMAGE"
 
 case $1 in
 html)

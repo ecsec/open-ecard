01c17f82cd761b7bba463bfde8a3314271e661ce
Tobias Wich <tobias.wich@ecsec.de>
2023-03-30 09:20:22 +0200

    Add OIDs to HashAlgorithms

--- crypto/crypto-common/src/main/java/org/openecard/crypto/common/HashAlgorithms.java
+++ crypto/crypto-common/src/main/java/org/openecard/crypto/common/HashAlgorithms.java
@@ -22,6 +22,10 @@
 
 package org.openecard.crypto.common;
 
+import org.openecard.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.openecard.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.openecard.bouncycastle.asn1.x509.X509ObjectIdentifiers;
+
 import javax.annotation.Nonnull;
 
 
@@ -35,21 +39,23 @@ public enum HashAlgorithms {
 //    CKM_RIPEMD160 (0x00000240L),
 //    CKM_MD2       (0x00000200L, "MD2"),
 //    CKM_MD5       (0x00000210L, "MD5", "http://www.w3.org/2001/04/xmldsig-more#md5"),
-    CKM_SHA_1     (0x00000220L, "SHA-1", "http://www.w3.org/2000/09/xmldsig#sha1"),
-    CKM_SHA256    (0x00000250L, "SHA-256", "http://www.w3.org/2001/04/xmlenc#sha256"),
-    CKM_SHA224    (0x00000255L, "SHA-224", "http://www.w3.org/2001/04/xmldsig-more#sha224"),
-    CKM_SHA384    (0x00000260L, "SHA-384", "http://www.w3.org/2001/04/xmldsig-more#sha384"),
-    CKM_SHA512    (0x00000270L, "SHA-512", "http://www.w3.org/2001/04/xmlenc#sha512");
+    CKM_SHA_1     (0x00000220L, "SHA-1", "http://www.w3.org/2000/09/xmldsig#sha1", X509ObjectIdentifiers.id_SHA1),
+    CKM_SHA256    (0x00000250L, "SHA-256", "http://www.w3.org/2001/04/xmlenc#sha256", NISTObjectIdentifiers.id_sha256),
+    CKM_SHA224    (0x00000255L, "SHA-224", "http://www.w3.org/2001/04/xmldsig-more#sha224", NISTObjectIdentifiers.id_sha224),
+    CKM_SHA384    (0x00000260L, "SHA-384", "http://www.w3.org/2001/04/xmldsig-more#sha384", NISTObjectIdentifiers.id_sha384),
+    CKM_SHA512    (0x00000270L, "SHA-512", "http://www.w3.org/2001/04/xmlenc#sha512", NISTObjectIdentifiers.id_sha512);
 //    CKM_SHA512_T  (0x00000050L);
 
     private final long pkcs11MechanismId;
     private final String jcaAlg;
     private final String algId;
+    private final ASN1ObjectIdentifier oid;
 
-    private HashAlgorithms(long id, String jcaAlg, String algId) {
+    private HashAlgorithms(long id, String jcaAlg, String algId, ASN1ObjectIdentifier oid) {
 	this.pkcs11MechanismId = id;
 	this.jcaAlg = jcaAlg;
 	this.algId = algId;
+	this.oid = oid;
     }
 
     public long getPkcs11Mechanism() {
@@ -64,6 +70,11 @@ public enum HashAlgorithms {
     @Nonnull
     public String getAlgId() {
 	return algId;
+    }
+
+    @Nonnull
+    public ASN1ObjectIdentifier getOid() {
+	return oid;
     }
 
 }

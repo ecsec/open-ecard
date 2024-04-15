e725b7dede871fd2ab68361b3bba0d1c2cfa727a
Florian Otto <florian.otto@ecsec.de>
2024-04-15 11:28:53 +0200

    Add plugin for version checking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 subprojects {
-	group = "org.openecard"
-	version = "2.1.12-SNAPSHOT"
+    group = "org.openecard"
+    version = "2.1.12-SNAPSHOT"
+}
+plugins {
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+allprojects {
+    apply {
+	plugin("com.github.ben-manes.versions")
+    }
 }

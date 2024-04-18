f4d77e94ed8e135a52706df96650f209c29eae24
Tobias Wich <tobias.wich@ecsec.de>
2024-04-18 10:42:46 +0200

    chore: Move version and group definition to properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,3 @@
-subprojects {
-    group = "org.openecard"
-    version = "2.1.12-SNAPSHOT"
-}
 plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("com.quittle.setup-android-sdk") version "3.1.0"

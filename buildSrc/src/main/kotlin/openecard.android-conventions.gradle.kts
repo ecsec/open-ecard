b45c33ee4683629f9d84d6d03d2af77af0c9c069
Mike Prechtl <mike.prechtl@ecsec.de>
2024-04-18 13:49:24 +0200

    Add repository information for publishing aar of android-lib

--- buildSrc/src/main/kotlin/openecard.android-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/openecard.android-conventions.gradle.kts
@@ -33,4 +33,15 @@ publishing {
 	    }
 	}
     }
+	repositories {
+		maven {
+			credentials {
+				username = System.getenv("MVN_ECSEC_USERNAME") ?: project.findProperty("mvnUsernameEcsec") as String?
+				password = System.getenv("MVN_ECSEC_PASSWORD") ?: project.findProperty("mvnPasswordEcsec") as String?
+			}
+			val releasesRepoUrl = uri("https://mvn.ecsec.de/repository/openecard-release")
+			val snapshotsRepoUrl = uri("https://mvn.ecsec.de/repository/openecard-snapshot")
+			url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+		}
+	}
 }

190ea026cbee9bb08125e54557a9ce5bfe8c9253
Tobias Wich <tobias.wich@ecsec.de>
2024-04-30 10:18:53 +0200

    chore: Migrate project to toml based catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,23 +12,30 @@ java {
 }
 
 
-repositories {
-	gradlePluginPortal()
-	google()
-	mavenCentral()
-}
-
 plugins {
 	// Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
 	`kotlin-dsl`
 }
 
 
-val androidPluginVersion: String by props
-
-//val kotlinPluginVersion = "1.9.20"
 dependencies {
-//	implementation("org.jetbrains.kotlin.jvm", "org.jetbrains.kotlin.jvm.gradle.plugin", kotlinPluginVersion)
-//	implementation("org.jetbrains.kotlin.plugin.allopen", "org.jetbrains.kotlin.plugin.allopen.gradle.plugin", kotlinPluginVersion )
-	implementation("com.android.library:com.android.library.gradle.plugin:${androidPluginVersion}")
+	implementation(libs.plugins.androidLibrary)
+}
+
+fun DependencyHandlerScope.implementation(pluginProv: Provider<PluginDependency>) {
+	pluginProv.orNull ?.let { plugin ->
+		val pluginId = plugin.pluginId
+		val pluginVer = plugin.version
+		this.implementation("$pluginId:$pluginId.gradle.plugin") {
+			version {
+				branch = pluginVer.branch
+				prefer(pluginVer.preferredVersion)
+				if (pluginVer.requiredVersion.isNotBlank()) {
+					require(pluginVer.requiredVersion)
+				} else {
+					strictly(pluginVer.strictVersion)
+				}
+			}
+		}
+	}
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -75,72 +75,7 @@ dependencyResolutionManagement {
 	versionCatalogs {
 
 		create("libs") {
-			// logging
-			library("slf4j-api", "org.slf4j", "slf4j-api").version("2.0.6")
-			library("slf4j-jdk14", "org.slf4j", "slf4j-jdk14").version("2.0.6")
-			library("logback-classic", "ch.qos.logback", "logback-classic").version("1.4.5")
-
-			// utilities
-			library("annotations", "com.google.code.findbugs", "annotations").version("3.0.1u2")
-
-			// testing
-			library("testng", "org.testng", "testng").version("7.7.0")
-			library("powermock-testng", "org.powermock", "powermock-module-testng").version("2.0.9")
-			library("powermock-mockito", "org.powermock", "powermock-api-mockito2").version("2.0.9")
-			bundle("test-basics", listOf("testng", "powermock-testng", "powermock-mockito"))
-			library("awaitility", "org.awaitility", "awaitility").version("4.2.0")
-
-			//adds job for generating reports about dependency versions
-			plugin("versions", "com.github.ben-manes.versions").version("0.51.0")
-
-			// jaxb
-			library("jaxb-api", "jakarta.xml.bind", "jakarta.xml.bind-api").version("4.0.0")
-			library("jaxb-ws-api", "jakarta.xml.ws", "jakarta.xml.ws-api").version("4.0.0")
-			library("jaxb-impl", "org.glassfish.jaxb", "jaxb-runtime").version("4.0.2")
-			plugin("cxf", "io.mateo.cxf-codegen").version("2.2.0")
-			library("jackson-jaxb", "com.fasterxml.jackson.module", "jackson-module-jakarta-xmlbind-annotations").version("2.14.0")
-
-//			library("bc-prov", "org.bouncycastle:bcprov-jdk15on:1.62")
-//			library("bc-pkix", "org.bouncycastle:bcpkix-jdk15on:1.62")
-//			library("bc-tls", "org.bouncycastle:bctls-jdk15on:1.62")
-			library("bc-prov", "org.openecard:bcprov-jdk15on:1.62")
-			library("bc-pkix", "org.openecard:bcpkix-jdk15on:1.62")
-			library("bc-tls", "org.openecard:bctls-jdk15on:1.62")
-
-			library("httpcore", "org.apache.httpcomponents", "httpcore").version("4.4.16")
-			// https://github.com/akuhtz/proxy-vole
-			library("proxyvole", "org.bidib.com.github.markusbernhardt", "proxy-vole").version("1.1.2")
-//			library("nashorn", "org.javadelight", "delight-nashorn-sandbox").version("0.2.5")
-
-			library("pdfbox", "org.apache.pdfbox", "pdfbox").version("2.0.28")
-
-			library("scio", "com.github.martinpaljak", "apdu4j-jnasmartcardio").version("0.2.7+220204")
-
-			library("android", "com.google.android", "android").version("api-33")
-
-//			version("robovm", "2.3.19-SNAPSHOT")
-			version("robovm", "2.3.21")
-			library("robovm-gradle", "com.mobidevelop.robovm", "robovm-gradle-plugin").versionRef("robovm")
-			library("robovm-rt", "com.mobidevelop.robovm", "robovm-rt").versionRef("robovm")
-			library("robovm-cocoa", "com.mobidevelop.robovm", "robovm-cocoatouch").versionRef("robovm")
-			library("roboface-annots", "org.openecard.tools", "roboface-annotation").version("1.4.0")
-			library("roboface-marshal", "org.openecard.tools", "roboface-marshaller").version("1.4.0")
-			library("roboface-processor", "org.openecard.tools", "roboface-processor").version("1.4.0")
-
-			library("xerces-imp", "xerces", "xercesImpl").version("2.12.0")
-
-			library("jose4j", "org.bitbucket.b_c", "jose4j").version("0.9.3")
-
-			library("jna-jpms", "net.java.dev.jna", "jna-jpms").version("5.12.1")
-			library("jna-jpms-platform", "net.java.dev.jna", "jna-platform-jpms").version("5.12.1")
-
-			version("jfx", "17.0.2")
-			plugin("jfx", "org.openjfx.javafxplugin").version("0.1.0")
-//			library("jfx-base", "org.openjfx", "javafx-base").version("17.0.2")
-//			library("jfx-controls", "org.openjfx", "javafx-controls").version("17.0.2")
-//			library("jfx-swing", "org.openjfx", "javafx-swing").version("17.0.2")
-
-			plugin("jpackage", "org.panteleyev.jpackageplugin").version("1.6.0")
+			from(files("libs.versions.toml"))
 
 //			library("nimbus-oidc", "com.nimbusds:oauth2-oidc-sdk:9.5.3")
 //

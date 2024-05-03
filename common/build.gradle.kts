8443c9668ac1bf522983e54d0f1e0c33ec441b80
Tobias Wich <tobias.wich@ecsec.de>
2024-05-03 19:31:45 +0200

    chore: Migrate common module to KMP

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,26 +1,43 @@
 description = "common"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-	api(libs.slf4j.api)
-	api(project(":wsdef:wsdef-common"))
-	api(project(":wsdef:wsdef-client"))
-	implementation(project(":i18n"))
-	api(libs.bc.prov)
-	api(libs.bc.tls)
-	api(libs.httpcore)
-
-	testImplementation(libs.bundles.test.basics)
-	testImplementation(project(":wsdef:jaxb-marshaller"))
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				api(libs.slf4j.api)
+				api(project(":wsdef:wsdef-common"))
+				api(project(":wsdef:wsdef-client"))
+				implementation(project(":i18n"))
+				api(libs.bc.prov)
+				api(libs.bc.tls)
+				api(libs.httpcore)
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(project(":wsdef:jaxb-marshaller"))
+				implementation(libs.bundles.test.powermock)
+			}
+		}
+	}
 }
 
 
-tasks.named("processResources", ProcessResources::class).configure {
+tasks.named("jvmProcessResources", ProcessResources::class).configure {
 	filesMatching("**/VERSION") {
 		expand("version" to project.version.toString())
 	}

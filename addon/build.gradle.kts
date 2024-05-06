4030e51e472904fc60de8be986e8b2d12e3ed3c4
Tobias Wich <tobias.wich@ecsec.de>
2024-05-06 11:09:55 +0200

    chore: Migrate addon to kotlin module

--- addon/build.gradle.kts
+++ addon/build.gradle.kts
@@ -1,20 +1,38 @@
 description = "addon"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-	api(project(":common"))
-//	api(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-
-	testImplementation(libs.bundles.test.basics)
-	testImplementation(project(":wsdef:jaxb-marshaller"))
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				//	api(libs.jaxb.api)
+				//	api(libs.jaxb.ws.api)
+				//	api(libs.slf4j.api)
+				api(project(":common"))
+				//	api(project(":wsdef:wsdef-client"))
+				//	implementation(project(":i18n"))
+				//	api(libs.bc.prov)
+				//	api(libs.bc.tls)
+				//	api(libs.httpcore)
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(project(":wsdef:jaxb-marshaller"))
+			}
+		}
+	}
 }

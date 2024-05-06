511bc28adfaf4713da1354b5ce2b160ee17622d2
Tobias Wich <tobias.wich@ecsec.de>
2024-05-06 11:05:47 +0200

    chore: Migrate http binding to kotlin module

--- bindings/http/build.gradle.kts
+++ bindings/http/build.gradle.kts
@@ -1,29 +1,49 @@
 description = "http"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-	api(project(":addon"))
-//	api(project(":wsdef:wsdef-common"))
-//	api(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-
-	testImplementation(libs.bundles.test.basics)
-	testImplementation(project(":gui:swing"))
-	testImplementation(project(":ifd:ifd-core"))
-	testImplementation(project(":management"))
-	testImplementation(project(":sal:tiny-sal"))
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				//	api(libs.jaxb.api)
+				//	api(libs.jaxb.ws.api)
+				//	api(libs.slf4j.api)
+				//	api(project(":wsdef:wsdef-common"))
+				//	api(project(":wsdef:wsdef-client"))
+				//	implementation(project(":i18n"))
+				//	api(libs.bc.prov)
+				//	api(libs.bc.tls)
+				//	api(libs.httpcore)
+				api(project(":addon"))
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				//implementation(libs.bundles.test.powermock)
+				implementation(project(":gui:swing"))
+				implementation(project(":ifd:ifd-core"))
+				implementation(project(":management"))
+				implementation(project(":sal:tiny-sal"))
+			}
+		}
+	}
 }
 
-tasks.named("processResources", ProcessResources::class) {
+
+tasks.named("jvmProcessResources", ProcessResources::class).configure {
 	doLast {
 		// build file listing of www files in resources dir, separated with :
 		val files = destinationDir.resolve("www").walk()

b110b728a46a2d655f5c74e81af7a5ef6b051853
Tobias Wich <tobias.wich@ecsec.de>
2024-05-06 16:17:37 +0200

    chore: Migrate status addon to kotlin module

--- addons/status/build.gradle.kts
+++ addons/status/build.gradle.kts
@@ -1,24 +1,41 @@
 description = "status"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-	api(project(":addon"))
-//	api(project(":wsdef:wsdef-common"))
-//	api(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-
-	testImplementation(libs.bundles.test.basics)
-//	testImplementation(project(":gui:swing"))
-//	testImplementation(project(":ifd:ifd-core"))
-//	testImplementation(project(":management"))
-//	testImplementation(project(":sal:tiny-sal"))
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				//	api(libs.jaxb.api)
+				//	api(libs.jaxb.ws.api)
+				//	api(libs.slf4j.api)
+				api(project(":addon"))
+				//	api(project(":wsdef:wsdef-common"))
+				//	api(project(":wsdef:wsdef-client"))
+				//	implementation(project(":i18n"))
+				//	api(libs.bc.prov)
+				//	api(libs.bc.tls)
+				//	api(libs.httpcore)
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(project(":wsdef:jaxb-marshaller"))
+				implementation(libs.bundles.test.powermock)
+			}
+		}
+	}
 }
+

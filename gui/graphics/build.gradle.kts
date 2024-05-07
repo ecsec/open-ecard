35d19a4704d86666ec973b464c7c4baa47efd843
Mike Prechtl <mike.prechtl@ecsec.de>
2024-05-07 12:05:27 +0200

    chore: Migrate gui-graphics to kotlin module

--- gui/graphics/build.gradle.kts
+++ gui/graphics/build.gradle.kts
@@ -1,20 +1,30 @@
 description = "graphics"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-	api(libs.slf4j.api)
-//	api(project(":crypto:crypto-common"))
-//	implementation(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-//	api(libs.proxyvole)
-
-	testImplementation(libs.bundles.test.basics)
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				api(libs.slf4j.api)
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics)
+			}
+		}
+	}
 }

0958ddf7697a56e0cd2e56885171ac1b87278966
Mike Prechtl <mike.prechtl@ecsec.de>
2024-05-07 10:22:10 +0200

    chore: Migrate tr03112 to kotlin module

--- addons/tr03112/build.gradle.kts
+++ addons/tr03112/build.gradle.kts
@@ -1,25 +1,34 @@
 description = "tr03112"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
-dependencies {
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-	api(project(":addon"))
-	api(project(":crypto:tls"))
-	api(project(":ifd:ifd-common"))
-	api(project(":sal:sal-common"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-
-	testImplementation(libs.bundles.test.basics)
-//	testImplementation(project(":gui:swing"))
-//	testImplementation(project(":ifd:ifd-core"))
-//	testImplementation(project(":management"))
-//	testImplementation(project(":sal:tiny-sal"))
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				api(project(":addon"))
+				api(project(":crypto:tls"))
+				api(project(":ifd:ifd-common"))
+				api(project(":sal:sal-common"))
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics)
+				implementation(project(":sal:tiny-sal"))
+			}
+		}
+	}
 }

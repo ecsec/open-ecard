5d903c0284477ebccf826eec3adc3d8197111019
Mike Prechtl <mike.prechtl@ecsec.de>
2024-05-07 13:28:09 +0200

    chore: Migrate i18n to kotlin module

--- i18n/build.gradle.kts
+++ i18n/build.gradle.kts
@@ -1,5 +1,26 @@
 description = "i18n"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
+}
+
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {}
+		}
+		val jvmTest by getting {
+			dependencies {}
+		}
+	}
 }

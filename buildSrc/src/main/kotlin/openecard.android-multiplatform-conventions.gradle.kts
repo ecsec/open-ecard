c436f702b90d1c39ee87d59dbfbb9e8be57e7be0
Tobias Wich <tobias.wich@ecsec.de>
2024-05-10 14:27:54 +0200

    chore: Set java task file encoding to UTF-8

--- buildSrc/src/main/kotlin/openecard.android-multiplatform-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/openecard.android-multiplatform-conventions.gradle.kts
@@ -21,3 +21,11 @@ tasks.withType<Test> {
 	maxHeapSize = testHeapSize
 	useTestNG()
 }
+
+// set encoding for legacy java tasks, remove once all java is migrated to kotlin
+tasks.withType<JavaCompile> {
+	options.encoding = "UTF-8"
+}
+tasks.withType<Javadoc> {
+	options.encoding = "UTF-8"
+}

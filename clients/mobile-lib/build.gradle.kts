47948691dda7e3bca11dc43417db7585dc6fd667
Mike Prechtl <mike.prechtl@ecsec.de>
2024-05-10 13:54:28 +0200

    chore: Migrate mobile-lib to kotlin module

--- clients/mobile-lib/build.gradle.kts
+++ clients/mobile-lib/build.gradle.kts
@@ -3,14 +3,51 @@ import com.android.build.gradle.internal.tasks.factory.dependsOn
 description = "mobile-lib"
 
 plugins {
-	id("openecard.lib-conventions")
+	id("openecard.lib-multiplatform-conventions")
 }
 
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val jvmMain by getting {
+			dependencies {
+				compileOnly(libs.robovm.rt)
+				compileOnly(libs.robovm.cocoa)
+				compileOnly(libs.roboface.annots)
+				implementation(libs.roboface.marshal)
+
+				api(project(":management"))
+				api(project(":sal:tiny-sal"))
+				api(project(":addons:tr03112"))
+				api(project(":addons:pin-management"))
+				api(project(":addons:status"))
+				api(project(":addons:genericcryptography"))
+				api(project(":ifd:ifd-protocols:pace"))
+			}
+		}
+		val jvmTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics)
+				implementation(project(":ifd:scio-backend:mobile-nfc"))
+				implementation(project(":wsdef:jaxb-marshaller"))
+			}
+		}
+	}
+}
 
 sourceSets {
 	create("ios") {
 		java {
-			srcDir("src/main/java")
+			srcDir("src/jvmMain/java")
 		}
 		compileClasspath += main.get().compileClasspath
 //		runtimeClasspath += main.get().runtimeClasspath
@@ -21,13 +58,6 @@ sourceSets {
 val ios by configurations.creating {
 	isCanBeConsumed = true
 	isCanBeResolved = false
-	// If you want this configuration to share the same dependencies, otherwise omit this line
-	extendsFrom(
-		configurations["api"],
-//		configurations["implementation"],
-//		configurations["compileOnly"],
-//		configurations["runtimeOnly"],
-	)
 }
 
 val iosHeaders by configurations.creating {
@@ -65,71 +95,6 @@ artifacts {
 	add(iosHeaders.name, shareHeader)
 }
 
-
-
-
-//sourceSets {
-//	create("ios") {
-//		java {
-//			srcDir("src/main/java")
-//		}
-//		compileClasspath += main.get().compileClasspath
-//		runtimeClasspath += main.get().runtimeClasspath
-//		annotationProcessorPath += main.get().annotationProcessorPath
-//	}
-//}
-//
-//configurations {
-//	create("ios") {
-////		extendsFrom(configurations.api.get())
-//	}
-//}
-//
-//val iosImplementation by configurations.named("iosImplementation") {
-//	extendsFrom(configurations.implementation.get())
-//}
-//val iosCompileOnly by configurations.named("iosCompileOnly") {
-//	extendsFrom(configurations.compileOnly.get())
-//}
-//configurations {
-//	this.named("iosImplementation") {
-//		extendsFrom(implementation.get())
-//	}
-//	this.named("iosApi") {
-//		extendsFrom(api.get())
-//	}
-//	this.named("iosAnnotationProcessor") {
-//		extendsFrom(annotationProcessor.get())
-//	}
-//}
-
-
-
 dependencies {
-	compileOnly(libs.robovm.rt)
-	compileOnly(libs.robovm.cocoa)
-	compileOnly(libs.roboface.annots)
-	implementation(libs.roboface.marshal)
-
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-	api(project(":management"))
-	api(project(":sal:tiny-sal"))
-	api(project(":addons:tr03112"))
-	api(project(":addons:pin-management"))
-	api(project(":addons:status"))
-	api(project(":addons:genericcryptography"))
-	api(project(":ifd:ifd-protocols:pace"))
-//	api(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-
 	annotationProcessor(libs.roboface.processor)
-
-	testImplementation(libs.bundles.test.basics)
-	testImplementation(project(":ifd:scio-backend:mobile-nfc"))
-	testImplementation(project(":wsdef:jaxb-marshaller"))
 }

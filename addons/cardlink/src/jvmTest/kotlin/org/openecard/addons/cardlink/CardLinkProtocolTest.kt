6f092f103ab1cc04dcc1c5ce82bac11558eaaf17
Mike Prechtl <mike.prechtl@ecsec.de>
2024-05-22 09:00:56 +0200

    chore: remove unnecessary code in CardLinkProtocolTest

--- addons/cardlink/src/jvmTest/kotlin/org/openecard/addons/cardlink/CardLinkProtocolTest.kt
+++ addons/cardlink/src/jvmTest/kotlin/org/openecard/addons/cardlink/CardLinkProtocolTest.kt
@@ -23,34 +23,14 @@
 package org.openecard.addons.cardlink
 
 import io.github.oshai.kotlinlogging.KotlinLogging
-import iso.std.iso_iec._24727.tech.schema.EstablishContext
-import iso.std.iso_iec._24727.tech.schema.Initialize
-import iso.std.iso_iec._24727.tech.schema.ListIFDs
 import org.mockito.Mockito
-import org.openecard.addon.AddonManager
-import org.openecard.common.ClientEnv
-import org.openecard.common.WSHelper
-import org.openecard.common.event.EventDispatcherImpl
 import org.openecard.common.ifd.scio.TerminalFactory
-import org.openecard.common.interfaces.Dispatcher
-import org.openecard.common.interfaces.EventDispatcher
-import org.openecard.common.sal.CombinedCIFProvider
-import org.openecard.common.util.ByteUtils
 import org.openecard.common.util.Promise
-import org.openecard.gui.UserConsent
-import org.openecard.gui.definition.ViewController
-import org.openecard.ifd.scio.IFD
-import org.openecard.management.TinyManagement
 import org.openecard.mobile.activation.*
 import org.openecard.mobile.activation.common.CommonActivationUtils
 import org.openecard.mobile.activation.common.NFCDialogMsgSetter
 import org.openecard.mobile.system.OpeneCardContextConfig
-import org.openecard.mobile.ui.*
-import org.openecard.recognition.CardRecognitionImpl
-import org.openecard.recognition.RepoCifProvider
-import org.openecard.sal.TinySAL
 import org.openecard.scio.PCSCFactory
-import org.openecard.transport.dispatcher.MessageDispatcher
 import org.openecard.ws.common.GenericInstanceProvider
 import org.openecard.ws.jaxb.JAXBMarshaller
 import org.testng.Assert
@@ -65,16 +45,12 @@ private val logger = KotlinLogging.logger {}
  */
 class CardLinkProtocolTest {
 
-	private lateinit var env: ClientEnv
-
 	private lateinit var activationUtils: CommonActivationUtils
 
 	private val promise: Promise<ActivationResult?> = Promise<ActivationResult?>()
 
 	@BeforeClass
 	fun setup() {
-		env = ClientEnv()
-
 		val pcscFactory : GenericInstanceProvider<TerminalFactory?> = object : GenericInstanceProvider<TerminalFactory?> {
 			override val instance = PCSCFactory()
 		}
@@ -100,66 +76,6 @@ class CardLinkProtocolTest {
 
 		val contextManager = activationUtils.context(nfcCapabilities)
 		contextManager.initializeContext(startServiceHandler)
-
-		val eventDispatcher = EventDispatcherImpl()
-		eventDispatcher.start()
-
-		val vc: ViewController = Mockito.mock(ViewController::class.java)
-		val d: Dispatcher = MessageDispatcher(env)
-		val tinyManagement = TinyManagement(env)
-		val uc : UserConsent = createUserConsent(d, msgSetter, eventDispatcher)
-		val mainSAL = TinySAL(env)
-
-		val ifd = IFD()
-		ifd.setEnvironment(env)
-		//ifd.addProtocol(ECardConstants.Protocol.PACE, PACEProtocolFactory())
-
-		env.gui = uc
-		env.dispatcher = d
-		env.management = tinyManagement
-		env.eventDispatcher = eventDispatcher
-		env.ifd = ifd
-		env.sal = mainSAL
-
-		val establishContext = EstablishContext()
-		val ecr = ifd.establishContext(establishContext)
-		logger.info { "Established context." }
-
-		val cr = CardRecognitionImpl(env)
-		env.recognition = cr
-
-		WSHelper.checkResult(ecr)
-		val contextHandle = ecr.contextHandle
-		logger.info { "ContextHandle: ${ByteUtils.toHexString(contextHandle)}" }
-		mainSAL.setIfdCtx(contextHandle)
-
-		val listIFDs = ListIFDs()
-		listIFDs.contextHandle = ecr.contextHandle
-
-		val cp = CombinedCIFProvider()
-		cp.addCifProvider(RepoCifProvider(cr))
-		env.cifProvider = cp
-
-		val addonManager = AddonManager(env, vc, mainSAL.salStateView)
-		mainSAL.setAddonManager(addonManager)
-
-		addonManager.registry.listAddons();
-
-		WSHelper.checkResult(mainSAL.initialize(Initialize()))
-	}
-
-	private fun createUserConsent(dispatcher: Dispatcher, msgSetter: NFCDialogMsgSetter, eventDispatcher: EventDispatcher): CompositeUserConsent {
-		val eacNavFac = EacNavigatorFactory.create(msgSetter, dispatcher)
-		val insertFac = InsertCardNavigatorFactory(msgSetter)
-		val pinMngFac = PINManagementNavigatorFactory(dispatcher, eventDispatcher)
-		val cardLinkNavFac = CardLinkNavigatorFactory.create(msgSetter, dispatcher)
-
-		val allFactories = listOf(eacNavFac, insertFac, pinMngFac, cardLinkNavFac)
-
-		return CompositeUserConsent(
-			allFactories,
-			MessageDialogStub()
-		)
 	}
 
 	@Test

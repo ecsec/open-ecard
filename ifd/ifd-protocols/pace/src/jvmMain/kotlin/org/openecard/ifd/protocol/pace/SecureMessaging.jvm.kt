ecsec/open-ecard

commit c0eead309a52cc5017d010330da61542b714e6b2

fix: Apply padding in SM only, in case the block is not already filled completely

ifd/ifd-protocols/pace/src/jvmMain/kotlin/org/openecard/ifd/protocol/pace/SecureMessaging.jvm.kt
@@ -417,7 +417,9 @@ class SecureMessaging(
 	private fun pad(data: ByteArray, blockSize: Int): ByteArray {
 		val result = ByteArray(data.size + (blockSize - data.size % blockSize))
 		System.arraycopy(data, 0, result, 0, data.size)
-		result[data.size] = PAD
+		if (data.size != blockSize) {
+			result[data.size] = PAD
+		}
 
 		return result
 	}

commit 21e29890d153664adbf6e458f660e1e7bc5a45a3

fix: Correct padding mistake introduced by c0eead309a52

ifd/ifd-protocols/pace/src/jvmMain/kotlin/org/openecard/ifd/protocol/pace/SecureMessaging.jvm.kt
@@ -428,11 +428,10 @@ class SecureMessaging(
 	 * @return Padded data
 	 */
 	private fun pad(data: ByteArray, blockSize: Int): ByteArray {
+		// as padding is mandatory, the result will contain an extra empty block in case the data is already a multiple of the block size
 		val result = ByteArray(data.size + (blockSize - data.size % blockSize))
 		System.arraycopy(data, 0, result, 0, data.size)
-		if (data.size != blockSize) {
-			result[data.size] = PAD
-		}
+		result[data.size] = PAD
 
 		return result
 	}
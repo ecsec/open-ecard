3cdc297cc16e3c3f0b09834b9329aacad1bbbfac
Florian Otto <florian.otto@ecsec.de>
2024-09-06 12:50:37 +0200

    Revert "ci: Add version qualifier in jpackage config"
    This reverts commit 32d7cb0e741a06a038d3fc3c714db80ed5365a52.

--- clients/richclient/build.gradle.kts
+++ clients/richclient/build.gradle.kts
@@ -57,7 +57,7 @@ val setAppVendor = "ecsec GmbH"
 val setAppLicenseFile = projectDir.resolve("../../LICENSE.GPL").path
 val setAppAboutUrl = "https://openecard.org/"
 val setAppVersion = VersionNumber.parse(project.version.toString()).let {
-	"${it.major}.${it.minor}.${it.micro}${ if (!it.qualifier.isNullOrEmpty()) "-${it.qualifier}" else ""}"
+	"${it.major}.${it.minor}.${it.micro}"
 }
 
 task("copyDependencies", Copy::class) {

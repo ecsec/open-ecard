f9664fc955e1648a42518a88ce4023afbb364b33
Tobias Wich <tobias.wich@ecsec.de>
2025-01-21 08:14:55 +0100

    feat: Convert android nfc to kotlin

--- ifd/scio-backend/android-nfc/build.gradle.kts
+++ ifd/scio-backend/android-nfc/build.gradle.kts
@@ -1,30 +1,29 @@
 description = "android-nfc"
 
 plugins {
-	id("openecard.android-conventions")
+	id("openecard.android-multiplatform-conventions")
 }
 
 android {
 	namespace = "org.openecard.ifd.nfc"
 }
 
-dependencies {
-//	implementation(libs.android)
-//	api(libs.jaxb.api)
-//	api(libs.jaxb.ws.api)
-//	api(libs.slf4j.api)
-//	api(project(":common"))
-//	api(project(":ifd:ifd-common"))
-	api(project(":ifd:scio-backend:mobile-nfc"))
-//	implementation(project(":wsdef:wsdef-client"))
-//	implementation(project(":i18n"))
-//	api(libs.bc.prov)
-//	api(libs.bc.tls)
-//	api(libs.httpcore)
-//	api(libs.proxyvole)
-//	api(project(":gui:graphics"))
-//	api(libs.pdfbox)
-//	api(libs.scio)
-
-	testImplementation(libs.bundles.test.basics)
+kotlin {
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+				implementation(libs.kotlin.logging)
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(libs.bundles.test.basics.kotlin)
+			}
+		}
+		val androidMain by getting {
+			dependencies {
+				api(project(":ifd:scio-backend:mobile-nfc"))
+			}
+		}
+	}
 }
---
+++ ifd/scio-backend/android-nfc/src/androidMain/kotlin/org/openecard/scio/AndroidNFCCardTerminal.kt
@@ -0,0 +1,61 @@
+/** **************************************************************************
+ * Copyright (C) 2019 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the Open eCard App.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
+package org.openecard.scio
+
+import android.nfc.tech.IsoDep
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.IOException
+
+/**
+ *
+ * @author Neil Crossley
+ */
+class AndroidNFCCardTerminal : NFCCardTerminal<AndroidNFCCard>() {
+
+    override fun prepareDevices(): Boolean {
+        val card = AndroidNFCCard(this)
+        return this.setNFCCard(card)
+    }
+
+    override val isCardPresent: Boolean
+        get() {
+            synchronized(cardLock) {
+                val currentCard: AndroidNFCCard? = this.nFCCard
+                return currentCard != null && currentCard.isTagPresent
+            }
+        }
+
+    @Throws(IOException::class)
+    fun setNFCTag(tag: IsoDep, timeout: Int) {
+        kotlin.synchronized(cardLock) {
+            val card = nFCCard
+            if (card == null) {
+                throw IOException("The NFC stack was not initialized and cannot prematurely accept the NFC tag.")
+            }
+            card.setTag(tag, timeout)
+        }
+        this.notifyCardPresent()
+    }
+
+}

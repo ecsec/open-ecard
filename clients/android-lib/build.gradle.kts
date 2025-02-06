0a623453d3c72f7c43f4b69c0948660bad51c02f
Tobias Wich <tobias.wich@ecsec.de>
2025-02-06 11:56:22 +0100

    chore: Don't downgrade jaxb in android

--- clients/android-lib/build.gradle.kts
+++ clients/android-lib/build.gradle.kts
@@ -30,30 +30,6 @@ kotlin {
 	}
 }
 
-dependencies {
-	constraints {
-		val reason = "Newer versions will break our build because of modularization since v4."
-		api("jakarta.xml.bind:jakarta.xml.bind-api") {
-			version {
-				strictly("3.0.1")
-			}
-			because(reason)
-		}
-		api("jakarta.xml.ws:jakarta.xml.ws-api") {
-			version {
-				strictly("3.0.1")
-			}
-			because(reason)
-		}
-		api("org.glassfish.jaxb:jaxb-runtime") {
-			version {
-				strictly("3.0.2-mobile")
-			}
-			because(reason)
-		}
-	}
-}
-
 configurations.api {
     exclude(group= "com.google.code.findbugs", module= "jsr305")
     exclude(group= "com.google.code.findbugs", module= "annotations")

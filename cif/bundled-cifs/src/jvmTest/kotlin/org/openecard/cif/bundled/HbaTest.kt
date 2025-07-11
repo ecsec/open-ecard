81e8636f7be839bcafa29f6473a78be2e2cfb08c
marinaartis <marina.artis@ecsec.de>
2025-07-11 14:02:02 +0200

    feat: Include CifVerifier in HbaTest

--- cif/bundled-cifs/src/jvmTest/kotlin/org/openecard/cif/bundled/HbaTest.kt
+++ cif/bundled-cifs/src/jvmTest/kotlin/org/openecard/cif/bundled/HbaTest.kt
@@ -1,14 +1,16 @@
 package org.openecard.cif.bundled
 
 import org.junit.jupiter.api.Disabled
+import org.openecard.cif.definition.CifVerifier
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class HbaTest {
 	@Disabled
 	@Test
-	fun `test eGK CIF`() {
+	fun `test HBA CIF`() {
 		val cif = HbaCif
+		CifVerifier(cif).verify()
 
 		assertEquals(7, cif.applications.size)
 

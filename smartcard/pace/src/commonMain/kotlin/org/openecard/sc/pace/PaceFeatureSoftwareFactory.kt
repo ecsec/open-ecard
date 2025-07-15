bd7e39cdc717dbb665a312c86413ee43c364126e
Tobias Wich <tobias.wich@ecsec.de>
2025-07-15 15:29:59 +0200

    feat: Move Pace factory to the pace package

--- smartcard/pace/src/commonMain/kotlin/org/openecard/sc/pace/PaceFeatureSoftwareFactory.kt
+++ smartcard/pace/src/commonMain/kotlin/org/openecard/sc/pace/PaceFeatureSoftwareFactory.kt
@@ -1,7 +1,8 @@
+package org.openecard.sc.pace
+
 import org.openecard.sc.iface.CardChannel
 import org.openecard.sc.iface.feature.PaceFeature
 import org.openecard.sc.iface.feature.PaceFeatureFactory
-import org.openecard.sc.pace.PaceProtocol
 
 class PaceFeatureSoftwareFactory : PaceFeatureFactory {
 	override fun create(channel: CardChannel): PaceFeature = PaceProtocol(channel)

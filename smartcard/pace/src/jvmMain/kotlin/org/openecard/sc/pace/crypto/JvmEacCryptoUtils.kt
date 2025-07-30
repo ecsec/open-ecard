a62961e2548d551fd5940810f874cf960ce636a1
Tobias Wich <tobias.wich@ecsec.de>
2025-07-30 12:15:29 +0200

    feat(pace): Add EAC crypto utils

---
+++ smartcard/pace/src/jvmMain/kotlin/org/openecard/sc/pace/crypto/JvmEacCryptoUtils.kt
@@ -0,0 +1,20 @@
+package org.openecard.sc.pace.crypto
+
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.openecard.sc.pace.asn1.StandardizedDomainParameters
+import org.openecard.sc.pace.crypto.BcDomainParameterResolver.resolveDomainParameters
+
+private val log = KotlinLogging.logger { }
+
+actual fun eacCryptoUtils(): EacCryptoUtils = JvmEacCryptoUtils()
+
+internal class JvmEacCryptoUtils : EacCryptoUtils {
+	@OptIn(ExperimentalUnsignedTypes::class)
+	override fun compressKey(
+		keyData: UByteArray,
+		domainParams: StandardizedDomainParameters,
+	): UByteArray {
+		val pk = BcEcKeyPair.decodePublicKey(domainParams.resolveDomainParameters(), keyData)
+		return pk.encodedCompressed
+	}
+}

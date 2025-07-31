46a15f543b023420ec715341ad137062aee798ab
Tobias Wich <tobias.wich@ecsec.de>
2025-07-31 19:25:37 +0200

    test: Add XML utilities to check serialized EAC messages

--- addons/tr03124/src/jvmTest/kotlin/org/openecard/addons/tr03124/xml/SerializationTest.kt
+++ addons/tr03124/src/jvmTest/kotlin/org/openecard/addons/tr03124/xml/SerializationTest.kt
@@ -1,9 +1,20 @@
 package org.openecard.addons.tr03124.xml
 
 import nl.adaptivity.xmlutil.serialization.XML
+import org.junit.jupiter.api.assertInstanceOf
+import org.openecard.utils.common.hex
+import org.w3c.dom.Document
+import org.w3c.dom.Node
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.namespace.NamespaceContext
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.xpath.XPathFactory
 import kotlin.test.Ignore
 import kotlin.test.Test
+import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalUnsignedTypes::class)
 class SerializationTest {
@@ -13,7 +24,6 @@ class SerializationTest {
 	fun `process Result`() {
 		val obj = Result("major", "minor", InternationalString("message", "en"))
 		val str = XML.encodeToString(obj)
-		println(str)
 		assertEquals(
 			"""
 			<Result xmlns="urn:oasis:names:tc:dss:1.0:core:schema"><ResultMajor>major</ResultMajor><ResultMinor>minor</ResultMinor><ResultMessage xml:lang="en">message</ResultMessage></Result>
@@ -26,12 +36,73 @@ class SerializationTest {
 	fun `process Eac1Input solo`() {
 		val original = readXml("Eac1Input_solo")
 		val obj = xml.decodeFromString<Eac1Input>(original)
-
-		assertEquals(7, obj.certificate.size)
-
+		assertEac1InputData(obj)
 		val ser = xml.encodeToString(obj)
-		println("Serialized XML:\n$ser")
-		println("First certificate hex: " + obj.certificate[0].v.toHexString())
+		assertEac1InputXml(ser, "/iso:EAC1InputType")
+	}
+
+	fun assertEac1InputData(obj: Eac1Input) {
+		assertEquals("urn:oid:1.3.162.15480.3.0.14", obj.protocol)
+		assertEquals(7, obj.certificate.size)
+		assertContentEquals(hex("7F218201487F4E8201005F29010042"), obj.certificate[0].v.sliceArray(0 until 15))
+		assertContentEquals(
+			hex("7F218201B67F4E82016E5F290100420E44454356434165494430303130327F4982011D06"),
+			obj.certificate[6].v.sliceArray(0 until 36),
+		)
+		assertContentEquals(
+			hex("3082027C060A04007F00070301030101A10E0C0C442D547275737420476D6248A21913"),
+			obj.certificateDescription.v.sliceArray(0 until 35),
+		)
+		assertContentEquals(
+			hex("7F4C12060904007F00070301020253050000000004"),
+			obj.requiredCHAT?.v,
+		)
+		assertContentEquals(
+			hex("7F4C12060904007F00070301020253050000000000"),
+			obj.optionalCHAT?.v,
+		)
+		assertContentEquals(
+			hex("67177315060904007F00070301040253083230323530373232"),
+			obj.authenticatedAuxiliaryData?.v,
+		)
+		assertTrue(obj.acceptedEIDType.isEmpty())
+	}
+
+	fun assertEac1InputXml(
+		data: String,
+		pathPrefix: String,
+	) {
+		val dom = parseXml(data)
+		dom.assertXpathEquals(
+			"$pathPrefix/@Protocol",
+			"urn:oid:1.3.162.15480.3.0.14",
+		)
+		dom.assertXpathEquals(
+			"count($pathPrefix/iso:Certificate)",
+			"7",
+		)
+		dom.assertXpathEquals(
+			"$pathPrefix/iso:Certificate[1]/text()",
+			"7F218201487F4E8201005F290100421044454456".lowercase(),
+		) {
+			it.trim().slice(0 until 40)
+		}
+		dom.assertXpathEquals(
+			"$pathPrefix/iso:CertificateDescription/text()",
+			"3082027C060A04007F00070301030101A10E0C0C".lowercase(),
+		) {
+			it.trim().slice(0 until 40)
+		}
+		dom.assertXpathEquals(
+			"$pathPrefix/iso:RequiredCHAT/text()",
+			"7F4C12060904007F00070301020253050000000004".lowercase(),
+		) {
+			it.trim()
+		}
+		dom.assertXpathEquals(
+			"count($pathPrefix/iso:AcceptedEIDType)",
+			"0",
+		)
 	}
 
 	@Test
@@ -66,8 +137,9 @@ class SerializationTest {
 	fun `process Eac1Input`() {
 		val original = readXml("Eac1Input")
 		val obj = xml.decodeFromString<DidAuthenticateRequest<Eac1Input>>(original)
-		// TODO: add assertions
+		assertEac1InputData(obj.data)
 		val ser = xml.encodeToString(obj)
+		assertEac1InputXml(ser, "/iso:DIDAuthenticate/iso:AuthenticationProtocolData")
 		// TODO: verify the serialized value
 	}
 
@@ -142,8 +214,9 @@ class SerializationTest {
 	fun `process Eac1Input SOAP`() {
 		val original = readSoap("Eac1Input")
 		val obj = xml.decodeFromString<Envelope>(original)
-		// TODO: add assertions
+		assertEac1InputData(assertInstanceOf<DidAuthenticateRequest<Eac1Input>>(obj.body.content).data)
 		val ser = xml.encodeToString(obj)
+		assertEac1InputXml(ser, "/soap:Envelope/soap:Body/iso:DIDAuthenticate/iso:AuthenticationProtocolData")
 		// TODO: verify the serialized value
 	}
 
@@ -226,4 +299,57 @@ class SerializationTest {
 			.use {
 				it.readAllBytes()
 			}.decodeToString()
+
+	fun parseXml(data: String): Document {
+		val documentBuilderFactory = DocumentBuilderFactory.newInstance()
+		documentBuilderFactory.isNamespaceAware = true
+		val docBuilder = documentBuilderFactory.newDocumentBuilder()
+		return docBuilder.parse(InputSource(StringReader(data)))
+	}
+
+	fun Node.assertXpathEquals(
+		xpath: String,
+		expected: String,
+		block: ((String) -> String) = { it },
+	) {
+		val xpathObj = XPathFactory.newInstance().newXPath()
+		xpathObj.namespaceContext = nsCtx
+		val result = xpathObj.evaluate(xpath, this)?.let { block(it) }
+		assertEquals(expected, result)
+	}
+
+	val nsCtx =
+		object : NamespaceContext {
+			private val nsMap = mutableMapOf<String, String>()
+
+			init {
+				addNs("iso", "urn:iso:std:iso-iec:24727:tech:schema")
+				addNs("soap", "http://schemas.xmlsoap.org/soap/envelope/")
+			}
+
+			private fun addNs(
+				prefix: String,
+				ns: String,
+			) {
+				nsMap[prefix] = ns
+			}
+
+			override fun getNamespaceURI(prefix: String): String? = nsMap[prefix]
+
+			override fun getPrefix(namespaceURI: String): String? =
+				getPrefixes(namespaceURI).let {
+					if (it.hasNext()) {
+						return it.next()
+					} else {
+						null
+					}
+				}
+
+			override fun getPrefixes(namespaceURI: String): Iterator<String> =
+				nsMap
+					.filterValues {
+						it == namespaceURI
+					}.values
+					.iterator()
+		}
 }

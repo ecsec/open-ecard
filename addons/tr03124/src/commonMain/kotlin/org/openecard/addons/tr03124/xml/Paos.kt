abe159656bb374ff8f003c46f46de12d2e7def46
Tobias Wich <tobias.wich@ecsec.de>
2025-08-25 13:22:59 +0200

    Revert "fix: Remove commented out code"
    This reverts commit 3daf0dae90bc7cafd555b52865dbfb9d529c6ba5.

--- addons/tr03124/src/commonMain/kotlin/org/openecard/addons/tr03124/xml/Paos.kt
+++ addons/tr03124/src/commonMain/kotlin/org/openecard/addons/tr03124/xml/Paos.kt
@@ -5,6 +5,22 @@ import kotlinx.serialization.Serializable
 import nl.adaptivity.xmlutil.serialization.XmlElement
 import nl.adaptivity.xmlutil.serialization.XmlSerialName
 
+// The PAOS specification vanished from the internet.
+// Here is the definition of the PAOS Element for reference purpose.
+//
+// <xs:element name="PAOS" type="PaosType"/>
+// <xs:complexType name="PaosType">
+//   <xs:complexContent>
+//     <xs:attribute ref="S:mustUnderstand" use="required"/>
+//     <xs:attribute ref="S:actor" use="required"/>
+//     <xs:sequence>
+//       <xs:element name="Version" type="xs:anyURI" minOccurs="1" maxOccurs="unbounded"/>
+//       <xs:element ref="a:EndpointReference" minOccurs="0" maxOccurs="unbounded"/>
+//       <xs:any namespace="##other" processContents="lax" minOccurs="0" maxOccurs="unbounded"/>
+//     </xs:sequence>
+//   </xs:simpleContent>
+// </xs:complexType>
+
 @Serializable
 @XmlSerialName("PAOS", prefix = Namespaces.PAOS.PREFIX, namespace = Namespaces.PAOS.NS)
 data class Paos(

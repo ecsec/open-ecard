39be4e90e354339a3296f5b6ab55c5dd2d495fa8
Florian Otto <florian.otto@ecsec.de>
2025-08-26 14:52:08 +0200

    test: add test for SM problem within cardlink

--- smartcard/pace/src/jvmTest/kotlin/org/openecard/sc/pace/PaceProtocolTest.kt
+++ smartcard/pace/src/jvmTest/kotlin/org/openecard/sc/pace/PaceProtocolTest.kt
@@ -2,9 +2,13 @@ package org.openecard.sc.pace
 
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.assertNotNull
+import org.openecard.sc.apdu.StatusWord
+import org.openecard.sc.apdu.StatusWordResult
 import org.openecard.sc.apdu.command.ReadBinary
 import org.openecard.sc.apdu.command.Select
 import org.openecard.sc.apdu.command.transmit
+import org.openecard.sc.apdu.toCommandApdu
 import org.openecard.sc.iface.feature.PaceEstablishChannelRequest
 import org.openecard.sc.iface.feature.PacePinId
 import org.openecard.sc.iface.withContext
@@ -13,11 +17,12 @@ import org.openecard.sc.pcsc.PcscTerminalFactory
 import org.openecard.utils.common.hex
 import java.security.cert.CertificateFactory
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 @WhenPcscStack
 class PaceProtocolTest {
-	val egkCan = "change for execution"
+	val egkCan = "123123"
 
 	@OptIn(ExperimentalUnsignedTypes::class)
 	@Test
@@ -42,6 +47,31 @@ class PaceProtocolTest {
 
 			val certs = CertificateFactory.getInstance("X.509").generateCertificates(cert.toByteArray().inputStream())
 			assertTrue { certs.isNotEmpty() }
+		}
+	}
+
+	@OptIn(ExperimentalUnsignedTypes::class)
+	@Test
+	fun `test internal authenticate on egk`() {
+		PcscTerminalFactory.instance.load().withContext { ctx ->
+			val terminal =
+				ctx.list().find { it.name.startsWith("REINER SCT cyberJack RFID basis") }
+					?: Assumptions.abort { "Necessary terminal not available" }
+			Assumptions.assumeTrue(terminal.isCardPresent()) { "Terminal does not contain a card" }
+
+			val con = terminal.connect()
+			val channel = checkNotNull(con.card).basicChannel
+
+			val paceProtocol = PaceProtocol(channel)
+			val req = PaceEstablishChannelRequest(PacePinId.CAN, egkCan, null, null)
+			val response = runBlocking { paceProtocol.establishChannel(req) }
+
+			channel.transmit(hex("002241A406840109800100").toCommandApdu())
+
+			val apdu = hex("00880000209C8CE4E11FE957A78D0780A110F1F97584A59037D4F0B8129C3A97BF9259826400").toCommandApdu()
+			val res = channel.transmit(apdu)
+			assertNotNull(res)
+			assertEquals(StatusWord.CONDITIONS_OF_USE_UNSATISFIED, res.status.type)
 		}
 	}
 }

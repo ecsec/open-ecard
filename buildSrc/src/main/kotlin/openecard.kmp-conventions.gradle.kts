ac26d8d198b11eb8a9cb80226a4526449b258a99
Tobias Wich <tobias.wich@ecsec.de>
2025-08-28 11:38:09 +0200

    chore: Factor out ktlint check to seperate conventions script

--- buildSrc/src/main/kotlin/openecard.kmp-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/openecard.kmp-conventions.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	kotlin("multiplatform")
-	id("org.jlleitschuh.gradle.ktlint")
+	id("openecard.ktlint-conventions")
 	// TODO: reactivate this when https://github.com/Kotlin/kotlinx-kover/issues/747 is solved
 	// id("openecard.coverage-conventions")
 	id("dev.mokkery")
@@ -20,43 +20,4 @@ val testHeapSize: String by project
 tasks.withType<Test> {
 	maxHeapSize = testHeapSize
 	useJUnitPlatform { }
-}
-
-// configure ktlint
-configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-	ignoreFailures = project.findProperty("ktlint.ignoreFailures")?.toString().toBoolean()
-	version =
-		versionCatalogs
-			.find("libs")
-			.get()
-			.findVersion("ktlint")
-			.get()
-			.requiredVersion
-
-	reporters {
-		customReporters {
-			register("gitlab") {
-				fileExtension = "json"
-				dependency =
-					versionCatalogs
-						.find("libs")
-						.get()
-						.findLibrary("ktlint.githubreporter")
-						.get()
-						.get()
-			}
-		}
-	}
-
-	filter {
-		// don't check generated sources
-		exclude {
-			val generatedRoot =
-				layout.buildDirectory
-					.dir("generated")
-					.get()
-					.asFile
-			it.file.startsWith(generatedRoot)
-		}
-	}
 }

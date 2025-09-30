27833b8372c812ce001cb25af45b66760a3d0ffa
Tobias Wich <tobias.wich@ecsec.de>
2025-09-30 18:51:26 +0200

    chore: Remove legacy module build scripts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ rootProject.name = "openecard-app"
 
 include("utils:common")
 include("utils:serialization")
-// include("utils:test")
 
 include("smartcard:sc-base")
 include("smartcard:pcsc-scio")
@@ -34,46 +33,15 @@ include("cif:bundled-cifs")
 include("releases")
 include("build-info")
 
-// include("wsdef:wsdef-common")
-// include("wsdef:wsdef-client")
-// include("wsdef:jaxb-marshaller")
-include("i18n")
-include("cifs")
-// include("common")
-// include("crypto:crypto-common")
-// include("crypto:tls")
-include("gui:graphics")
-include("gui:swing")
-// include("ifd:ifd-common")
-// include("ifd:ifd-core")
-// include("ifd:scio-backend:pcsc")
-// include("ifd:scio-backend:mobile-nfc")
-// include("ifd:scio-backend:android-nfc")
-// include("ifd:scio-backend:ios-nfc")
-// include("ifd:ifd-protocols:pace")
-// include("sal:sal-common")
-// include("sal:tiny-sal")
-// include("management")
-// include("bindings:http")
 include("bindings:ktor")
-// include("addon")
-// include("addons:chipgateway")
-// include("addons:genericcryptography")
-// include("addons:pin-management")
-// include("addons:status")
-// include("addons:tr03112")
-// include("addons:cardlink")
 
 include("addons:tr03124")
 
+include("i18n")
+include("gui:graphics")
+include("gui:swing")
 include("clients:richclient-res")
 include("clients:richclient")
-// include("clients:mobile-lib")
-// include("clients:ios-common")
-// include("clients:ios-lib")
-// include("clients:android-common")
-// include("clients:android-lib")
-// include("clients:desktop-lib")
 
 dependencyResolutionManagement {
 
@@ -97,14 +65,8 @@ dependencyResolutionManagement {
 	}
 
 	versionCatalogs {
-
 		create("libs") {
 			from(files("libs.versions.toml"))
-
-// 			library("nimbus-oidc", "com.nimbusds:oauth2-oidc-sdk:9.5.3")
-//
-// 			plugin("xjc-codegen", "com.github.bjornvester.xjc").version("1.8.1")
-// 			plugin("jsonschema2pojo", "org.jsonschema2pojo").version("1.2.1")
 		}
 	}
 }

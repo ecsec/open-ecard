e432ac9f9a0b4f74505137929d776c3557a7b9cc
Tobias Wich <tobias.wich@ecsec.de>
2025-10-07 09:08:52 +0200

    chore: Enable full desktop target for pcsc-native

--- smartcard/pcsc-native/build.gradle.kts
+++ smartcard/pcsc-native/build.gradle.kts
@@ -3,15 +3,10 @@ description = "Native PCSC Implementation"
 plugins {
 	id("openecard.kmp-lib-conventions")
 	id("openecard.kmp-jvm-conventions")
-	// id("openecard.kmp-desktop-conventions")
+	id("openecard.kmp-desktop-conventions")
 }
 
 kotlin {
-	// TODO: once we have all native implementations, use desktop convention script
-	linuxX64 { }
-	mingwX64 { }
-	// macosX64 { }
-
 	sourceSets {
 		commonMain.dependencies {
 			implementation(libs.kotlin.logging)

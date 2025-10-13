859d2ba62076de6e2a24c806162bd2d3f8d648ce
Tobias Wich <tobias.wich@ecsec.de>
2025-10-13 19:55:41 +0200

    feat: Add default cardwatcher callback, so only relevant callback

--- clients/richclient/src/main/kotlin/org/openecard/richclient/sc/CardWatcherCallback.kt
+++ clients/richclient/src/main/kotlin/org/openecard/richclient/sc/CardWatcherCallback.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import kotlin.coroutines.cancellation.CancellationException
 
 interface CardWatcherCallback {
 	fun onInitialState(cardState: CardState)
@@ -21,22 +22,48 @@ interface CardWatcherCallback {
 		cardType: String,
 	)
 
+	/**
+	 * Class providing no-op default methods for all callback functions.
+	 */
+	open class CardWatcherCallbackDefault : CardWatcherCallback {
+		override fun onInitialState(cardState: CardState) {}
+
+		override fun onTerminalAdded(terminalName: String) {}
+
+		override fun onTerminalRemoved(terminalName: String) {}
+
+		override fun onCardInserted(terminalName: String) {}
+
+		override fun onCardRemoved(terminalName: String) {}
+
+		override fun onCardRecognized(
+			terminalName: String,
+			cardType: String,
+		) {}
+	}
+
 	companion object {
 		fun CardWatcherCallback.registerWith(
 			watcher: CardWatcher,
-			scope: CoroutineScope = CoroutineScope(Dispatchers.Default),
+			scope: CoroutineScope = CoroutineScope(Dispatchers.IO),
 		): Job =
 			scope.launch {
 				val sink = watcher.registerSink()
-				sink.collect {
-					when (it) {
-						is CardStateEvent.InitialCardState -> onInitialState(it.cardState)
-						is CardStateEvent.TerminalAdded -> onTerminalAdded(it.terminalName)
-						is CardStateEvent.TerminalRemoved -> onTerminalRemoved(it.terminalName)
-						is CardStateEvent.CardInserted -> onCardInserted(it.terminalName)
-						is CardStateEvent.CardRemoved -> onCardRemoved(it.terminalName)
-						is CardStateEvent.CardRecognized -> onCardRecognized(it.terminalName, it.cardType)
+				try {
+					sink.collect {
+						when (it) {
+							is CardStateEvent.InitialCardState -> onInitialState(it.cardState)
+							is CardStateEvent.TerminalAdded -> onTerminalAdded(it.terminalName)
+							is CardStateEvent.TerminalRemoved -> onTerminalRemoved(it.terminalName)
+							is CardStateEvent.CardInserted -> onCardInserted(it.terminalName)
+							is CardStateEvent.CardRemoved -> onCardRemoved(it.terminalName)
+							is CardStateEvent.CardRecognized -> onCardRecognized(it.terminalName, it.cardType)
+						}
 					}
+				} catch (ex: CancellationException) {
+					// we got cancelled, so remove the callback from the watcher
+					watcher.unregisterSink(sink)
+					throw ex
 				}
 			}
 	}

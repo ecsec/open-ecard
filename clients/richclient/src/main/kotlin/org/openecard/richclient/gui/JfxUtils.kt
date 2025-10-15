4a4e6e5ac64e05ab40613009dced8920abd7e845
Tobias Wich <tobias.wich@ecsec.de>
2025-10-15 19:52:56 +0200

    feat: Add adapter for moko resources to Java ResourceBundle

--- clients/richclient/src/main/kotlin/org/openecard/richclient/gui/JfxUtils.kt
+++ clients/richclient/src/main/kotlin/org/openecard/richclient/gui/JfxUtils.kt
@@ -4,14 +4,20 @@ import javafx.embed.swing.SwingFXUtils
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.scene.image.Image
+import org.openecard.richclient.MR
+import org.openecard.richclient.MokoResourceBundle
 import java.awt.image.BufferedImage
+import java.util.ResourceBundle
 
 object JfxUtils {
+	val richclientResourceBundle by lazy { MokoResourceBundle(MR.strings.values()) }
+
 	fun <V : Parent, C> loadFxml(
 		fileName: String,
 		prefix: String = "/fxml/",
+		resourceBundle: ResourceBundle? = richclientResourceBundle,
 	): Pair<V, C> {
-		val loader = FXMLLoader(javaClass.getResource("$prefix$fileName"))
+		val loader = FXMLLoader(javaClass.getResource("$prefix$fileName"), resourceBundle)
 		val view = loader.load<V>()
 		val controller = loader.getController<C>()
 		return view to controller

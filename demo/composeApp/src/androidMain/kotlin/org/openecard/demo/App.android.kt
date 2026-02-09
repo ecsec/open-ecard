ddfa74dfcc70c0812101458bc5806fa62dc63305
marinaartis <marina.artis@ecsec.de>
2026-02-09 12:14:35 +0100

    fix: Remove unnecessary files

--- demo/composeApp/src/androidMain/kotlin/org/openecard/demo/App.android.kt
+++
@@ -1,6 +0,0 @@
-package org.openecard.demo
-
-import android.content.Context
-import android.content.Intent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext

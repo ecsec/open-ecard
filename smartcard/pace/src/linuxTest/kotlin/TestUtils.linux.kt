8a05b0751299811a387bed0bd69c5e9b5a4db541
Tobias Wich <tobias.wich@ecsec.de>
2026-02-25 12:55:46 +0100

    refactor: Correct setting of log level after updating kt-logging

--- smartcard/pace/src/linuxTest/kotlin/TestUtils.linux.kt
+++ smartcard/pace/src/linuxTest/kotlin/TestUtils.linux.kt
@@ -2,5 +2,5 @@ import io.github.oshai.kotlinlogging.KotlinLoggingConfiguration
 import io.github.oshai.kotlinlogging.Level
 
 actual fun configureLog() {
-	KotlinLoggingConfiguration.logLevel = Level.DEBUG
+	KotlinLoggingConfiguration.direct.logLevel = Level.DEBUG
 }
